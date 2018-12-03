269198161af682f784868e72c5ac0c9d98c2fae9
Gabriel Andersson <gabriel.andersson@zenuity.com>
2018-12-03 12:36:54 +0100

    :construction: Added day 4 template

---
+++ src/main/java/krabban91/kodvent/kodvent/day4/Day4.java
@@ -0,0 +1,33 @@
+package krabban91.kodvent.kodvent.day4;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class Day4 {
+
+    private static String inputPath = "day4.txt";
+
+    public Day4() {
+
+        System.out.println("::: Starting Day 4:::");
+        int part1 = getPart1();
+        System.out.println("::: answer to part 1:::");
+        System.out.println(part1);
+        int part2 = getPart2();
+        System.out.println("::: answer to part 2:::");
+        System.out.println(part2);
+    }
+
+    private int getPart1() {
+        return -1;
+    }
+
+    private int getPart2() {
+        return -1;
+    }
+}
