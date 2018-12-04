7d8b090eed5bcd7e4cfe7fe800cfaeab0041dae9
Gabriel Andersson <anderssongabriel665@gmail.com>
2018-12-04 21:18:30 +0100

    :recycle: Refactored day 1.

--- src/main/java/krabban91/kodvent/kodvent/day1/FrequencyHandler.java
+++ src/main/java/krabban91/kodvent/kodvent/day1/FrequencyHandler.java
@@ -8,46 +8,54 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FrequencyHandler {
 
     private static String inputPath = "day1.txt";
-
+    private List<Integer> frequencyDeltas;
     private List<Integer> frequencies = new LinkedList<>();
     private Integer firstTwice = null;
 
-    public  FrequencyHandler() {
+    public FrequencyHandler() {
         System.out.println("::: Starting Day 1 :::");
-        int part1 = 0;
-        part1 = getPart1(part1);
+        loadInput();
         System.out.println(": answer to part 1 :");
+        int part1 = getPart1();
         System.out.println(part1);
-        while (firstTwice == null) {
-            part1 = getPart1(part1);
-        }
         System.out.println(": answer to part 2 :");
-        Integer part2 = firstTwice;
+        int part2 = getPart2();
         System.out.println(part2);
     }
 
-    private int getPart1(int part1) {
+    public int getPart1() {
+        return frequencyDeltas.stream().reduce(0, Integer::sum);
+    }
+
+    public int getPart2() {
+        int startValue = 0;
+        while (firstTwice == null) {
+            startValue = frequencyDeltas.stream().reduce(startValue, this::sum);
+        }
+        return firstTwice;
+    }
+
+    private void loadInput() {
         try (Stream<String> stream = Files.lines(Paths.get(new ClassPathResource(inputPath).getFile().getPath()))) {
-            part1 = stream.mapToInt(Integer::parseInt).reduce(part1,this::sum);
+            frequencyDeltas = stream.map(Integer::parseInt).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return part1;
     }
 
-    private int sum(int l, int r){
+    private int sum(int l, int r) {
         int frequency = l + r;
-        if(frequencies.contains(frequency)){
-            if(firstTwice == null){
+        if (frequencies.contains(frequency)) {
+            if (firstTwice == null) {
                 firstTwice = frequency;
             }
-        }
-        else {
+        } else {
             frequencies.add(frequency);
         }
         return frequency;
