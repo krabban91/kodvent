731ff68f19179b842a30764d5f2795cbf8d21c3e
Gabriel Andersson <gabriel.andersson@zenuity.com>
2018-12-27 16:04:11 +0100

    :sparkles: Year 2015. Day 1

---
+++ src/main/java/krabban91/kodvent/kodvent/y2015/d01/Day1.java
@@ -0,0 +1,43 @@
+package krabban91.kodvent.kodvent.y2015.d01;
+
+import krabban91.kodvent.kodvent.utilities.Input;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class Day1 {
+    String in;
+
+    public long getPart1() {
+        return in.chars().map(c -> c == (int) '(' ? 1 : -1).reduce(0, Integer::sum);
+    }
+
+    public int getPart2() {
+        AtomicInteger floor = new AtomicInteger(0);
+        AtomicInteger position = new AtomicInteger(0);
+        in.chars().forEach(c -> {
+            if (floor.get() >= 0) {
+                floor.addAndGet(c == (int) '(' ? 1 : -1);
+                position.incrementAndGet();
+            }
+        });
+        return position.get();
+    }
+
+    public void readInput(String inputPath) {
+        in = Input.getSingleLine(inputPath);
+    }
+
+    public Day1() {
+        System.out.println("::: Starting Day 1 :::");
+        String inputPath = "y2015/d01/input.txt";
+        readInput(inputPath);
+        long part1 = getPart1();
+        System.out.println(": answer to part 1 :");
+        System.out.println(part1);
+        long part2 = getPart2();
+        System.out.println(": answer to part 2 :");
+        System.out.println(part2);
+    }
+}
--- src/test/java/krabban91/kodvent/kodvent/y2015/d01/Day1Test.java
+++ src/test/java/krabban91/kodvent/kodvent/y2015/d01/Day1Test.java
@@ -1,5 +1,6 @@
-package krabban91.kodvent.kodvent.y2018.d03;
+package krabban91.kodvent.kodvent.y2015.d01;
 
+import krabban91.kodvent.kodvent.y2018.d03.FabricSlicer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +11,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FabricSlicerTest {
+public class Day1Test {
 
     @Autowired
-    FabricSlicer fabricSlicer;
+    Day1 day1;
 
     @Test
     public void getPart1() {
-        long part1 = fabricSlicer.getPart1();
-        assertThat(part1).isEqualTo(4);
+        long part1 = day1.getPart1();
+        assertThat(part1).isEqualTo(-1);
     }
 
     @Test
     public void getPart2() {
-        int part2 = fabricSlicer.getPart2();
-        assertThat(part2).isEqualTo(3);
+        int part2 = day1.getPart2();
+        assertThat(part2).isEqualTo(5);
     }
+
 }
