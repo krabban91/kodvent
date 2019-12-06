61b7c36f1e88ef052f74a924c2771fa52c318a97
Gabriel Andersson <anderssongabriel665@gmail.com>
2019-12-06 08:13:16 +0100

    :sparkles: year 2019 day 6 (#14)
    * Prepare year 2019 day 06
    
    * completed part 1 and part 2
    
    * slight refactoring of day 06
    
    * made helper functions static
    
    * Hide methods that don't need to be exposed
    
    * Renamed stepsTaken -> orbitalDistance

--- src/main/java/krabban91/kodvent/kodvent/y2019/d06/Day6.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d06/Day6.java
@@ -1,24 +1,20 @@
 package krabban91.kodvent.kodvent.y2019.d06;
 
 import krabban91.kodvent.kodvent.utilities.Input;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ConcurrentReferenceHashMap;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-//@Component
+@Component
 public class Day6 {
-    List<String> in;
-
-    public long getPart1() {
-        return -1L;
-    }
-
-    public long getPart2() {
-        return -1;
-    }
-
-    public void readInput(String inputPath) {
-        in = Input.getLines(inputPath);
-    }
+    List<OrbitRelation> in;
 
     public Day6() {
         System.out.println("::: Starting Day 6 :::");
@@ -30,5 +26,72 @@ public class Day6 {
         long part2 = getPart2();
         System.out.println(": answer to part 2 :");
         System.out.println(part2);
+    }
+
+    @NotNull
+    private static Map<String, Set<String>> getOrbits(List<OrbitRelation> relations) {
+        Map<String, Set<String>> isOrbitedBy = new HashMap<>();
+        relations.forEach(o -> {
+            isOrbitedBy.computeIfAbsent(o.main, k -> new HashSet<>());
+            isOrbitedBy.computeIfPresent(o.main, (k, v) -> {
+                v.add(o.orbiter);
+                return v;
+            });
+        });
+        return isOrbitedBy;
+    }
+
+    private static boolean indirectOrbit(String target, String current, Map<String, Set<String>> isOrbitedBy) {
+        if (target.equals(current)) {
+            return true;
+        }
+        if (!isOrbitedBy.containsKey(current)) {
+            return false;
+        }
+        return isOrbitedBy.get(current).stream().anyMatch(s -> s.equals(target) || indirectOrbit(target, s, isOrbitedBy));
+    }
+
+    public long getPart1() {
+        Map<String, Set<String>> isOrbitedBy = getOrbits(in);
+        Map<String, Integer> countOfOrbits = new ConcurrentReferenceHashMap<>();
+        isOrbitedBy.keySet().forEach(e -> countOfOrbits.computeIfAbsent(e, k -> celestialBodysOf(k, isOrbitedBy, countOfOrbits)));
+        return countOfOrbits.values().stream().mapToInt(i -> i).sum();
+    }
+
+    public long getPart2() {
+        Map<String, Set<String>> orbitedBy = getOrbits(in);
+        return orbitalDistance("YOU", "SAN", 0, orbitedBy) - 2;
+    }
+
+    private int celestialBodysOf(String current, Map<String, Set<String>> isOrbitedBy, Map<String, Integer> counts) {
+        Set<String> bodys = isOrbitedBy.get(current);
+        if (bodys == null) {
+            return 0;
+        }
+        return bodys.size() + bodys.stream()
+                .mapToInt(body -> counts.computeIfAbsent(body, key -> celestialBodysOf(key, isOrbitedBy, counts)))
+                .sum();
+    }
+
+    private int orbitalDistance(String current, String target, int stepsTaken, Map<String, Set<String>> isOrbitedBy) {
+        if (current.equals(target)) {
+            return stepsTaken;
+        }
+        if (indirectOrbit(target, current, isOrbitedBy)) {
+            //move out
+            String newCurrent = isOrbitedBy.get(current).stream().filter(s -> indirectOrbit(target, s, isOrbitedBy)).findFirst().get();
+            return orbitalDistance(newCurrent, target, stepsTaken + 1, isOrbitedBy);
+        } else {
+            //move in
+            String newCurrent = in.stream().filter(rel -> rel.orbiter.equals(current)).findFirst().get().main;
+            return orbitalDistance(newCurrent, target, stepsTaken + 1, isOrbitedBy);
+        }
+    }
+
+    public void readInput(String inputPath) {
+        in = Input.getLines(inputPath)
+                .stream()
+                .map(OrbitRelation::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/test/java/krabban91/kodvent/kodvent/y2019/d06/Day6Test.java
@@ -0,0 +1,27 @@
+package krabban91.kodvent.kodvent.y2019.d06;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Day6Test {
+
+    @Autowired
+    Day6 day6;
+
+    @Test
+    public void getPart1() {
+        assertThat(day6.getPart1()).isEqualTo(417916L);
+    }
+
+    @Test
+    public void getPart2() {
+        assertThat(day6.getPart2()).isEqualTo(523);
+    }
+}
