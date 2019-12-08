83afa4852586c36d6ec2bf67aeb00e928b615d92
krabban91 <anderssongabriel665@gmail.com>
2019-12-08 10:22:46 +0100

    startup day 9 as component

---
+++ src/test/java/krabban91/kodvent/kodvent/y2019/d09/Day9Test.java
@@ -0,0 +1,27 @@
+package krabban91.kodvent.kodvent.y2019.d09;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Day9Test {
+
+    @Autowired
+    Day9 day9;
+
+    @Test
+    public void getPart1() {
+        assertThat(day9.getPart1()).isEqualTo(-1);
+    }
+
+    @Test
+    public void getPart2() {
+        assertThat(day9.getPart2()).isEqualTo(-1);
+    }
+}
