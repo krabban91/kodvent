3490c662fb14d707698a7211d3160bc6d0099cc2
Gabriel Andersson <anderssongabriel665@gmail.com>
2019-12-08 10:20:26 +0100

    :sparkles: year 2019 day 08 (#16)
    * completed part 1 and 2. Barely.
    
    * Removed test for part 2
    * Added LogUtils method for map of Point, Integer.

--- src/main/java/krabban91/kodvent/kodvent/utilities/logging/LogUtils.java
+++ src/main/java/krabban91/kodvent/kodvent/utilities/logging/LogUtils.java
@@ -1,6 +1,10 @@
 package krabban91.kodvent.kodvent.utilities.logging;
 
+import java.awt.*;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.function.Function;
 
 public class LogUtils {
 
@@ -11,5 +15,18 @@ public class LogUtils {
                         .reduce("", (l1, l2) -> l1 + l2, (l1, l2) -> "" + l1 + l2))
                 .reduce((l1, l2) -> l1 + "\n" + l2)
                 .orElse("");
+    }
+    public static String mapToText(Map<Point, Integer> map, Function<Integer, String> valueMapper) {
+        int width = map.keySet().stream().mapToInt(p -> p.x + 1).max().orElse(0);
+        int height = map.keySet().stream().mapToInt(p -> p.y + 1).max().orElse(0);
+        StringBuilder b = new StringBuilder();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Integer obj = map.get(new Point(x, y));
+                b.append(obj == 0 ? " " : "*");
+            }
+            b.append("\n");
+        }
+        return b.toString();
     }
 }
--- src/main/java/krabban91/kodvent/kodvent/y2019/d08/Day8.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d08/Day8.java
@@ -1,24 +1,19 @@
 package krabban91.kodvent.kodvent.y2019.d08;
 
 import krabban91.kodvent.kodvent.utilities.Input;
+import krabban91.kodvent.kodvent.utilities.logging.LogUtils;
+import org.springframework.stereotype.Component;
 
+import java.awt.*;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-//@Component
+@Component
 public class Day8 {
-    List<String> in;
-
-    public long getPart1() {
-        return -1L;
-    }
-
-    public long getPart2() {
-        return -1;
-    }
-
-    public void readInput(String inputPath) {
-        in = Input.getLines(inputPath);
-    }
+    List<Integer> in;
 
     public Day8() {
         System.out.println("::: Starting Day 8 :::");
@@ -27,8 +22,61 @@ public class Day8 {
         long part1 = getPart1();
         System.out.println(": answer to part 1 :");
         System.out.println(part1);
-        long part2 = getPart2();
+        String part2 = getPart2();
         System.out.println(": answer to part 2 :");
         System.out.println(part2);
+    }
+
+    public long getPart1() {
+        Map<Integer, Map<Point, Integer>> image = getImage(25, 6);
+        return image.values()
+                .stream()
+                .min(Comparator.comparingLong(m -> m.values()
+                        .stream()
+                        .filter(i -> i == 0)
+                        .count()))
+                .map(m -> m.values().stream().filter(i -> i == 1).count() * m.values().stream().filter(i -> i == 2).count())
+                .orElse(-1L);
+    }
+
+    public String getPart2() {
+        int width = 25;
+        int height = 6;
+        Map<Point, Integer> finalImage = new HashMap<>();
+
+        Map<Integer, Map<Point, Integer>> image = getImage(25, 6);
+        for (int i = 0; i < in.size(); i++) {
+            Integer integer = image.get(layerNumber(width, height, i)).get(new Point(getX(width, height, i), getY(width, i)));
+            if (integer != 2) {
+                finalImage.putIfAbsent(new Point(getY(width, i), getX(width, height, i)), integer);
+            }
+        }
+        return LogUtils.mapToText(finalImage, i -> i == 0 ? " " : "*");
+    }
+
+    public void readInput(String inputPath) {
+        in = Input.getSingleLine(inputPath).chars().mapToObj(i -> (char) i + "")
+                .map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private Map<Integer, Map<Point, Integer>> getImage(int width, int height) {
+        Map<Integer, Map<Point, Integer>> image = new HashMap<>();
+        for (int i = 0; i < in.size(); i++) {
+            image.putIfAbsent(layerNumber(width, height, i), new HashMap<>());
+            image.get(layerNumber(width, height, i)).put(new Point(getX(width, height, i), getY(width, i)), in.get(i));
+        }
+        return image;
+    }
+
+    private int getY(int width, int i) {
+        return i % width;
+    }
+
+    private int getX(int width, int height, int i) {
+        return (getY(width * height, i)) / width;
+    }
+
+    private int layerNumber(int width, int height, int i) {
+        return i / (width * height);
     }
 }
