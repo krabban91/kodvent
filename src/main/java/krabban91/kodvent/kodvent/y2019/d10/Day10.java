559417da7466718bb6e78b0047b5955fdee304fc
Gabriel Andersson <gabriel.andersson@zenuity.com>
2019-12-10 12:45:26 +0100

    Refactored the day 10

--- src/main/java/krabban91/kodvent/kodvent/y2019/d10/Day10.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d10/Day10.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -17,7 +18,7 @@ import java.util.stream.IntStream;
 
 @Component
 public class Day10 {
-    List<List<Boolean>> in;
+    Map<Point, Boolean> in;
 
     public Day10() {
         System.out.println("::: Starting Day 10 :::");
@@ -32,75 +33,96 @@ public class Day10 {
     }
 
     public long getPart1() {
-        List<List<AsteroidSlot>> collect = IntStream.range(0, in.size()).mapToObj(y -> IntStream.range(0, in.get(y).size()).mapToObj(x -> new AsteroidSlot(x, y, in.get(y).get(x))).collect(Collectors.toList())).collect(Collectors.toList());
-        Map<Point, AsteroidSlot> allSlots = collect.stream().flatMap(List::stream).collect(Collectors.toMap(AsteroidSlot::getPoint, a->a));
+        Map<Point, Boolean> allSlots = new HashMap<>(in);
         return getStation(allSlots)
-                .map(e->e.getValue().size())
+                .map(e -> e.getValue().size())
                 .orElse(-1);
     }
 
-    public Optional<Map.Entry<Point, Set<Double>>> getStation(Map<Point, AsteroidSlot> allSlots) {
-        List<AsteroidSlot> asteroidsList = allSlots.values().stream().filter(a -> a.isAsteroid()).collect(Collectors.toList());
+    public long getPart2() {
+        Map<Point, Boolean> allSlots = new HashMap<>(in);
+        Point origo = getStation(allSlots)
+                .get()
+                .getKey();
+        LinkedList<Point> blownUp = new LinkedList<>();
+        double currentAngle = Math.PI / 2;
+        while (blownUp.size() != 200) {
+            Map<Double, Set<Point>> potentialTargets = getPotentialTargets(origo, allSlots);
+            Map.Entry<Double, Point> target = target(origo, potentialTargets, strictlyPositiveRadians(currentAngle));
+            currentAngle = target.getKey();
+            blownUp.add(target.getValue());
+            allSlots.remove(target.getValue());
+        }
+        return blownUp.getLast().x * 100 + blownUp.getLast().y;
+    }
+
+    public Optional<Map.Entry<Point, Set<Double>>> getStation(Map<Point, Boolean> allSlots) {
+        List<Map.Entry<Point, Boolean>> asteroidsList = allSlots.entrySet()
+                .stream()
+                .filter(Map.Entry::getValue)
+                .collect(Collectors.toList());
         Map<Point, Set<Double>> collect1 = asteroidsList.stream()
-                .collect(Collectors.toMap(AsteroidSlot::getPoint, origo -> asteroidsList.stream()
-                        .map(asteroid -> satan2(origo.getPoint(), asteroid.getPoint()))
+                .collect(Collectors.toMap(Map.Entry::getKey, origo -> asteroidsList.stream()
+                        .map(asteroid -> satan2(origo.getKey(), asteroid.getKey()))
                         .collect(Collectors.toSet())));
         return collect1.entrySet().stream()
-                .max(Comparator.comparingInt(e-> e.getValue().size()));
+                .max(Comparator.comparingInt(e -> e.getValue().size()));
     }
 
-    public long getPart2() {
-        List<List<AsteroidSlot>> collect = IntStream.range(0, in.size()).mapToObj(y -> IntStream.range(0, in.get(y).size()).mapToObj(x -> new AsteroidSlot(x, y, in.get(y).get(x))).collect(Collectors.toList())).collect(Collectors.toList());
-        Map<Point, AsteroidSlot> allSlots = collect.stream().flatMap(List::stream).collect(Collectors.toMap(AsteroidSlot::getPoint, a->a));
-        Point origo = getStation(allSlots).get().getKey();
-        int blownUpAsteroids = 0;
-        Point latestBlownUp = null;
-        double currentAngle = Math.PI/2;
-        while(blownUpAsteroids != 200){
-            Map<Double, Set<Point>> potentialTargets = getPotentialTargets(origo, allSlots);
-            Map.Entry<Double, Point> target = target(origo, potentialTargets, (currentAngle + Math.PI*2)%(Math.PI*2));
-            currentAngle = target.getKey();
-            latestBlownUp = target.getValue();
-            blownUpAsteroids +=1;
-            allSlots.get(target.getValue()).obliterate();
-        }
-        // 405 is too low. 924 is too low
-        return latestBlownUp.x*100 + latestBlownUp.y;
-    }
-
-    private Map<Double, Set<Point>> getPotentialTargets(Point origo, Map<Point, AsteroidSlot> allSlots) {
+    private Map<Double, Set<Point>> getPotentialTargets(Point origo, Map<Point, Boolean> allSlots) {
         Map<Double, Set<Point>> map = new HashMap<>();
         allSlots.entrySet().stream()
-                .filter(e->e.getValue().isAsteroid())
-                .forEach(e-> {
-            double angle = satan2(origo, e.getKey());
-            map.computeIfAbsent(angle, a->new HashSet<>());
-            map.get(angle).add(e.getKey());
-        });
+                .filter(Map.Entry::getValue)
+                .forEach(e -> {
+                    double angle = satan2(origo, e.getKey());
+                    map.computeIfAbsent(angle, a -> new HashSet<>());
+                    map.get(angle).add(e.getKey());
+                });
         return map;
     }
-    private Map.Entry<Double, Point> target(Point origo, Map<Double, Set<Point>> potential, double currentAngle) {
-        if (currentAngle == 0){
-            currentAngle = Math.PI*2;
-        }
-        double finalCurrentAngle = currentAngle;
-        Optional<Map.Entry<Double, Set<Point>>> max = potential.entrySet().stream()
-                .filter(e -> e.getKey().compareTo(finalCurrentAngle) < 0)
-                .max(Comparator.comparingDouble(e -> e.getKey()));
 
-        Map.Entry<Double, Set<Point>> closestAngleWise = max
+    private Map.Entry<Double, Point> target(Point origo, Map<Double, Set<Point>> potential, double currentAngle) {
+        return potential.entrySet().stream()
+                .filter(e -> e.getKey().compareTo(currentAngle) < 0)
+                .max(Comparator.comparingDouble(Map.Entry::getKey))
+                .map(e -> Map.entry(
+                        e.getKey(),
+                        e.getValue()
+                                .stream()
+                                .min(Comparator.comparingInt(p -> Distances.manhattan(origo, p)))
+                                .get()))
                 .get();
-        Optional<Point> min = closestAngleWise.getValue().stream().min(Comparator.comparingInt(p -> Distances.manhattan(origo, p)));
-        return Map.entry(closestAngleWise.getKey(), min.get());
     }
 
 
     private double satan2(Point origo, Point point) {
-        return (Math.atan2(-1*(point.y - origo.y) , ((double) point.x - origo.x))+Math.PI*2)%(Math.PI*2);
+        // adjust vector to start from (0,0)
+        // adjust y factor to negative axis
+        double x = point.x - origo.x;
+        double y = -1 * (point.y - origo.y);
+        double angle = Math.atan2(y, x);
+        // ensure that angle is always positive
+        return positiveRadians(angle);
+    }
+
+    private double positiveRadians(double angle) {
+        double fullCycle = Math.PI * 2;
+        return (angle + fullCycle) % (fullCycle);
+    }
+
+    private double strictlyPositiveRadians(double angle) {
+        double out = positiveRadians(angle);
+        return out == 0 ? Math.PI * 2 : out;
     }
 
 
     public void readInput(String inputPath) {
-        in = Input.getLines(inputPath).stream().map(s-> s.chars().mapToObj(i-> ((char)i)=='#').collect(Collectors.toList())).collect(Collectors.toList());
+        List<String> lines = Input.getLines(inputPath);
+        in = IntStream.range(0, lines.size())
+                .mapToObj(y -> IntStream.range(0, lines.get(y).length())
+                        .mapToObj(x -> Map.entry(new Point(x, y), lines.get(y).charAt(x) == '#'))
+                        .collect(Collectors.toList()))
+                .flatMap(List::stream)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
