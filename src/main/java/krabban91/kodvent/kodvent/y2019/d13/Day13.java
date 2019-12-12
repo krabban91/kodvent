3e4fb39b38fc9ba2ccec1fe4751734d397a10c48
Gabriel Andersson <gabriel.andersson@zenuity.com>
2019-12-12 17:04:10 +0100

    :construction: Started day 13 as component

--- src/main/java/krabban91/kodvent/kodvent/y2019/d13/Day13.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d13/Day13.java
@@ -1,24 +1,13 @@
 package krabban91.kodvent.kodvent.y2019.d13;
 
 import krabban91.kodvent.kodvent.utilities.Input;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-//@Component
+@Component
 public class Day13 {
     List<String> in;
-
-    public long getPart1() {
-        return -1L;
-    }
-
-    public long getPart2() {
-        return -1;
-    }
-
-    public void readInput(String inputPath) {
-        in = Input.getLines(inputPath);
-    }
 
     public Day13() {
         System.out.println("::: Starting Day 13 :::");
@@ -30,5 +19,18 @@ public class Day13 {
         long part2 = getPart2();
         System.out.println(": answer to part 2 :");
         System.out.println(part2);
+    }
+
+    public long getPart1() {
+
+        return -1L;
+    }
+
+    public long getPart2() {
+        return -1;
+    }
+
+    public void readInput(String inputPath) {
+        in = Input.getLines(inputPath);
     }
 }
