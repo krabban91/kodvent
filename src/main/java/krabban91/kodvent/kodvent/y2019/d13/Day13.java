edbf5bbc41edc3c735ee3eda6907c976009d11a4
krabban91 <anderssongabriel665@gmail.com>
2019-12-13 06:37:24 +0100

    Refactored part 1 to execute as runnable

--- src/main/java/krabban91/kodvent/kodvent/y2019/d13/Day13.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d13/Day13.java
@@ -2,6 +2,7 @@ package krabban91.kodvent.kodvent.y2019.d13;
 
 import krabban91.kodvent.kodvent.utilities.Input;
 import krabban91.kodvent.kodvent.y2019.shared.IntCodeComputer;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
@@ -12,7 +13,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -21,7 +25,7 @@ import java.util.stream.Stream;
 public class Day13 {
     List<Long> in;
 
-    public Day13() {
+    public Day13() throws InterruptedException {
         System.out.println("::: Starting Day 13 :::");
         String inputPath = "y2019/d13/input.txt";
         readInput(inputPath);
@@ -33,17 +37,30 @@ public class Day13 {
         System.out.println(part2);
     }
 
-    public long getPart1() {
+    public long getPart1() throws InterruptedException {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
         final LinkedBlockingDeque<Long> outputs = new LinkedBlockingDeque<>();
         IntCodeComputer computer = new IntCodeComputer(in, new LinkedBlockingDeque<>(), outputs);
-        computer.run();
-        final ArrayList<Long> copy = new ArrayList<>(outputs);
+        executor.execute(computer);
         final Map<Point, Tile> map = new HashMap<>();
-        IntStream.range(0,copy.size()/3).forEach(i-> map.put(new Point(copy.get(i*3).intValue(), copy.get(i*3+1).intValue()), new Tile(copy.get(i*3+2))));
+        while (!computer.hasHalted() || !outputs.isEmpty()){
+            final Long x = computer.pollOutput(2);
+            final Long y = computer.pollOutput(2);
+            final Long type = computer.pollOutput(2);
+            map.put(new Point(x.intValue(),y.intValue()),new Tile(type));
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1L, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return map.values().stream()
                 .filter(Tile::isBlock)
                 .count();
     }
+
 
     public long getPart2() {
 
