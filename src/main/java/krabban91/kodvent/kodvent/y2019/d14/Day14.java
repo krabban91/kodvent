054a015d732aad83712279fae2e8de7aa5d2616d
krabban91 <anderssongabriel665@gmail.com>
2019-12-14 10:49:07 +0100

    Added the examples to test suite.

--- src/main/java/krabban91/kodvent/kodvent/y2019/d14/Day14.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d14/Day14.java
@@ -19,9 +19,9 @@ public class Day14 {
         System.out.println("::: Starting Day 14 :::");
         String inputPath = "y2019/d14/input.txt";
         readInput(inputPath);
-        long part1 = getPart1();
-        System.out.println(": answer to part 1 :");
-        System.out.println(part1);
+        //long part1 = getPart1();
+        //System.out.println(": answer to part 1 :");
+        //System.out.println(part1);
         long part2 = getPart2();
         System.out.println(": answer to part 2 :");
         System.out.println(part2);
@@ -36,53 +36,160 @@ public class Day14 {
         // 44 XJWVT -> 44*(7 DCFZ, 7 PSHF)
         final ConcurrentReferenceHashMap<String, Integer> current = new ConcurrentReferenceHashMap<>();
         current.put(fuel.output, 1);
-        final long ore = reverseEngineerMolecule(currentMaterials, Set.of("ORE"), in);
-        Map<String, Set<String>> reactionMap = in.stream().collect(Collectors.toMap(e -> e.output, e -> e.input.keySet()));
-        
-        return getOreCount(fuel, collect, current);
+        return reverseEngineerMolecule(currentMaterials, Set.of("ORE"), in);
 
-        //65007L is wrong. 198072697914L is wrong
+        //65007L is wrong. 198072697914L is wrong, 85254 is too low
     }
 
     private long reverseEngineerMolecule(Map<String, Integer> start, Set<String> target, List<Reaction> transitions) {
         long bestScenario = Long.MAX_VALUE;
-        for (int i = 0; i < 1000; i++) {
+        final List<Reaction> oreT = transitions.stream().filter(r -> r.input.containsKey("ORE")).collect(Collectors.toList());
+        final List<Reaction> other = transitions.stream().filter(r -> !r.input.containsKey("ORE")).collect(Collectors.toList());
+        for (int i = 0; i < 100; i++) {  // try more times in case there are better ways.
             Map<String, Integer> current = new HashMap<>(start);
             while (!current.keySet().equals(target)) {
                 Collections.shuffle(transitions);
                 Map<String, Integer> tmp = new HashMap<>(current);
-                for (Reaction rea : transitions) {
-                    if (current.containsKey(rea.output)) {
-                        final Integer integer = current.get(rea.output);
-                        final int left = integer - rea.quantity;
-                        if (left < 0) {
-                            if (rea.input.size() == 1 && rea.input.containsKey("ORE")) {
-                                current.remove(rea.output);
-                                current.computeIfPresent("ORE", (k, v) -> v + rea.input.get("ORE"));
-                                current.putIfAbsent("ORE", rea.input.get("ORE"));
+                if (current.keySet().stream().allMatch(s -> oreT.stream().anyMatch(r -> r.output.equals(s)))) {
+                    while (!current.keySet().equals(target)) {
+                        for (Reaction rea : oreT) {
+                            if (current.containsKey(rea.output)) {
+                                final Integer outQuantity = current.get(rea.output);
+                                final int left = outQuantity - rea.quantity;
+                                if (rea.input.containsKey("ORE")) {
+                                    if (left <= 0) {
+                                        current.remove(rea.output);
+                                    } else {
+                                        current.computeIfPresent(rea.output, (k, v) -> v - rea.quantity);
+                                    }
+                                    current.computeIfPresent("ORE", (k, v) -> v + rea.input.get("ORE"));
+                                    current.putIfAbsent("ORE", rea.input.get("ORE"));
+                                }
                             }
-                            continue;
-                        } else if (left == 0) {
-                            current.remove(rea.output);
-                        } else {
-                            current.computeIfPresent(rea.output, (k, v) -> left);
                         }
-                        Map<String, Integer> finalCurrent = current;
-                        rea.input.forEach((k, v) -> {
-                            finalCurrent.putIfAbsent(k, v);
-                            finalCurrent.computeIfPresent(k, (kk, vv) -> vv + v);
-                        });
                     }
                 }
+                for (Reaction rea : other) {
+                    if (current.containsKey(rea.output)) {
+                        final Integer outQuantity = current.get(rea.output);
+                        final int left = outQuantity - rea.quantity;
+                        {
+                            int factor = 1;
+                            if (left <= 0) {
+                                current.remove(rea.output);
+                                int tmpVal = 0;
+                                int tmpFactor = 0;
+                                while (tmpVal<rea.quantity){
+                                    tmpVal +=outQuantity;
+                                    tmpFactor++;
+                                }
+                                factor = tmpFactor;
 
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> left);
+                            }
+                            Map<String, Integer> finalCurrent = current;
+                            int finalFactor = factor;
+                            rea.input.forEach((k, v) -> {
+                                finalCurrent.computeIfPresent(k, (kk, vv) -> vv + finalFactor * v);
+                                finalCurrent.putIfAbsent(k, finalFactor * v);
+                            });
+                        }
+                    }
+                }
                 if (current.equals(tmp)) {
-                    current = start;
+                    current = new HashMap<>(start);
                     Collections.shuffle(transitions);
                 }
             }
             bestScenario = Math.min(bestScenario, current.get("ORE"));
         }
         return bestScenario;
+        /*
+        for (Reaction rea : transitions) {
+                    if (current.containsKey(rea.output)) {
+                        final Integer outQuantity = current.get(rea.output);
+                        final int left = outQuantity - rea.quantity;
+                        if (rea.input.containsKey("ORE")) {
+                            if (left <= 0) {
+                                current.remove(rea.output);
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> v - rea.quantity);
+                            }
+                            current.computeIfPresent("ORE", (k, v) -> v + rea.input.get("ORE"));
+                            current.putIfAbsent("ORE", rea.input.get("ORE"));
+                        } else {
+                            int factor = 1;
+                            if (left < 0) {
+                                current.remove(rea.output);
+                                factor = rea.quantity / outQuantity + 1;
+                            } else if (left == 0) {
+                                current.remove(rea.output);
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> left);
+                            }
+                            Map<String, Integer> finalCurrent = current;
+                            int finalFactor = factor;
+                            rea.input.forEach((k, v) -> {
+                                finalCurrent.computeIfPresent(k, (kk, vv) -> vv + finalFactor *v);
+                                finalCurrent.putIfAbsent(k, finalFactor *v);
+                            });
+                            int a = 0;
+                        }
+                    }
+                }
+
+                for (Reaction rea : oreT) {
+                    if (current.containsKey(rea.output)) {
+                        final Integer integer = current.get(rea.output);
+                        final int left = integer - rea.quantity;
+                        if (rea.input.containsKey("ORE")) {
+
+                            if (left <= 0) {
+                                current.remove(rea.output);
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> v - rea.quantity);
+                            }
+                            current.computeIfPresent("ORE", (k, v) -> v + rea.input.get("ORE"));
+                            current.putIfAbsent("ORE", rea.input.get("ORE"));
+                        }
+                    }
+                }
+                }
+
+                for (Reaction rea : other) {
+                    if (current.containsKey(rea.output)) {
+                        final Integer integer = current.get(rea.output);
+                        final int left = integer - rea.quantity;
+                        if (rea.input.containsKey("ORE")) {
+
+                            if (left <= 0) {
+                                current.remove(rea.output);
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> v - rea.quantity);
+                            }
+                            current.computeIfPresent("ORE", (k, v) -> v + rea.input.get("ORE"));
+                            current.putIfAbsent("ORE", rea.input.get("ORE"));
+                        } else {
+                            int factor = 1;
+                            if (left < 0) {
+                                current.remove(rea.output);
+                                factor = rea.quantity / integer + 1;
+                            } else if (left == 0) {
+                                current.remove(rea.output);
+                            } else {
+                                current.computeIfPresent(rea.output, (k, v) -> left);
+                            }
+                            Map<String, Integer> finalCurrent = current;
+                            int finalFactor = factor;
+                            rea.input.forEach((k, v) -> {
+                                finalCurrent.computeIfPresent(k, (kk, vv) -> vv + finalFactor *v);
+                                finalCurrent.putIfAbsent(k, finalFactor *v);
+                            });
+                            int a = 0;
+                        }
+                    }
+                }*/
     }
 
     private long getOreCount(Reaction reaction, Map<String, Reaction> collect, ConcurrentReferenceHashMap<String, Integer> current) {
--- src/test/java/krabban91/kodvent/kodvent/y2019/d14/Day14Test.java
+++ src/test/java/krabban91/kodvent/kodvent/y2019/d14/Day14Test.java
@@ -17,11 +17,37 @@ public class Day14Test {
 
     @Test
     public void getPart1() {
+        day14.readInput("y2019/d14/input.txt");
         assertThat(day14.getPart1()).isEqualTo(-1);
     }
 
     @Test
+    public void getPart1Example1() {
+        day14.readInput("y2019/d14/example1.txt");
+        assertThat(day14.getPart1()).isEqualTo(31);
+    }
+    @Test
+    public void getPart1Example2() {
+        day14.readInput("y2019/d14/example2.txt");
+        assertThat(day14.getPart1()).isEqualTo(165);
+    }
+    @Test
+    public void getPart1Example3() {
+        day14.readInput("y2019/d14/example3.txt");
+        assertThat(day14.getPart1()).isEqualTo(13312);
+    }    @Test
+    public void getPart1Example4() {
+        day14.readInput("y2019/d14/example4.txt");
+        assertThat(day14.getPart1()).isEqualTo(180697);
+    }    @Test
+    public void getPart1Example5() {
+        day14.readInput("y2019/d14/example5.txt");
+        assertThat(day14.getPart1()).isEqualTo(2210736);
+    }
+
+    @Test
     public void getPart2() {
+        day14.readInput("y2019/d14/input.txt");
         assertThat(day14.getPart2()).isEqualTo(-1);
     }
 }
