fb5e34a0e90dc40f2afdc1ee6b9d82326f3e267b
Gabriel Andersson <gabriel.andersson@zenuity.com>
2019-12-17 17:42:55 +0100

    Completed part 1
    * Repeated means to actually repeat. no mathematical action

--- src/main/java/krabban91/kodvent/kodvent/y2019/d16/Day16.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d16/Day16.java
@@ -1,9 +1,9 @@
 package krabban91.kodvent.kodvent.y2019.d16;
 
 import krabban91.kodvent.kodvent.utilities.Input;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,7 +17,7 @@ public class Day16 {
         System.out.println("::: Starting Day 16 :::");
         String inputPath = "y2019/d16/input.txt";
         readInput(inputPath);
-        long part1 = getPart1();
+        String part1 = getPart1();
         System.out.println(": answer to part 1 :");
         System.out.println(part1);
         long part2 = getPart2();
@@ -25,49 +25,43 @@ public class Day16 {
         System.out.println(part2);
     }
 
-    public long getPart1() {
-        in = "12345678";
+    public String getPart1() {
         List<Integer> basePattern = Arrays.asList(0, 1, 0, -1);
-        final List<Integer> pattern = firstPattern(basePattern);
-        final List<Integer> pattern1 = repeatPattern(pattern, basePattern, 1);
-        final List<Integer> pattern2 = repeatPattern(pattern1, basePattern, 2);
-        final List<Integer> input = in.chars().mapToObj(c -> Integer.parseInt((char) c + "")).collect(Collectors.toList());
-        return -1L;
+
+        List<Integer> input = in.chars().mapToObj(c -> Integer.parseInt((char) c + "")).collect(Collectors.toList());
+        for (int i = 0; i < 100; i++) {
+            input = FFT(input, basePattern);
+        }
+        return input.subList(0,8).stream().reduce("", (a,b) -> a+""+b, (s1,s2)-> s1+s2);
     }
 
     public List<Integer> FFT(List<Integer> input, List<Integer> basePattern) {
-        return null;
-    }
-
-    private List<Integer> firstPattern(List<Integer> basePattern) {
-        final List<Integer> collect = fullBasePattern(basePattern);
-        return collect.subList(1, in.length() + 1);
-    }
-
-    @NotNull
-    private List<Integer> fullBasePattern(List<Integer> basePattern) {
-        return IntStream.rangeClosed(0, in.length())
-                .mapToObj(i -> basePattern.get(i % basePattern.size()))
-                .collect(Collectors.toList());
-    }
-
-    private List<Integer> repeatPattern(List<Integer> pattern, List<Integer> basePattern, int index){
-        final List<Integer> pattern1 = fullBasePattern(basePattern);
-        return IntStream.range(0,pattern.size()).mapToObj(i-> i<index? +0:Math.abs(pattern.get(i))+pattern1.get(i+1)).collect(Collectors.toList());
-    }
-
-    public List<Integer> patternForIndex(List<Integer> basePattern, int index){
-
-        final List<Integer> pattern = fullBasePattern(basePattern);
-        List<Integer> result = firstPattern(basePattern);
-        for(int iter = 0; iter<index; iter++){
-            List<Integer> finalResult = result;
-            result =  IntStream.range(0,result.size())
-                    .mapToObj(i-> i<index? +0: finalResult.get(i)+pattern.get(i+1))
-                    .collect(Collectors.toList());
-
+        List<Integer> output = new ArrayList<>();
+        for (int i = 0; i <input.size(); i++) {
+            output.add(calculate(input, patternForIndex(basePattern, i)));
         }
-        return result;
+        return output;
+    }
+
+
+    public List<Integer> patternForIndex(List<Integer> basePattern, int index) {
+        List<Integer> result = IntStream.range(0,basePattern.size())
+                .mapToObj(i-> IntStream.rangeClosed(0,index)
+                        .mapToObj(t-> basePattern.get(i))
+                        .collect(Collectors.toList()))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        return repeatAndShiftPattern(result);
+    }
+
+    private List<Integer> repeatAndShiftPattern(List<Integer> basePattern) {
+        return IntStream.rangeClosed(0, in.length()+1)
+                .mapToObj(i -> basePattern.get(i % basePattern.size()))
+                .collect(Collectors.toList()).subList(1,in.length()+1);
+    }
+
+    public int calculate(List<Integer> input, List<Integer> pattern){
+        return Math.abs(IntStream.range(0,input.size()).map(i-> input.get(i)*pattern.get(i)).sum())%10;
     }
 
     public long getPart2() {
--- src/test/java/krabban91/kodvent/kodvent/y2019/d16/Day16Test.java
+++ src/test/java/krabban91/kodvent/kodvent/y2019/d16/Day16Test.java
@@ -18,23 +18,22 @@ public class Day16Test {
     Day16 day16;
 
     @Test
-    public void patternForIndex(){
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 0).subList(0,5)).isEqualTo(List.of(1,0,-1,0,1));
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 1).subList(0,5)).isEqualTo(List.of(0,1,1,0,0));
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 2).subList(0,5)).isEqualTo(List.of(0,0,1,1,1));
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 3).subList(0,5)).isEqualTo(List.of(0,0,0,1,1));
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 4).subList(0,5)).isEqualTo(List.of(0,0,0,0,1));
-        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 5).subList(0,5)).isEqualTo(List.of(0,0,0,0,0));
-
+    public void patternForIndex() {
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 0).subList(0, 5)).isEqualTo(List.of(1, 0, -1, 0, 1));
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 1).subList(0, 5)).isEqualTo(List.of(0, 1, 1, 0, 0));
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 2).subList(0, 5)).isEqualTo(List.of(0, 0, 1, 1, 1));
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 3).subList(0, 5)).isEqualTo(List.of(0, 0, 0, 1, 1));
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 4).subList(0, 5)).isEqualTo(List.of(0, 0, 0, 0, 1));
+        assertThat(day16.patternForIndex(List.of(0, 1, 0, -1), 5).subList(0, 5)).isEqualTo(List.of(0, 0, 0, 0, 0));
     }
 
     @Test
-    public void getPart1() throws InterruptedException {
-        assertThat(day16.getPart1()).isEqualTo(-1);
+    public void getPart1() {
+        assertThat(day16.getPart1()).isEqualTo("58672132");
     }
 
     @Test
-    public void getPart2() throws InterruptedException {
+    public void getPart2() {
         assertThat(day16.getPart2()).isEqualTo(-1);
     }
 }
