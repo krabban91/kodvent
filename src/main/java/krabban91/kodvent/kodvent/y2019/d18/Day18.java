bb2bf6c920e10361b71ab9dcf99d8af980177c7b
Gabriel Andersson <gabriel.andersson@zenuity.com>
2019-12-29 11:36:33 +0100

    Prevent moving towards keys beyond other keys

--- src/main/java/krabban91/kodvent/kodvent/y2019/d18/Day18.java
+++ src/main/java/krabban91/kodvent/kodvent/y2019/d18/Day18.java
@@ -84,6 +84,12 @@ public class Day18 {
         Map<Map.Entry<Point, Integer>, DistanceToPoint> pathsToKeys = keys.entrySet().stream().collect(Collectors.toMap(e -> e, e -> getDistanceToPoint(startLocation, e.getKey(), network)));
 
 
+        Map<Integer, Set<Integer>> passedKeys = pathsToKeys.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey().getValue(), e -> e.getValue().allVisited().stream()
+                        .filter(keys::containsKey)
+                        .map(keys::get)
+                        .filter(i -> !i.equals(e.getKey().getValue()))
+                        .collect(Collectors.toSet())));
         Map<Integer, List<Integer>> dependencyKeys = pathsToKeys.entrySet().stream()
                 .collect(Collectors.toMap(e -> e.getKey().getValue(), e -> e.getValue().allVisited().stream()
                         .filter(doors::containsKey)
@@ -105,12 +111,14 @@ public class Day18 {
         int stepsTaken = Integer.MAX_VALUE;
         logMap(map);
         Map<List<Integer>, Map<Integer, Set<Integer>>> differentDependencies = new HashMap<>();
+        Map<List<Integer>, Map<Integer, Set<Integer>>> differentPassedKeys = new HashMap<>();
         PriorityQueue<Map.Entry<List<Integer>, LinkedList<DistanceToPoint>>> priorityQueuePaths = new PriorityQueue<>(Comparator.comparingInt(e -> e.getValue().getLast().heuristic()));
 
         Map<List<Integer>, Map<Point, Integer>> differentKeys = new HashMap<>();
         Map<List<Integer>, Map<Point, Integer>> differentDoors = new HashMap<>();
 
         differentDependencies.put(Collections.emptyList(), deepCopyDependencies(dependencyGraph));
+        differentPassedKeys.put(Collections.emptyList(), deepCopyDependencies(passedKeys));
         priorityQueuePaths.add(Map.entry(Collections.emptyList(), new LinkedList<>(Collections.singletonList(new DistanceToPoint(startLocation, startLocation)))));
         differentKeys.put(Collections.emptyList(), new HashMap<>(keys));
         differentDoors.put(Collections.emptyList(), new HashMap<>(doors));
@@ -122,17 +130,20 @@ public class Day18 {
             Map<Point, Integer> localishKeys = differentKeys.get(localishMapKey);
 
             Map<Integer, Set<Integer>> localishDependencies = deepCopyDependencies(differentDependencies.get(localishMapKey));
+            Map<Integer, Set<Integer>> localishPassedKeys = deepCopyDependencies(differentPassedKeys.get(localishMapKey));
 
             // TODO : This generated a heap space overflow for more possible paths than 8 and using BFS
             List<Point> targets = localishDependencies.entrySet().stream()
                     .filter(e -> e.getValue().isEmpty())
                     .filter(e -> !mapKey.contains(e.getKey()))
+                    .filter(e-> localishPassedKeys.get(e.getKey()).isEmpty())
                     .map(e -> keyLookup.get(e.getKey()))
                     .collect(Collectors.toList());
             Map<Point, Integer> pointIntegerMap1 = differentDoors.get(localishMapKey);
             for (Point target : targets) {
-
+                //TODO: Build information about localPassedKeys and add to differentPassedKeys
                 Map<Integer, Set<Integer>> localDependencies = deepCopyDependencies(localishDependencies);
+                Map<Integer, Set<Integer>> localPassedKeys = deepCopyDependencies(localishPassedKeys);
                 List<Integer> localMapKey = new ArrayList<>(localishMapKey);
                 HashMap<Point, Integer> localKeys = new HashMap<>(localishKeys);
                 LinkedList<DistanceToPoint> localPaths = new LinkedList<>(poll.getValue());
@@ -155,9 +166,13 @@ public class Day18 {
                 DistanceToPoint searchCopy = new DistanceToPoint(search,
                         () -> localKeys.values().stream().map(keyLookup::get).mapToInt(p -> Distances.manhattan(p, search.destination())).sum());
                 //() -> 0);
+
+                // TODO: Replace with only destination
                 keysInPath.forEach(removedKey -> {
                     localDependencies.remove(removedKey);
                     localDependencies.forEach((i, s) -> s.remove(removedKey));
+                    localPassedKeys.remove(removedKey);
+                    localPassedKeys.forEach((i,s)-> s.remove(removedKey));
                     // unlock door connected to key
                     localDoors.entrySet().stream()
                             .filter(i -> removedKey == i.getValue() + UPPER_TO_LOWER)
@@ -171,10 +186,11 @@ public class Day18 {
                     return -1;
 
                 }
-                differentDependencies.remove(mapKey);
+                /*differentDependencies.remove(mapKey);
+                differentPassedKeys.remove(mapKey);
                 differentDoors.remove(mapKey);
                 differentKeys.remove(mapKey);
-
+*/
                 localPaths.addLast(searchCopy);
                 int stepsTakenSoFar = localPaths.stream().mapToInt(DistanceToPoint::cost).sum();
                 if (localKeys.isEmpty()) {
@@ -183,22 +199,30 @@ public class Day18 {
                         int finalStepsTaken = stepsTaken;
                         List<List<Integer>> keysToRemove = priorityQueuePaths.stream().filter(e -> e.getValue().getLast().heuristic() >= finalStepsTaken).map(Map.Entry::getKey).collect(Collectors.toList());
                         priorityQueuePaths.removeIf(e -> keysToRemove.contains(e.getKey()));
-                        keysToRemove.forEach(k -> {
+                        /*keysToRemove.forEach(k -> {
                             differentDependencies.remove(k);
+                            differentPassedKeys.remove(k);
                             differentDoors.remove(k);
                             differentKeys.remove(k);
                         });
 
+                         */
+
                     }
+                    /*
                     differentDependencies.remove(mapKey);
+                    differentPassedKeys.remove(mapKey);
                     differentDoors.remove(mapKey);
                     differentKeys.remove(mapKey);
+
+                     */
                 } else {
                     if (stepsTakenSoFar < stepsTaken) {
                         if (!differentDependencies.containsKey(localMapKey)) {
                             priorityQueuePaths.add(Map.entry(localMapKey, localPaths));
                         }
                         differentDependencies.put(localMapKey, localDependencies);
+                        differentPassedKeys.put(localMapKey, localPassedKeys);
                         differentDoors.put(localMapKey, localDoors);
                         differentKeys.put(localMapKey, localKeys);
                         //differentMaps.put(localMapKey, localMap);
@@ -206,7 +230,6 @@ public class Day18 {
                 }
             }
         }
-
         // return number of steps taken
         // 6534 is too high. 5680 is wrong, 5636 is wrong, 5594 is wrong
         return stepsTaken;
