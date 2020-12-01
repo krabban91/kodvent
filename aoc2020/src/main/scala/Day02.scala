3272f56c7f6d5482bf76e27191fcccfedcb458ce
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-01 16:03:34 +0100

    :construction: Preparation for 2020:day02

---
+++ aoc2020/src/main/scala/Day02.scala
@@ -0,0 +1,11 @@
+
+
+object Day02 extends App with AoC with AoCTest {
+
+  printResult
+  printResultTest
+
+  override def part1(input: scala.Seq[String]): Long = -1
+
+  override def part2(input: scala.Seq[String]): Long = -1
+}
