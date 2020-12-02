dbb5e4a653b9bb161f5b886ecb7a52942ce51444
Gabriel Andersson <anderssongabriel665@gmail.com>
2020-12-02 06:43:22 +0100

    :sparkles: year 2020 day 02 (#37)
    * :construction: Preparation for 2020:day02
    
    * :sparkles: completed y2020d02
    
    Co-authored-by: gandersson <gabriel.andersson@recordedfuture.com>

---
+++ aoc2020/src/main/scala/Day02.scala
@@ -0,0 +1,33 @@
+
+
+object Day02 extends App with AoC with AoCTest {
+
+  printResult
+  printResultTest
+
+  override def part1(input: scala.Seq[String]): Long = input
+    .map(Input(_))
+    .map(in => {
+      val count = in.pwd.count(_.equals(in.char))
+      count >= in.min && count <= in.max
+    })
+    .count(b => b)
+
+  override def part2(input: scala.Seq[String]): Long = input
+    .map(Input(_))
+    .map(in => in.pwd(in.min - 1).equals(in.char) ^ in.pwd(in.max - 1).equals(in.char))
+    .count(b => b)
+
+  case class Input(pwd: String, min: Int, max: Int, char: Char)
+
+  object Input {
+    def apply(in: String): Input = {
+      val pwd = in.split(":")(1).trim
+      val min = in.split(":")(0).split("-")(0).toInt
+      val max = in.split(":")(0).split("-")(1).split(" ")(0).toInt
+      val char = in.split(":")(0).split(" ")(1).charAt(0)
+      Input(pwd, min, max, char)
+    }
+  }
+}
+
