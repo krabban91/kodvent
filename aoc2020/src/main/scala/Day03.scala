de9efe5dd12a2237366ceb3d47765c751470c0f1
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-03 06:19:39 +0100

    :sparkles: completed day 03

--- aoc2020/src/main/scala/Day03.scala
+++ aoc2020/src/main/scala/Day03.scala
@@ -2,12 +2,25 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day03 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
+  override def part1(strings: scala.Seq[String]): Long = countTrees(strings, 3, 1)
 
-  override def part1(strings: scala.Seq[String]): Long = -1
+  override def part2(strings: scala.Seq[String]): Long = countTrees(strings, 1, 1) *
+    countTrees(strings, 3, 1) *
+    countTrees(strings, 5, 1) *
+    countTrees(strings, 7, 1) *
+    countTrees(strings, 1, 2)
 
-  override def part2(strings: scala.Seq[String]): Long = -1
+
+  private def countTrees(strings: Seq[String], right: Int, down: Int): Long = {
+    var x = 0
+    var y = 0
+    var trees: Long = 0
+    while (y < strings.size) {
+      if (strings(y)(x % strings(y).size).equals('#')) trees += 1
+      y += down
+      x += right
+
+    }
+    trees
+  }
 }
--- aoc2020/src/test/scala/Day03Spec.scala
+++ aoc2020/src/test/scala/Day03Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day03Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day03.part1TestResult shouldEqual -1
+    Day03.part1TestResult shouldEqual 7L
   }
   "Part1" should "be correct" in {
-    Day03.part1Result shouldEqual -1
+    Day03.part1Result shouldEqual 232L
   }
   "Part2 Test" should "be correct" in {
-    Day03.part2TestResult shouldEqual -1
+    Day03.part2TestResult shouldEqual 336L
   }
   "Part2" should "be correct" in {
-    Day03.part2Result shouldEqual -1
+    Day03.part2Result shouldEqual 3952291680L
   }
 }
