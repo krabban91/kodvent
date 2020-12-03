baba3abb2c69683b336d859d9795570a3598dc13
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-03 07:28:36 +0100

    :hammer: Added template files to help preparing for new days

---
+++ aoc2020/src/main/scala/DayXX.scala
@@ -0,0 +1,13 @@
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+
+object DayXX extends App with AoCPart1Test with AoCPart2Test {
+
+  printResultPart1Test
+  printResultPart1
+  printResultPart2Test
+  printResultPart2
+
+  override def part1(strings: scala.Seq[String]): Long = -1
+
+  override def part2(strings: scala.Seq[String]): Long = -1
+}
