4dd291e79a12b09e64f03876f3a75ed3a1e78598
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-05 09:14:28 +0100

    :art: restructure solution to something more applicable

--- aoc2020/src/main/scala/Day05.scala
+++ aoc2020/src/main/scala/Day05.scala
@@ -5,60 +5,42 @@ object Day05 extends App with AoCPart1 with AoCPart2 {
   printResultPart1
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = strings.map(Boardingpass(_)).map(_.seatId()).max
+  override def part1(strings: Seq[String]): Long = strings.map(Boardingpass(_)).map(_.seatId).max
 
-  override def part2(strings: Seq[String]): Long = {
-    val seats = strings.map(Boardingpass(_))
-    missingSeat(
-      seats
-        .filter(b => b.row != seats.minBy(_.row).row && b.row != seats.maxBy(_.row).row)
-        .groupBy(_.row)
-        .minBy((kv) => kv._2.size)._2)
-      .seatId()
+  override def part2(strings: Seq[String]): Long = missingSeat(strings.map(Boardingpass(_)).map(_.seatId).sorted)
+
+  def missingSeat(seats: Seq[Long]): Long = seats
+    .indices.find(i => i != 0 && seats(i - 1) + 1 != seats(i))
+    .map(i => seats(i) - 1)
+    .getOrElse(-1)
+
+  object Boardingpass {
+    def apply(string: String): Boardingpass = Boardingpass(Integer.parseInt(fromElfBinary(string), 2))
+
+    private def fromElfBinary(string: String) = string
+      .replace('F', '0')
+      .replace('B', '1')
+      .replace('L', '0')
+      .replace('R', '1')
+
   }
 
-  def missingSeat(seats: Seq[Boardingpass]): Boardingpass = {
-    val cols = seats.map(_.column).sorted
-    var missing = -1
-    var prev = -1
-    for (i <- cols) {
-      if (prev != i - 1) {
-        missing = i - 1
-      }
-      prev = i
-    }
-    Boardingpass(seats.head.row, missing, "")
-  }
-}
+  case class Boardingpass(seatId: Long) {
 
-case class Boardingpass(row: Int, column: Int, raw: String) {
-  def seatId(): Long = {
-    row * 8 + column
+    def row: Long = (seatId - (seatId % 8)) / 8
+
+    def column: Long = (seatId % 8)
+
+    def getId: String = rowInElfBinary + columnInElfBinary
+
+    private def columnInElfBinary: String = ("0" * 3 + column.toBinaryString takeRight 3)
+      .replace('0', 'L')
+      .replace('1', 'R')
+
+    private def rowInElfBinary: String = ("0" * 7 + row.toBinaryString takeRight 7)
+      .replace('0', 'F')
+      .replace('1', 'B')
   }
 
-  def getId: String = Boardingpass.toElfBinaryRow("0" * 7 + row.toBinaryString takeRight 7) +
-    Boardingpass.toElfBinarySeat("0" * 3 + column.toBinaryString takeRight 3)
 }
 
-object Boardingpass {
-  def apply(string: String): Boardingpass = Boardingpass(
-    Integer.parseInt(fromElfBinary(string.substring(0, 7)), 2),
-    Integer.parseInt(fromElfBinary(string.substring(7)), 2), string)
-
-  def apply(seatId: Int): Boardingpass =
-    Boardingpass((seatId - (seatId % 8)) / 8, (seatId % 8), "")
-
-  private def fromElfBinary(string: String) = string
-    .replace('F', '0')
-    .replace('B', '1')
-    .replace('L', '0')
-    .replace('R', '1')
-
-  private def toElfBinarySeat(string: String) = string
-    .replace('0', 'L')
-    .replace('1', 'R')
-
-  private def toElfBinaryRow(string: String) = string
-    .replace('0', 'F')
-    .replace('1', 'B')
-}
