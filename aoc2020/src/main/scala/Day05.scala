b52774653809d6449e83f975778d33253b107c5f
Gabriel Andersson <anderssongabriel665@gmail.com>
2020-12-05 09:18:54 +0100

    :sparkles: year 2020 day 05 (#40)
    * :construction: copied the templates for day 05
    
    * :sparkles: completed day 05
    
    * Realised too far in that the expected
      result was the seatId and not the
      binary elf format.
      - refactoring coming up
    
    * :art: restructure solution to something more applicable
    
    * removed debug logging from day 05
    
    Co-authored-by: gandersson <gabriel.andersson@recordedfuture.com>

---
+++ aoc2020/src/main/scala/Day05.scala
@@ -0,0 +1,43 @@
+import aoc.numeric.{AoCPart1, AoCPart2}
+
+object Day05 extends App with AoCPart1 with AoCPart2 {
+
+  override def part1(strings: Seq[String]): Long = strings.map(Boardingpass(_)).map(_.seatId).max
+
+  override def part2(strings: Seq[String]): Long = missingSeat(strings.map(Boardingpass(_)).map(_.seatId).sorted)
+
+  def missingSeat(seats: Seq[Long]): Long = seats
+    .indices.find(i => i != 0 && seats(i - 1) + 1 != seats(i))
+    .map(i => seats(i) - 1)
+    .getOrElse(-1)
+
+  object Boardingpass {
+    def apply(string: String): Boardingpass = Boardingpass(Integer.parseInt(fromElfBinary(string), 2))
+
+    private def fromElfBinary(string: String) = string
+      .replace('F', '0')
+      .replace('B', '1')
+      .replace('L', '0')
+      .replace('R', '1')
+
+  }
+
+  case class Boardingpass(seatId: Long) {
+
+    def row: Long = (seatId - (seatId % 8)) / 8
+
+    def column: Long = (seatId % 8)
+
+    def getId: String = rowInElfBinary + columnInElfBinary
+
+    private def columnInElfBinary: String = ("0" * 3 + column.toBinaryString takeRight 3)
+      .replace('0', 'L')
+      .replace('1', 'R')
+
+    private def rowInElfBinary: String = ("0" * 7 + row.toBinaryString takeRight 7)
+      .replace('0', 'F')
+      .replace('1', 'B')
+  }
+
+}
+
