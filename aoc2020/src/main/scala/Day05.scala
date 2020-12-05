5c8e885c3bb73438b8418543707356a540bdcc95
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-05 08:41:23 +0100

    :sparkles: completed day 05
    * Realised too far in that the expected
      result was the seatId and not the
      binary elf format.
      - refactoring coming up

--- aoc2020/src/main/scala/Day05.scala
+++ aoc2020/src/main/scala/Day05.scala
@@ -1,13 +1,64 @@
-import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import aoc.numeric.{AoCPart1, AoCPart2}
 
-object Day05 extends App with AoCPart1Test with AoCPart2Test {
+object Day05 extends App with AoCPart1 with AoCPart2 {
 
-  printResultPart1Test
   printResultPart1
-  printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = strings.map(Boardingpass(_)).map(_.seatId()).max
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    val seats = strings.map(Boardingpass(_))
+    missingSeat(
+      seats
+        .filter(b => b.row != seats.minBy(_.row).row && b.row != seats.maxBy(_.row).row)
+        .groupBy(_.row)
+        .minBy((kv) => kv._2.size)._2)
+      .seatId()
+  }
+
+  def missingSeat(seats: Seq[Boardingpass]): Boardingpass = {
+    val cols = seats.map(_.column).sorted
+    var missing = -1
+    var prev = -1
+    for (i <- cols) {
+      if (prev != i - 1) {
+        missing = i - 1
+      }
+      prev = i
+    }
+    Boardingpass(seats.head.row, missing, "")
+  }
+}
+
+case class Boardingpass(row: Int, column: Int, raw: String) {
+  def seatId(): Long = {
+    row * 8 + column
+  }
+
+  def getId: String = Boardingpass.toElfBinaryRow("0" * 7 + row.toBinaryString takeRight 7) +
+    Boardingpass.toElfBinarySeat("0" * 3 + column.toBinaryString takeRight 3)
+}
+
+object Boardingpass {
+  def apply(string: String): Boardingpass = Boardingpass(
+    Integer.parseInt(fromElfBinary(string.substring(0, 7)), 2),
+    Integer.parseInt(fromElfBinary(string.substring(7)), 2), string)
+
+  def apply(seatId: Int): Boardingpass =
+    Boardingpass((seatId - (seatId % 8)) / 8, (seatId % 8), "")
+
+  private def fromElfBinary(string: String) = string
+    .replace('F', '0')
+    .replace('B', '1')
+    .replace('L', '0')
+    .replace('R', '1')
+
+  private def toElfBinarySeat(string: String) = string
+    .replace('0', 'L')
+    .replace('1', 'R')
+
+  private def toElfBinaryRow(string: String) = string
+    .replace('0', 'F')
+    .replace('1', 'B')
 }
--- aoc2020/src/test/scala/Day05Spec.scala
+++ aoc2020/src/test/scala/Day05Spec.scala
@@ -1,17 +1,29 @@
+import Day05.Boardingpass
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day05Spec extends AnyFlatSpec with Matchers {
-  "Part1 Test" should "be correct" in {
-    Day05.part1TestResult shouldEqual -1
-  }
+
   "Part1" should "be correct" in {
-    Day05.part1Result shouldEqual -1
+    Day05.part1Result shouldEqual 894
   }
-  "Part2 Test" should "be correct" in {
-    Day05.part2TestResult shouldEqual -1
-  }
+
   "Part2" should "be correct" in {
-    Day05.part2Result shouldEqual -1
+    Day05.part2Result shouldEqual 579
+  }
+  "Boardingpasses" should "not be mutative" in {
+    Boardingpass("BBFBFBFLRR").getId shouldEqual "BBFBFBFLRR"
+    Boardingpass("BFFBFFFLRR").getId shouldEqual "BFFBFFFLRR"
+  }
+  "Sanity inputs" should "be valid" in {
+    Boardingpass("BFFFBBFRRR").row shouldEqual 70
+    Boardingpass("BFFFBBFRRR").column shouldEqual 7
+    Boardingpass("BFFFBBFRRR").seatId shouldEqual 567
+    Boardingpass("FFFBBBFRRR").row shouldEqual 14
+    Boardingpass("FFFBBBFRRR").column shouldEqual 7
+    Boardingpass("FFFBBBFRRR").seatId shouldEqual 119
+    Boardingpass("BBFFBBFRLL").row shouldEqual 102
+    Boardingpass("BBFFBBFRLL").column shouldEqual 4
+    Boardingpass("BBFFBBFRLL").seatId shouldEqual 820
   }
 }
