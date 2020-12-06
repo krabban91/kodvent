e5db6ec50b37a4814ad7535059aa3a28e9737a45
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-06 07:01:11 +0100

    :art: cleaning up the answers

--- aoc2020/src/main/scala/Day06.scala
+++ aoc2020/src/main/scala/Day06.scala
@@ -10,23 +10,18 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    val in = fromRaw(strings)
-    val s = in.map(_.replace("\t", "")).map(s => s.toSeq.distinct.size)
-    s.sum
-  }
+  override def part1(strings: Seq[String]): Long = fromRaw(strings)
+    .map(_.replace("\t", ""))
+    .map(_.toSeq.distinct.size)
+    .sum
 
-  override def part2(strings: Seq[String]): Long = {
-    val in = fromRaw(strings)
-    val s = in.map(_.split("\t").filterNot(_.isBlank)).map(s => {
-      val m = mutable.Map[Int, Int]()
-      for (ss <- s) {
-        ss.chars.forEach(i => m(i) = 1 + m.getOrElse(i, 0))
-      }
-      m.values.count(_ == s.length)
-    })
-    s.sum
-  }
+  override def part2(strings: Seq[String]): Long = fromRaw(strings)
+    .map(_.split("\t").filterNot(_.isBlank))
+    .map(respondents => respondents
+      .flatMap(_.toSeq).toSeq.distinct
+      .map(q => respondents.count(_.contains(q)))
+      .count(_ == respondents.length))
+    .sum
 
   def fromRaw(strings: Seq[String]): Seq[String] = {
     var groups = mutable.ListBuffer[String]()
