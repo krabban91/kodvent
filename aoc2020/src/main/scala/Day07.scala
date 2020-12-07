d45e944e85eec973817dd0f030f148998977d91e
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-07 06:55:43 +0100

    :sparkles: completed day 07

--- aoc2020/src/main/scala/Day07.scala
+++ aoc2020/src/main/scala/Day07.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day07 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,68 @@ object Day07 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val bags = strings.map(ColorCodedBag(_))
+    bags.count(canContain(_, bags.find(b => b.name == "shiny gold").get, bags)) - 1
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    val bags = strings.map(ColorCodedBag(_))
+    countBagsRecursive(bags.find(b => b.name == "shiny gold").get, bags) - 1
+  }
+
+  private def canContain(b: ColorCodedBag, shiny: ColorCodedBag, bags: Seq[ColorCodedBag]): Boolean = {
+    val st = mutable.Stack[ColorCodedBag]()
+    st.push(b)
+    while (st.nonEmpty) {
+      val bag = st.pop()
+      if (bag.equals(shiny)) {
+        return true
+      }
+      bag.contains.foreach(bb => {
+        bags.find(i => i.name == bb._1).map(st.push)
+      })
+    }
+    false
+  }
+
+  private def countBags(bag: ColorCodedBag, bags: Seq[ColorCodedBag]): Long = {
+    val uncounted = mutable.Stack[ColorCodedBag]()
+    uncounted.push(bag)
+    var count = 0L
+    while (uncounted.nonEmpty) {
+      val bag = uncounted.pop()
+      count += 1
+      bag.contains.foreach(bb => {
+        bags.find(i => i.name == bb._1).map(i => Range(0, bb._2).foreach(_ => uncounted.push(i)))
+      })
+    }
+    count
+  }
+
+  private def countBagsRecursive(bag: ColorCodedBag, bags: Seq[ColorCodedBag]): Long = 1L + bag.contains
+    .map(bb => bags
+      .find(i => i.name == bb._1)
+      .map(i => bb._2 * countBagsRecursive(i, bags))
+      .getOrElse(0L))
+    .sum
+
+  case class ColorCodedBag(name: String, contains: Map[String, Int])
+
+  object ColorCodedBag {
+    def apply(string: String): ColorCodedBag = {
+      val name = string.split("bags contain")(0).trim.replace("bag", "").replace("bags", "")
+      val c = string.split("bags contain")(1).trim
+      if (c.equals("no other bags.")) {
+        ColorCodedBag(name, Map())
+      } else {
+        ColorCodedBag(name, c.split(",").map(_.trim).map(s => {
+          val ss = s.split("\\s+", 2)
+          ss(1).replace("bags", "").replace("bag", "").replace(".", "").trim -> ss(0).trim.toInt
+        }).toMap)
+      }
+    }
+  }
+
 }
--- aoc2020/src/test/scala/Day07Spec.scala
+++ aoc2020/src/test/scala/Day07Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day07Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day07.part1TestResult shouldEqual -1
+    Day07.part1TestResult shouldEqual 4
   }
   "Part1" should "be correct" in {
-    Day07.part1Result shouldEqual -1
+    Day07.part1Result shouldEqual 265
   }
   "Part2 Test" should "be correct" in {
-    Day07.part2TestResult shouldEqual -1
+    Day07.part2TestResult shouldEqual 32
   }
   "Part2" should "be correct" in {
-    Day07.part2Result shouldEqual -1
+    Day07.part2Result shouldEqual 14177
   }
 }
