c40a53ff68d84421a404212767fd0631c3dc6163
Gabriel Andersson <anderssongabriel665@gmail.com>
2020-12-08 07:18:27 +0100

    :sparkles: year 2020 day 08 (#43)
    * :construction: copied the templates for day 08
    
    * :sparkles: completed day 08
    
    * :art: rewrote solution for day 08
    
    * :mute: remove logging from day 08
    
    Co-authored-by: gandersson <gabriel.andersson@recordedfuture.com>

---
+++ aoc2020/src/main/scala/Day08.scala
@@ -0,0 +1,63 @@
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+
+import scala.collection.mutable
+
+object Day08 extends App with AoCPart1Test with AoCPart2Test {
+
+  override def part1(strings: Seq[String]): Long = {
+    val program = strings.map(Instruction(_))
+    runConsule(program, strict = false).get
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    val input = strings.map(Instruction(_))
+    for (i <- input.indices.filter(i => Seq("nop", "jmp").contains(input(i).operation))) {
+      val program: Seq[Instruction] = withSwappedInstruction(input, i)
+      val result = runConsule(program, strict = true)
+      if(result.isDefined) {
+        return result.get
+      }
+    }
+    -1
+  }
+
+  private def runConsule(program: Seq[Instruction], strict: Boolean): Option[Int] = {
+    var accumulator = 0
+    var pointer = 0
+    val setOps = mutable.Set[Int]()
+    while (!setOps.contains(pointer) && pointer < program.size) {
+      setOps.add(pointer)
+      val op = program(pointer)
+      if (op.operation.equals("acc")) {
+        accumulator += op.value
+        pointer += 1
+      } else if (op.operation.equals("jmp")) {
+        pointer += op.value
+      } else if (op.operation.equals("nop")) {
+        pointer += 1
+      }
+    }
+    (strict, setOps.contains(pointer)) match {
+      case (true, true) => None
+      case _ => Option(accumulator)
+    }
+  }
+
+  private def withSwappedInstruction(input: Seq[Instruction], index: Int): Seq[Instruction] = {
+    val program = mutable.ListBuffer.from(input)
+    program.update(index,
+      if (input(index).operation.equals("nop")) Instruction("jmp", input(index).value)
+      else Instruction("nop", input(index).value))
+    program.toSeq
+  }
+
+  case class Instruction(operation: String, value: Int)
+
+  object Instruction {
+    def apply(string: String): Instruction = {
+      val pair = string.split("\\s+")
+      Instruction(pair(0), pair(1).toInt)
+    }
+  }
+
+}
---
+++ aoc2020/src/test/scala/Day08Spec.scala
@@ -0,0 +1,17 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day08Spec extends AnyFlatSpec with Matchers {
+  "Part1 Test" should "be correct" in {
+    Day08.part1TestResult shouldEqual 5
+  }
+  "Part1" should "be correct" in {
+    Day08.part1Result shouldEqual 1614
+  }
+  "Part2 Test" should "be correct" in {
+    Day08.part2TestResult shouldEqual 8
+  }
+  "Part2" should "be correct" in {
+    Day08.part2Result shouldEqual 1260
+  }
+}
