9c942f12bccfd3fe6998dca616784ceec13d940d
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-09 07:14:35 +0100

    :mute: remove logging from day 09

--- aoc2020/src/main/scala/Day09.scala
+++ aoc2020/src/main/scala/Day09.scala
@@ -4,11 +4,6 @@ import scala.collection.mutable
 
 object Day09 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     val preambleSize = if (strings.size == 20) 5 else 25 // test input is smaller
     val all = strings.map(_.toLong)
