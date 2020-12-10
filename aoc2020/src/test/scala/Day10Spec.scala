8f86052bedc8c4090d3b6c71fa9f65e296a3428b
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-10 07:49:32 +0100

    added extra test cases for the bonus example

--- aoc2020/src/test/scala/Day10Spec.scala
+++ aoc2020/src/test/scala/Day10Spec.scala
@@ -8,10 +8,16 @@ class Day10Spec extends AnyFlatSpec with Matchers {
   "Part1" should "be correct" in {
     Day10.part1Result shouldEqual 1856
   }
+  "Part1 bonus input" should "be correct" in {
+    Day10.part1(Day10.read("day10/bonus.txt")) shouldEqual 220
+  }
   "Part2 Test" should "be correct" in {
     Day10.part2TestResult shouldEqual 8
   }
   "Part2" should "be correct" in {
     Day10.part2Result shouldEqual 2314037239808L
+  }
+  "Part2 bonus input" should "be correct" in {
+    Day10.part2(Day10.read("day10/bonus.txt")) shouldEqual 19208
   }
 }
