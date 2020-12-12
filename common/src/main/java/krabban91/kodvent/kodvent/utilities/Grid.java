162972761baf93b511ea978a121e71d9653f2073
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-12 08:07:08 +0100

    removed depricated usage of Grid<>

--- common/src/main/java/krabban91/kodvent/kodvent/utilities/Grid.java
+++ common/src/main/java/krabban91/kodvent/kodvent/utilities/Grid.java
@@ -17,11 +17,7 @@ import java.util.stream.IntStream;
 
 public class Grid<V> {
 
-    private List<? extends List<V>> raw;
-
-    public Grid() {
-
-    }
+    private final List<? extends List<V>> raw;
 
     public Grid(List<? extends List<V>> grid) {
         this.raw = grid;
@@ -43,18 +39,6 @@ public class Grid<V> {
                         .collect(Collectors.toSet()))
                 .flatMap(Set::stream)
                 .collect(Collectors.toSet());
-    }
-
-    public List<V> getSurroundingTiles(int row, int col, List<? extends List<V>> item) {
-        return IntStream
-                .rangeClosed(Math.max(row - 1, 0), Math.min(row + 1, item.size() - 1))
-                .mapToObj(i -> IntStream
-                        .rangeClosed(Math.max(col - 1, 0), Math.min(col + 1, item.get(row).size() - 1))
-                        .mapToObj(j -> (i == row && j == col) ? null : item.get(i).get(j))
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toList()))
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
     }
 
     public List<V> getSurroundingTiles(int row, int col) {
@@ -96,12 +80,6 @@ public class Grid<V> {
     public void forEachRangeClosed(int x0, int x1, int y0, int y1, BiConsumer<Integer, Integer> action) {
         IntStream.rangeClosed(y0, y1)
                 .forEach(row -> IntStream.rangeClosed(x0, x1)
-                        .forEach(col -> action.accept(col, row)));
-    }
-
-    public void forEachRanged(List<? extends List<V>> grid, BiConsumer<Integer, Integer> action) {
-        IntStream.range(0, grid.size())
-                .forEach(row -> IntStream.range(0, grid.get(0).size())
                         .forEach(col -> action.accept(col, row)));
     }
 
