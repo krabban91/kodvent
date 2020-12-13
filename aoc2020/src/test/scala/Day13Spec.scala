5105fc7ddd6ffb2d32c52297028e548f6741336a
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-13 09:24:00 +0100

    :sparkles: completed day 13 part 2
    * The problem was too hard for me to not
      use google. When i was informed that
      it is the CRT (Chinese Remainder Theorem)
      i had to try again.
      - Eventually I
        found a way to iteratively find the
        frequency that buses conincide.

--- aoc2020/src/test/scala/Day13Spec.scala
+++ aoc2020/src/test/scala/Day13Spec.scala
@@ -9,9 +9,17 @@ class Day13Spec extends AnyFlatSpec with Matchers {
     Day13.part1Result shouldEqual 4808
   }
   "Part2 Test" should "be correct" in {
-    Day13.part2TestResult shouldEqual -1
+    Day13.part2TestResult shouldEqual 1068781
   }
   "Part2" should "be correct" in {
-    Day13.part2Result shouldEqual -1
+    Day13.part2Result shouldEqual 741745043105674L
+  }
+
+  "Part2 bonus input" should "be correct" in {
+    Day13.part2(Seq("1","17,x,13,19")) shouldEqual 3417
+    Day13.part2(Seq("1","67,7,59,61")) shouldEqual 754018
+    Day13.part2(Seq("1","67,x,7,59,61")) shouldEqual 779210
+    Day13.part2(Seq("1","67,7,x,59,61")) shouldEqual 1261476
+    Day13.part2(Seq("1","1789,37,47,1889")) shouldEqual 1202161486
   }
 }
