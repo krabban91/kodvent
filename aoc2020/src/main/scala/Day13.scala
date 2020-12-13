e6208664b103a5ede4b092764afb199f7d88a19c
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-13 06:15:18 +0100

    :sparkles: completed day 13 part 1

--- aoc2020/src/main/scala/Day13.scala
+++ aoc2020/src/main/scala/Day13.scala
@@ -7,7 +7,21 @@ object Day13 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val start = strings.head.toInt
+    var time = start
+    val buses = strings(1).split(",").filterNot(_ == "x").map(_.toInt).map(Bus)
+    while(!buses.exists(_.departs(time))){
+      time += 1
+    }
+    buses.find(_.departs(time)).get.id * (time - start)
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  case class Bus(id: Int){
+
+  def departs(time: Int):Boolean = time%id == 0
+  }
+
 }
--- aoc2020/src/test/scala/Day13Spec.scala
+++ aoc2020/src/test/scala/Day13Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day13Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day13.part1TestResult shouldEqual -1
+    Day13.part1TestResult shouldEqual 295
   }
   "Part1" should "be correct" in {
-    Day13.part1Result shouldEqual -1
+    Day13.part1Result shouldEqual 4808
   }
   "Part2 Test" should "be correct" in {
     Day13.part2TestResult shouldEqual -1
