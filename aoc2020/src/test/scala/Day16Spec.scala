e984e169b992e7306a870ecbb16fe4c3fb022e7d
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-16 08:24:30 +0100

    added test cases for provided example

--- aoc2020/src/test/scala/Day16Spec.scala
+++ aoc2020/src/test/scala/Day16Spec.scala
@@ -11,4 +11,32 @@ class Day16Spec extends AnyFlatSpec with Matchers {
   "Part2" should "be correct" in {
     Day16.part2Result shouldEqual 589685618167L
   }
+
+  val input: Array[String] = "class: 0-1 or 4-19\nrow: 0-5 or 8-19\nseat: 0-13 or 16-19\n\nyour ticket:\n11,12,13\n\nnearby tickets:\n3,9,18\n15,1,5\n5,14,9".split("\n")
+
+  "input" should "parse data correctly" in {
+    val (r, y, t) = Day16.input(input)
+    r.size shouldEqual 3
+    r.head shouldEqual Day16.Rule("class", Seq((0, 1), (4, 19)))
+    y shouldEqual Seq(11, 12, 13)
+    t.size shouldEqual 3
+    t.head shouldEqual Seq(3, 9, 18)
+  }
+  "matchingColumns" should "produce a list of possible rule indices" in {
+    val (r, y, t) = Day16.input(input)
+    val matching = Day16.matchingColumns(r, t)
+    matching(r.head) shouldEqual Seq(1, 2)
+    matching(r(1)) shouldEqual Seq(0, 1, 2)
+    matching(r(2)) shouldEqual Seq(2)
+  }
+
+  "viableMatching" should "produce a rule to column mapping" in {
+    val (r, y, t) = Day16.input(input)
+    val viable = Day16.viableMatching(Day16.matchingColumns(r, t))
+    viable.isDefined shouldEqual true
+    val v = viable.get
+    v(r.head) shouldEqual 1
+    v(r(1)) shouldEqual 0
+    v(r(2)) shouldEqual 2
+  }
 }
