9970feb5b884815d9f01a16d2abebb7c37e7e3c8
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-17 08:10:02 +0100

    :sparkles: solved day 17 part 1

--- aoc2020/src/main/scala/Day17.scala
+++ aoc2020/src/main/scala/Day17.scala
@@ -1,13 +1,64 @@
+import java.util.stream.Collectors
+
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.logging.{LogUtils, Loggable}
+import krabban91.kodvent.kodvent.utilities.{CubeGrid, Point3D}
+
+import scala.jdk.CollectionConverters._
 
 object Day17 extends App with AoCPart1Test with AoCPart2Test {
+
+  val debug = false
 
   printResultPart1Test
   printResultPart1
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    var curr: CubeGrid[Conway] = new CubeGrid[Conway](java.util.List.of(strings.map(s => s.chars()
+      .mapToObj(c => Conway(c == '#'))
+      .collect(Collectors.toList[Conway]))
+      .asJava))
+    for (i <- Range(0, 6)) {
+      if (debug) {
+        println(s"iteration: $i.")
+        logCube(curr)
+      }
+      curr = curr.expandOneStep(Conway(_))
+      curr = curr.map((c, p) => c.cycle(curr.getSurroundingTiles(p).asScala))
+    }
+    curr.sum(c => if (c.active) 1 else 0)
+  }
 
-  override def part2(strings: Seq[String]): Long = -1
+  private def logCube(next: CubeGrid[Day17.Conway]): Unit = {
+    for (z <- Range(0, next.depth())) {
+      println(s"- z=$z")
+      println(LogUtils.tiles(next.getRaw.get(z)))
+    }
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    -1
+  }
+
+  def generator(point: Point3D): Conway = {
+    Conway(false)
+  }
+
+
+  case class Conway(active: Boolean) extends Loggable {
+    def cycle(neighbors: Iterable[Conway]): Conway = {
+      val count = neighbors
+        .count(t => t.active)
+      if (active) Conway(count == 2 || count == 3) else Conway(count == 3)
+    }
+
+    override def showTile(): _root_.java.lang.String = if (active) "#" else "."
+  }
+
+  object Conway {
+    def apply(point: Point3D): Conway = Conway(false)
+  }
+
 }
---
+++ common/src/main/java/krabban91/kodvent/kodvent/utilities/CubeGrid.java
@@ -0,0 +1,118 @@
+package krabban91.kodvent.kodvent.utilities;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CubeGrid<V> {
+
+    private final List<? extends List<? extends List<V>>> raw;
+
+    public CubeGrid(List<? extends List<? extends List<V>>> cubeGrid) {
+        this.raw = cubeGrid;
+    }
+
+    public int depth() {
+        return this.raw.size();
+    }
+    public int height() {
+        return this.raw.get(0).size();
+    }
+
+    public int width() {
+        return this.raw.get(0).get(0).size();
+    }
+
+
+    public List<V> getSurroundingTiles(Point3D p) {
+        return getNearestTilesOfSurroundingDirections(p, v -> true);
+    }
+
+    public List<V> getNearestTilesOfSurroundingDirections(Point3D from, Predicate<V> predicate) {
+        return getNearestTilesOfSurroundingDirections(from.getX(), from.getY(), from.getZ(), predicate);
+    }
+
+    public List<V> getNearestTilesOfSurroundingDirections(int x, int y, int z, Predicate<V> predicate) {
+        List<Point> directions = List.of(new Point(0, 0), new Point(0, -1), new Point(-1, -1), new Point(-1, 0), new Point(-1, 1), new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(1, -1));
+        List<Point3D> allDirections = directions.stream().map(p -> List.of(new Point3D(p.x, p.y, -1), new Point3D(p.x, p.y, 0), new Point3D(p.x, p.y, 1))).flatMap(List::stream).filter(p-> !p.equals(new Point3D(0,0,0))).collect(Collectors.toList());
+        return allDirections.stream()
+                .map(p -> getNearestInDirection(x, y, z, p, predicate))
+                .flatMap(Optional::stream)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<V> getNearestInDirection(int x, int y, int z, Point3D direction, Predicate<V> predicate) {
+        int dx = x + direction.getX();
+        int dy = y + direction.getY();
+        int dz = z + direction.getZ();
+        Optional<V> tile = this.get(dx, dy, dz);
+        while (tile.isPresent() && !predicate.test(tile.get())) {
+            dx += direction.getX();
+            dy += direction.getY();
+            dz += direction.getZ();
+
+            tile = this.get(dx, dy, dz);
+        }
+        return tile;
+    }
+
+    public CubeGrid<V> map(BiFunction<V, Point3D, V> function) {
+        return new CubeGrid<>(IntStream.range(0, raw.size())
+                .mapToObj(z -> IntStream.range(0, raw.get(0).size())
+                        .mapToObj(y -> IntStream.range(0, raw.get(z).get(y).size())
+                                .mapToObj(x -> function.apply(this.get(x,y,z).get(), new Point3D(x, y, z)))
+                                .collect(Collectors.toList()))
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList()));
+    }
+
+    public CubeGrid<V> expandOneStep(Function<Point3D, V> generator) {
+        return new CubeGrid<>(IntStream.range(-1, raw.size()+1)
+                .mapToObj(z -> IntStream.range(-1, raw.get(0).size()+1)
+                        .mapToObj(y -> IntStream.range(-1, raw.get(0).get(0).size()+1)
+                                .mapToObj(x -> this.get(x,y, z).orElseGet(() -> generator.apply(new Point3D(x, y, z))))
+                                .collect(Collectors.toList()))
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList()));
+    }
+
+    public Optional<V> get(Point3D p) {
+        return this.get(p.getX(), p.getY(), p.getZ());
+    }
+
+    public Optional<V> get(int x, int y, int z) {
+        return (x < 0 || x >= width() || y < 0 || y >= height() || z < 0 || z >= depth()) ?
+                Optional.empty() :
+                Optional.of(this.raw.get(z).get(y).get(x));
+    }
+
+    public long sum(ToLongFunction<V> valueMethod) {
+        return this.raw.stream().mapToLong(ll -> ll.stream().mapToLong(l -> l.stream().mapToLong(valueMethod).sum()).sum()).sum();
+    }
+
+    public List<? extends List<? extends List<V>>> getRaw() {
+        return raw;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CubeGrid<?> grid = (CubeGrid<?>) o;
+
+        return raw.equals(grid.raw);
+    }
+
+    @Override
+    public int hashCode() {
+        return raw.hashCode();
+    }
+
+}
