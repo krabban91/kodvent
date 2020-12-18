6384472c830af29d4cb52a0ae5526d3b2955dfa6
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-18 08:04:53 +0100

    :sparkles: completed day 18 part 2

--- aoc2020/src/main/scala/Day18.scala
+++ aoc2020/src/main/scala/Day18.scala
@@ -13,7 +13,8 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val in = strings.map(s => Expression(s, false))
+    in.map(_.evaluate).sum
   }
 
   trait Expression{
@@ -33,51 +34,87 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   case class Parentesis(expr: Expression) extends Expression{
     override def evaluate: Long = expr.evaluate
   }
-  object Expression{
-    def apply(string: String, rightToLeft: Boolean): Expression = {
-      if (rightToLeft){
-        val rev = string.reverse
-        val m = string.reverse.indexOf("*")
-        val a = string.reverse.indexOf("+")
-        val pr = string.reverse.indexOf(")")
-        (m,a,pr) match{
-          case (-1,-1,-1) => Value(string.trim.toLong)
-          case (_,_,v3) if v3 == 0 => {
-            var count = 0
-            var indx = -1
-            for(i <- Range(0,string.length).reverse){
-              if(string.charAt(i) == ')'){
-                count +=1
-              }
-              if(string.charAt(i) == '('){
-                count -=1
-              }
-              if(indx == -1 && count == 0){
-                indx = i
-              }
-            }
-            val r = Parentesis(Expression(string.substring(indx+ 1, string.length - 1).trim, true))
-            if(indx == 0){
-              r
-            } else {
-              string.charAt(indx-2) match {
-                case '+' => Addition(Expression(string.substring(0, indx - 2).trim, true), r)
-                case '*' => Multiplication(Expression(string.substring(0, indx - 2).trim, true), r)
-                case _ =>
-                  println("missed again")
-                  Expression(string, true)
-              }
-            }
 
-          }
-          case (v1,v2,_) if v1 >= 0 && (v2 < 0 || v1 < v2) => Multiplication(Expression(string.reverse.split("\\*", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\*", 2)(1).reverse.trim, true))
-          case (v1,v2,_) if v1 < 0 || v2 < v1 => Addition(Expression(string.reverse.split("\\+", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\+", 2)(1).reverse.trim, true))
-          case _ =>
-            println("missed one")
-            Expression(string, true)
-        }
+  object Expression{
+    def apply(string: String, rightToLefts: Boolean): Expression = {
+      if (rightToLefts){
+        rightToLeft(string)
       } else {
-        Expression(string, true)
+        addBeforeMul(string)
+      }
+    }
+
+    private def addBeforeMul(string: String) = {
+      val rev = string.reverse
+      val m = string.reverse.indexOf("*")
+      val a = string.reverse.indexOf("+")
+      val pr = string.reverse.indexOf(")")
+      var counta = 0
+
+      val accessible = Range(0, string.length).map(i => {
+        if (string.charAt(i) == '(') {
+          counta += 1
+        }
+        if (string.charAt(i) == ')') {
+          counta -= 1
+        }
+        counta
+      })
+      val reachable = string.indices.filter(i => accessible(i) == 0 && string.charAt(i) != ')')
+      val muls = reachable.filter(i => accessible(i) == 0 && string.charAt(i) == '*')
+      val adds = reachable.filter(i => accessible(i) == 0 && string.charAt(i) == '+')
+      if (muls.nonEmpty) {
+        Multiplication(Expression(string.substring(0, muls.head).trim, false), Expression(string.substring(muls.head + 1).trim, false))
+      } else if (adds.nonEmpty) {
+        Addition(Expression(string.substring(0, adds.head).trim, false), Expression(string.substring(adds.head + 1).trim, false))
+      } else if (reachable.nonEmpty) {
+        Value(string.toLong)
+      } else {
+        Parentesis(Expression(string.substring(0 + 1, string.length - 1).trim, false))
+      }
+    }
+
+    private def rightToLeft(string: String) = {
+      val rev = string.reverse
+      val m = string.reverse.indexOf("*")
+      val a = string.reverse.indexOf("+")
+      val pr = string.reverse.indexOf(")")
+      val pl = string.indexOf("(")
+      (m, a, pr) match {
+        case (-1, -1, -1) => Value(string.trim.toLong)
+        case (_, _, v3) if v3 == 0 => {
+          var count = 0
+          var indx = -1
+          for (i <- Range(0, string.length).reverse) {
+            if (string.charAt(i) == ')') {
+              count += 1
+            }
+            if (string.charAt(i) == '(') {
+              count -= 1
+            }
+            if (indx == -1 && count == 0) {
+              indx = i
+            }
+          }
+          val r = Parentesis(Expression(string.substring(indx + 1, string.length - 1).trim, true))
+          if (indx == 0) {
+            r
+          } else {
+            string.charAt(indx - 2) match {
+              case '*' => Multiplication(Expression(string.substring(0, indx - 2).trim, true), r)
+              case '+' => Addition(Expression(string.substring(0, indx - 2).trim, true), r)
+              case _ =>
+                println("missed again")
+                Value(-1)
+            }
+          }
+
+        }
+        case (v1, v2, _) if v1 >= 0 && (v2 < 0 || v1 < v2) => Multiplication(Expression(string.reverse.split("\\*", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\*", 2)(1).reverse.trim, true))
+        case (v1, v2, _) if v1 < 0 || v2 < v1 => Addition(Expression(string.reverse.split("\\+", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\+", 2)(1).reverse.trim, true))
+        case _ =>
+          println("missed one")
+          Expression(string, true)
       }
     }
   }
--- aoc2020/src/test/scala/Day18Spec.scala
+++ aoc2020/src/test/scala/Day18Spec.scala
@@ -9,10 +9,10 @@ class Day18Spec extends AnyFlatSpec with Matchers {
     Day18.part1Result shouldEqual 701339185745L
   }
   "Part2 Test" should "be correct" in {
-    Day18.part2TestResult shouldEqual -1
+    Day18.part2TestResult shouldEqual 231
   }
   "Part2" should "be correct" in {
-    Day18.part2Result shouldEqual -1
+    Day18.part2Result shouldEqual 4208490449905L
   }
 
   "Expressions rightToLeft" should "be evaluated correctly" in {
@@ -23,5 +23,17 @@ class Day18Spec extends AnyFlatSpec with Matchers {
     Day18.Expression("5 + (8 * 3 + 9 + 3 * 4 * 3)", true).evaluate shouldEqual 437L
     Day18.Expression("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true).evaluate shouldEqual 12240L
     Day18.Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true).evaluate shouldEqual 13632L
+  }
+  "Expressions addBeforeMul" should "be evaluated correctly" in {
+    Day18.Expression("1 + 2 * 3 + 4 * 5 + 6", false).evaluate shouldEqual  231L
+    val expressions = Day18.Expression("1 + (2 * 3) + (4 * (5 + 6))", false)
+    expressions.evaluate shouldEqual 51L
+    val expression1 = Day18.Expression("2 * 3 + (4 * 5)", false)
+    expression1.evaluate shouldEqual 46L
+    Day18.Expression("5 + (8 * 3 + 9 + 3 * 4 * 3)", false).evaluate shouldEqual 1445
+    Day18.Expression("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", false).evaluate shouldEqual 669060
+    Day18.Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", false).evaluate shouldEqual 23340
+    val expression = Day18.Expression("2 * 4 + 2 + ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6)", false)
+    expression.evaluate shouldEqual 23340
   }
 }
