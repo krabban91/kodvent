cfc488cdb81767d913185c9014fe9828470fe033
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-18 07:08:26 +0100

    :sparkles: completed day 18 part 1

--- aoc2020/src/main/scala/Day18.scala
+++ aoc2020/src/main/scala/Day18.scala
@@ -7,7 +7,78 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val in = strings.map(s => Expression(s, true))
+    in.map(_.evaluate).sum
+  }
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    -1
+  }
+
+  trait Expression{
+    def evaluate: Long
+  }
+
+  case class Value(v: Long) extends Expression{
+    override def evaluate: Long = v
+  }
+  case class Multiplication(l: Expression, r: Expression) extends Expression{
+    override def evaluate: Long = l.evaluate*r.evaluate
+  }
+  case class Addition(l: Expression, r: Expression) extends Expression{
+    override def evaluate: Long = l.evaluate+r.evaluate
+
+  }
+  case class Parentesis(expr: Expression) extends Expression{
+    override def evaluate: Long = expr.evaluate
+  }
+  object Expression{
+    def apply(string: String, rightToLeft: Boolean): Expression = {
+      if (rightToLeft){
+        val rev = string.reverse
+        val m = string.reverse.indexOf("*")
+        val a = string.reverse.indexOf("+")
+        val pr = string.reverse.indexOf(")")
+        (m,a,pr) match{
+          case (-1,-1,-1) => Value(string.trim.toLong)
+          case (_,_,v3) if v3 == 0 => {
+            var count = 0
+            var indx = -1
+            for(i <- Range(0,string.length).reverse){
+              if(string.charAt(i) == ')'){
+                count +=1
+              }
+              if(string.charAt(i) == '('){
+                count -=1
+              }
+              if(indx == -1 && count == 0){
+                indx = i
+              }
+            }
+            val r = Parentesis(Expression(string.substring(indx+ 1, string.length - 1).trim, true))
+            if(indx == 0){
+              r
+            } else {
+              string.charAt(indx-2) match {
+                case '+' => Addition(Expression(string.substring(0, indx - 2).trim, true), r)
+                case '*' => Multiplication(Expression(string.substring(0, indx - 2).trim, true), r)
+                case _ =>
+                  println("missed again")
+                  Expression(string, true)
+              }
+            }
+
+          }
+          case (v1,v2,_) if v1 >= 0 && (v2 < 0 || v1 < v2) => Multiplication(Expression(string.reverse.split("\\*", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\*", 2)(1).reverse.trim, true))
+          case (v1,v2,_) if v1 < 0 || v2 < v1 => Addition(Expression(string.reverse.split("\\+", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\+", 2)(1).reverse.trim, true))
+          case _ =>
+            println("missed one")
+            Expression(string, true)
+        }
+      } else {
+        Expression(string, true)
+      }
+    }
+  }
 }
--- aoc2020/src/test/scala/Day18Spec.scala
+++ aoc2020/src/test/scala/Day18Spec.scala
@@ -3,15 +3,25 @@ import org.scalatest.matchers.should.Matchers
 
 class Day18Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day18.part1TestResult shouldEqual -1
+    Day18.part1TestResult shouldEqual 71
   }
   "Part1" should "be correct" in {
-    Day18.part1Result shouldEqual -1
+    Day18.part1Result shouldEqual 701339185745L
   }
   "Part2 Test" should "be correct" in {
     Day18.part2TestResult shouldEqual -1
   }
   "Part2" should "be correct" in {
     Day18.part2Result shouldEqual -1
+  }
+
+  "Expressions rightToLeft" should "be evaluated correctly" in {
+    Day18.Expression("1 + 2 * 3 + 4 * 5 + 6", true).evaluate shouldEqual  71L
+    val expression = Day18.Expression("1 + (2 * 3) + (4 * (5 + 6))", true)
+    expression.evaluate shouldEqual 51L
+    Day18.Expression("2 * 3 + (4 * 5)", true).evaluate shouldEqual 26L
+    Day18.Expression("5 + (8 * 3 + 9 + 3 * 4 * 3)", true).evaluate shouldEqual 437L
+    Day18.Expression("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true).evaluate shouldEqual 12240L
+    Day18.Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true).evaluate shouldEqual 13632L
   }
 }
