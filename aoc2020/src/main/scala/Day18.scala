07e0028a3219a045f9706bd1843073b3cbcad50b
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-18 10:35:46 +0100

    :art: cleaned up code in day 18
    * I tried doing it with case matching in the beginning
      but failed terribly. When the result was in it was
      a lot easier to clean up.

--- aoc2020/src/main/scala/Day18.scala
+++ aoc2020/src/main/scala/Day18.scala
@@ -7,115 +7,76 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    val in = strings.map(s => Expression(s, true))
-    in.map(_.evaluate).sum
-  }
+  override def part1(strings: Seq[String]): Long = strings.map(Expression.part1).map(_.evaluate).sum
 
-  override def part2(strings: Seq[String]): Long = {
-    val in = strings.map(s => Expression(s, false))
-    in.map(_.evaluate).sum
-  }
+  override def part2(strings: Seq[String]): Long = strings.map(Expression.part2).map(_.evaluate).sum
 
-  trait Expression{
+  trait Expression {
     def evaluate: Long
   }
 
-  case class Value(v: Long) extends Expression{
+  case class Value(v: Long) extends Expression {
     override def evaluate: Long = v
   }
-  case class Multiplication(l: Expression, r: Expression) extends Expression{
-    override def evaluate: Long = l.evaluate*r.evaluate
+
+  case class Multiplication(l: Expression, r: Expression) extends Expression {
+    override def evaluate: Long = l.evaluate * r.evaluate
   }
-  case class Addition(l: Expression, r: Expression) extends Expression{
-    override def evaluate: Long = l.evaluate+r.evaluate
+
+  case class Addition(l: Expression, r: Expression) extends Expression {
+    override def evaluate: Long = l.evaluate + r.evaluate
 
   }
-  case class Parentesis(expr: Expression) extends Expression{
+
+  case class Parentesis(expr: Expression) extends Expression {
     override def evaluate: Long = expr.evaluate
   }
 
-  object Expression{
-    def apply(string: String, rightToLefts: Boolean): Expression = {
-      if (rightToLefts){
-        rightToLeft(string)
-      } else {
-        addBeforeMul(string)
+  object Expression {
+    def part1(string: String): Expression = {
+      val levels = useableIndices(string)
+      string.indices.filter(i => levels(i) == 0 && string.charAt(i) != ')') match {
+        case Seq() => Parentesis(part1(string.substring(0 + 1, string.length - 1).trim))
+        case reachable => reachable.filter(i => levels(i) == 0 && Seq('*', '+').contains(string.charAt(i))).reverse match {
+          case op +: _ => string(op) match {
+            case '+' => Addition(
+              part1(string.substring(0, op).trim),
+              part1(string.substring(op + 1).trim))
+            case '*' => Multiplication(
+              part1(string.substring(0, op).trim),
+              part1(string.substring(op + 1).trim))
+            case _ => Value(-1)
+          }
+          case _ => Value(string.toLong)
+        }
       }
     }
 
-    private def addBeforeMul(string: String) = {
-      val rev = string.reverse
-      val m = string.reverse.indexOf("*")
-      val a = string.reverse.indexOf("+")
-      val pr = string.reverse.indexOf(")")
-      var counta = 0
+    def part2(string: String): Expression = {
+      val levels = useableIndices(string)
+      string.indices.filter(i => levels(i) == 0 && string.charAt(i) != ')') match {
+        case Seq() => Parentesis(part2(string.substring(0 + 1, string.length - 1).trim))
+        case reachable => reachable.filter(i => levels(i) == 0 && string.charAt(i) == '*') match {
+          case m +: _ => Multiplication(
+            part2(string.substring(0, m).trim),
+            part2(string.substring(m + 1).trim))
+          case _ => reachable.filter(i => levels(i) == 0 && string.charAt(i) == '+') match {
+            case a +: _ => Addition(
+              part2(string.substring(0, a).trim),
+              part2(string.substring(a + 1).trim))
+            case _ => Value(string.toLong)
+          }
+        }
+      }
+    }
 
-      val accessible = Range(0, string.length).map(i => {
-        if (string.charAt(i) == '(') {
-          counta += 1
-        }
-        if (string.charAt(i) == ')') {
-          counta -= 1
-        }
-        counta
+    private def useableIndices(string: String): Seq[Int] = {
+      var level: Int = 0
+      Range(0, string.length).map(i => {
+        level += (if (string.charAt(i) == '(') 1 else if (string.charAt(i) == ')') -1 else 0)
+        level
       })
-      val reachable = string.indices.filter(i => accessible(i) == 0 && string.charAt(i) != ')')
-      val muls = reachable.filter(i => accessible(i) == 0 && string.charAt(i) == '*')
-      val adds = reachable.filter(i => accessible(i) == 0 && string.charAt(i) == '+')
-      if (muls.nonEmpty) {
-        Multiplication(Expression(string.substring(0, muls.head).trim, false), Expression(string.substring(muls.head + 1).trim, false))
-      } else if (adds.nonEmpty) {
-        Addition(Expression(string.substring(0, adds.head).trim, false), Expression(string.substring(adds.head + 1).trim, false))
-      } else if (reachable.nonEmpty) {
-        Value(string.toLong)
-      } else {
-        Parentesis(Expression(string.substring(0 + 1, string.length - 1).trim, false))
-      }
-    }
-
-    private def rightToLeft(string: String) = {
-      val rev = string.reverse
-      val m = string.reverse.indexOf("*")
-      val a = string.reverse.indexOf("+")
-      val pr = string.reverse.indexOf(")")
-      val pl = string.indexOf("(")
-      (m, a, pr) match {
-        case (-1, -1, -1) => Value(string.trim.toLong)
-        case (_, _, v3) if v3 == 0 => {
-          var count = 0
-          var indx = -1
-          for (i <- Range(0, string.length).reverse) {
-            if (string.charAt(i) == ')') {
-              count += 1
-            }
-            if (string.charAt(i) == '(') {
-              count -= 1
-            }
-            if (indx == -1 && count == 0) {
-              indx = i
-            }
-          }
-          val r = Parentesis(Expression(string.substring(indx + 1, string.length - 1).trim, true))
-          if (indx == 0) {
-            r
-          } else {
-            string.charAt(indx - 2) match {
-              case '*' => Multiplication(Expression(string.substring(0, indx - 2).trim, true), r)
-              case '+' => Addition(Expression(string.substring(0, indx - 2).trim, true), r)
-              case _ =>
-                println("missed again")
-                Value(-1)
-            }
-          }
-
-        }
-        case (v1, v2, _) if v1 >= 0 && (v2 < 0 || v1 < v2) => Multiplication(Expression(string.reverse.split("\\*", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\*", 2)(1).reverse.trim, true))
-        case (v1, v2, _) if v1 < 0 || v2 < v1 => Addition(Expression(string.reverse.split("\\+", 2)(0).reverse.trim, true), Expression(string.reverse.split("\\+", 2)(1).reverse.trim, true))
-        case _ =>
-          println("missed one")
-          Expression(string, true)
-      }
     }
   }
+
 }
--- aoc2020/src/test/scala/Day18Spec.scala
+++ aoc2020/src/test/scala/Day18Spec.scala
@@ -15,25 +15,25 @@ class Day18Spec extends AnyFlatSpec with Matchers {
     Day18.part2Result shouldEqual 4208490449905L
   }
 
-  "Expressions rightToLeft" should "be evaluated correctly" in {
-    Day18.Expression("1 + 2 * 3 + 4 * 5 + 6", true).evaluate shouldEqual  71L
-    val expression = Day18.Expression("1 + (2 * 3) + (4 * (5 + 6))", true)
+  "Expressions part1" should "be evaluated correctly" in {
+    Day18.Expression.part1("1 + 2 * 3 + 4 * 5 + 6").evaluate shouldEqual  71L
+    val expression = Day18.Expression.part1("1 + (2 * 3) + (4 * (5 + 6))")
     expression.evaluate shouldEqual 51L
-    Day18.Expression("2 * 3 + (4 * 5)", true).evaluate shouldEqual 26L
-    Day18.Expression("5 + (8 * 3 + 9 + 3 * 4 * 3)", true).evaluate shouldEqual 437L
-    Day18.Expression("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true).evaluate shouldEqual 12240L
-    Day18.Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true).evaluate shouldEqual 13632L
+    Day18.Expression.part1("2 * 3 + (4 * 5)").evaluate shouldEqual 26L
+    Day18.Expression.part1("5 + (8 * 3 + 9 + 3 * 4 * 3)").evaluate shouldEqual 437L
+    Day18.Expression.part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").evaluate shouldEqual 12240L
+    Day18.Expression.part1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").evaluate shouldEqual 13632L
   }
-  "Expressions addBeforeMul" should "be evaluated correctly" in {
-    Day18.Expression("1 + 2 * 3 + 4 * 5 + 6", false).evaluate shouldEqual  231L
-    val expressions = Day18.Expression("1 + (2 * 3) + (4 * (5 + 6))", false)
+  "Expressions part2" should "be evaluated correctly" in {
+    Day18.Expression.part2("1 + 2 * 3 + 4 * 5 + 6").evaluate shouldEqual  231L
+    val expressions = Day18.Expression.part2("1 + (2 * 3) + (4 * (5 + 6))")
     expressions.evaluate shouldEqual 51L
-    val expression1 = Day18.Expression("2 * 3 + (4 * 5)", false)
+    val expression1 = Day18.Expression.part2("2 * 3 + (4 * 5)")
     expression1.evaluate shouldEqual 46L
-    Day18.Expression("5 + (8 * 3 + 9 + 3 * 4 * 3)", false).evaluate shouldEqual 1445
-    Day18.Expression("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", false).evaluate shouldEqual 669060
-    Day18.Expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", false).evaluate shouldEqual 23340
-    val expression = Day18.Expression("2 * 4 + 2 + ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6)", false)
+    Day18.Expression.part2("5 + (8 * 3 + 9 + 3 * 4 * 3)").evaluate shouldEqual 1445
+    Day18.Expression.part2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").evaluate shouldEqual 669060
+    Day18.Expression.part2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").evaluate shouldEqual 23340
+    val expression = Day18.Expression.part2("2 * 4 + 2 + ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6)")
     expression.evaluate shouldEqual 23340
   }
 }
