a5427598a57e618dd6c8c84eb58cae0d5d8e3ad8
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-19 07:57:00 +0100

    :sparkles: completed day 19 part 1

--- aoc2020/src/main/scala/Day19.scala
+++ aoc2020/src/main/scala/Day19.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day19 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,64 @@ object Day19 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val (ruleMap, messages) = initialData(strings)
+    messages.count(s => ruleMap(0).fits(s, ruleMap))
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  def initialData(strings: Seq[String]): (Map[Int, Rule], Seq[String]) = {
+    val spl = groupsSeparatedByTwoNewlines(strings)
+    val rules = spl(0).split("\n").filterNot(_.isBlank).map(Rule(_))
+    val messages = spl(1).split("\n").filterNot(_.isBlank)
+    val ruleMap = rules.map(r => r.id -> r).toMap
+    (ruleMap, messages)
+  }
+
+  trait Rule {
+    def fits(string: String, allRules: Map[Int, Rule]): Boolean
+
+    def matching(string: String, allRules: Map[Int, Rule]): Set[String]
+
+    def id: Int
+  }
+
+  case class Exact(id: Int, rule: String) extends Rule {
+
+    override def fits(string: String, allRules: Map[Int, Rule]): Boolean = string == rule
+
+    override def matching(string: String, allRules: Map[Int, Rule]): Set[String] = {
+      if (string.startsWith(rule)) Set(string.replaceFirst(rule, "")) else Set()
+    }
+  }
+
+  case class Multiple(id: Int, rules: Seq[Seq[Int]]) extends Rule {
+
+    override def fits(string: String, allRules: Map[Int, Rule]): Boolean = this.matching(string, allRules).contains("")
+
+    override def matching(string: String, allRules: Map[Int, Rule]): Set[String] = rules
+      .map(l => l.map(allRules(_)))
+      .flatMap(r => r.foldLeft(Set(string))((v, r) => v.flatMap(s => r.matching(s, allRules))))
+      .toSet
+  }
+
+  object Rule {
+    def map(strings: Seq[String]): Map[Int, Rule] = {
+      val rules = strings.filterNot(_.isBlank).map(Rule(_))
+      rules.map(r => r.id -> r).toMap
+    }
+
+    def apply(string: String): Rule = {
+      val id = string.split(":")(0).trim.toInt
+      val ss = string.split(":")(1).trim
+      ss match {
+        case "\"a\"" => Exact(id, "a")
+        case "\"b\"" => Exact(id, "b")
+        case _ => Multiple(id, ss.split("\\|")
+          .map(_.trim).map(s => s.split(" ").map(_.toInt).toSeq).toSeq)
+      }
+    }
+  }
+
 }
--- aoc2020/src/test/scala/Day19Spec.scala
+++ aoc2020/src/test/scala/Day19Spec.scala
@@ -1,17 +1,35 @@
+import Day19.Rule
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.mutable
+
 class Day19Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day19.part1TestResult shouldEqual -1
+    Day19.part1TestResult shouldEqual 2
   }
   "Part1" should "be correct" in {
-    Day19.part1Result shouldEqual -1
+    Day19.part1Result shouldEqual 113
   }
   "Part2 Test" should "be correct" in {
     Day19.part2TestResult shouldEqual -1
   }
   "Part2" should "be correct" in {
     Day19.part2Result shouldEqual -1
+  }
+
+  "matching" should "produce the resulting string of applying pattern" in {
+    val str = ("42: 14 42 | 14\n14: \"b\"").split("\n")
+    val ma = Day19.Rule.map(str)
+    ma(14).matching("ba", ma) shouldEqual Set("a")
+    ma(42).matching("ba", ma) shouldEqual Set("a")
+    ma(42).matching("bba", ma) shouldEqual Set("a", "ba")
+    ma(42).matching("bbba", ma) shouldEqual Set("a", "ba", "bba")
+  }
+
+  "bonus" should "be correct" in  {
+    val (ruleMap, messages) = Day19.initialData(Day19.read("day19/bonus.txt"))
+    messages.count(s => ruleMap(0).fits(s, ruleMap)) shouldEqual 3
+
   }
 }
