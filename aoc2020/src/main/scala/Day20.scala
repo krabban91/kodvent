468718d5c9fc1ddefbf6fce71a2afc2ec224f4e8
Gabriel Andersson <anderssongabriel665@gmail.com>
2020-12-20 13:51:02 +0100

    :sparkles: year 2020 day 20 (#56)
    * :construction: copied the templates for day 20
    
    * updated input files for day 20
    
    * :sparkles: Completed day 20 part 1
    
    * :sparkles: Completed day 20 part 2
    
    * :mute: remove logging from day 20
    
    Co-authored-by: gandersson <gabriel.andersson@recordedfuture.com>

---
+++ aoc2020/src/main/scala/Day20.scala
@@ -0,0 +1,140 @@
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.grid.Grid
+import krabban91.kodvent.kodvent.utilities.logging.{LogUtils, Loggable}
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
+
+
+object Day20 extends App with AoCPart1Test with AoCPart2Test {
+
+  override def part1(strings: Seq[String]): Long = {
+    val tiles = initialData(strings)
+    val valid = matchTiles(tiles)
+    val side = math.sqrt(tiles.size).round.toInt
+    valid.map(_.map(_.id)).map(s => s.head * s(side - 1) * s(tiles.size - 1) * s(tiles.size - (side))).getOrElse(-1)
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    val image = Tile.fromTiles(matchTiles(initialData(strings)).get)
+    val seaMonster = Day20.Tile(Day20.read("day20$_seamonster.txt"))
+    image.permutations
+      .find(i => i.sweepSeaMonster(seaMonster) != i)
+      .map(_.sweepSeaMonster(seaMonster))
+      .get.grid.sum(_.roughness)
+  }
+
+  def initialData(strings: Seq[String]): Seq[Tile] = {
+    groupsSeparatedByTwoNewlines(strings)
+      .map(s => s.split("\n").filterNot(_.isBlank))
+      .map(Tile(_))
+  }
+
+  def matchTiles(tiles: Seq[Tile]): Option[Seq[Tile]] = {
+    val side = math.sqrt(tiles.size).round.toInt
+    val frontier = mutable.PriorityQueue[Seq[Tile]](Seq())(Ordering.by(s => s.size))
+    var valid: Option[Seq[Tile]] = None
+    while (frontier.nonEmpty) {
+      val pop = frontier.dequeue()
+      if (pop.size == tiles.size) {
+        valid = Option(pop)
+        frontier.clear()
+      } else {
+        val potential = tiles
+          .filterNot(t => pop.map(_.id).contains(t.id))
+          .flatMap(_.permutations)
+          .map(t => pop ++ Seq(t))
+          .toSet
+        val next = potential.filter(s => Tile.isValidAlignment(s, side))
+        frontier.addAll(next)
+      }
+    }
+    valid
+  }
+
+  case class Tile(id: Long, grid: Grid[Mini]) {
+
+    def flipHorizontally: Tile = Tile(id, this.grid.map((v, p) => this.grid.get(this.grid.width() - p.x - 1, p.y).get()))
+
+    def flipVertically: Tile = Tile(id, this.grid.map((v, p) => this.grid.get(p.x, this.grid.height() - p.y - 1).get()))
+
+    def rotate: Tile = Tile(id, this.grid.map((v, p) => this.grid.get(this.grid.height() - p.y - 1, p.x).get()))
+
+    def fitsToTheRight(other: Tile): Boolean = Range(0, this.grid.height()).forall(i => this.grid.get(this.grid.width() - 1, i) == other.grid.get(0, i))
+
+    def fitsToTheBottom(other: Tile): Boolean = Range(0, this.grid.width()).forall(i => this.grid.get(i, this.grid.height() - 1) == other.grid.get(i, 0))
+
+    def rotations: Set[Tile] = Set(this, this.rotate, this.rotate.rotate, this.rotate.rotate.rotate)
+
+    def flips: Set[Tile] = Set(this, this.flipVertically, this.flipHorizontally)
+
+    def permutations: Set[Tile] = this.rotations.flatMap(_.flips)
+
+    override def toString: String = s"Tile $id: \n${LogUtils.tiles(grid)}"
+
+    def sweepSeaMonster(seaMonster: Tile): Tile = {
+      var output = this
+      val smHeight = seaMonster.grid.height()
+      val smWidth = seaMonster.grid.width()
+      for (y <- Range(0, this.grid.height() - smHeight); x <- Range(0, this.grid.width() - smWidth)) {
+        if (Range(0, smHeight).forall(dy => Range(0, smWidth).forall(dx => {
+          val mask = seaMonster.grid.get(dx, dy).get.v
+          val loc = output.grid.get(x + dx, y + dy).get.v
+          mask == '.' || loc == 'O' || loc == '#'
+        }))) {
+          output = Tile(id, output.grid.map((m, p) => {
+            if (p.x >= x && p.x <= x + smWidth && p.y >= y && p.y <= y + smHeight) {
+              seaMonster.grid.get(p.x - x, p.y - y).map(a => {
+                if (a.v == '#')
+                  Mini('O')
+                else m
+              }).orElse(m)
+            } else m
+          }))
+        }
+      }
+      output
+    }
+  }
+
+  case class Mini(v: Char) extends Loggable {
+    override def showTile(): String = v.toString
+
+    def roughness: Long = if (v == '#') 1 else 0
+  }
+
+  object Tile {
+    def apply(input: Seq[String]): Tile = Tile(
+      input.head.split(":")(0).split(" ")(1).trim.toLong,
+      new Grid[Mini](input.tail
+        .map(s => s
+          .map(Mini(_))
+          .asJava)
+        .asJava))
+
+    def fromTiles(tiles: Seq[Tile]): Tile = {
+      val side = math.sqrt(tiles.size).round.toInt
+      val sb = new StringBuilder()
+      sb.append("Tile -1:\n")
+      for (tY <- Range(0, side)) {
+        val tileRow = tiles.slice(tY * side, (tY + 1) * side)
+        for (y <- Range(1, tileRow.head.grid.height() - 1)) {
+          tileRow.foreach(t => sb.append(t.grid.getRaw.get(y).subList(1, t.grid.width() - 1).asScala.map(_.showTile()).reduce((l, r) => l + r)))
+          sb.append("\n")
+        }
+      }
+      Tile(sb.toString().split("\n"))
+    }
+
+    def isValidAlignment(alignment: Seq[Tile], side: Int): Boolean = alignment.indices.forall(i => {
+      val r = if (i != alignment.size - 1 && i % side != side - 1) {
+        alignment(i).fitsToTheRight(alignment(i + 1))
+      } else true
+      val d = if (i + side < alignment.size && i / side != side - 1) {
+        alignment(i).fitsToTheBottom(alignment(i + side))
+      } else true
+      r && d
+    })
+  }
+
+}
---
+++ aoc2020/src/test/scala/Day20Spec.scala
@@ -0,0 +1,51 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day20Spec extends AnyFlatSpec with Matchers {
+  "Part1 Test" should "be correct" in {
+    Day20.part1TestResult shouldEqual 20899048083289L
+  }
+  "Part1" should "be correct" in {
+    Day20.part1Result shouldEqual 14986175499719L
+  }
+  "Part2 Test" should "be correct" in {
+    Day20.part2TestResult shouldEqual 273
+  }
+  "Part2" should "be correct" in {
+    Day20.part2Result shouldEqual 2161
+  }
+
+  "Tiles" should "modify correctly" in {
+    val origTile = Day20.Tile("Tile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..".split("\n"))
+    val copy = Day20.Tile("Tile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..".split("\n"))
+    val verticalFlip = Day20.Tile("Tile 1951:\n#...##.#..\n..#.#..#.#\n.###....#.\n###.##.##.\n.###.#####\n.##.#....#\n#...######\n.....#..##\n#.####...#\n#.##...##.".split("\n"))
+    origTile shouldEqual copy
+    origTile.flipVertically shouldEqual verticalFlip
+    origTile.flipVertically shouldEqual verticalFlip
+    origTile.flipHorizontally should not be origTile
+    origTile.flipHorizontally.flipHorizontally shouldEqual origTile
+
+    val o1 = Day20.Tile("Tile 2473:\n#....####.\n#..#.##...\n#.##..#...\n######.#.#\n.#...#.#.#\n.#########\n.###.#..#.\n########.#\n##...##.#.\n..###.#.#.".split("\n"))
+    val rotateflip = Day20.Tile("Tile 2473:\n..#.###...\n##.##....#\n..#.###..#\n###.#..###\n.######.##\n#.#.#.#...\n#.###.###.\n#.###.##..\n.######...\n.##...####".split("\n"))
+    o1.rotate.rotate.rotate.flipVertically shouldEqual rotateflip
+  }
+
+  "Tiles" should "fit correctly" in {
+    val origTile = Day20.Tile("Tile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..".split("\n"))
+    origTile.fitsToTheBottom(origTile.flipVertically) shouldEqual true
+    origTile.fitsToTheRight(origTile.flipHorizontally) shouldEqual true
+  }
+
+  "Seamonsters" should "be found" in {
+    val seaMonster = Day20.Tile(Day20.read("day20$_seamonster.txt"))
+    val image = Day20.Tile(Day20.read("test/day20$_joined.txt"))
+    println(seaMonster)
+    val res = image.permutations.find(i => {
+      val tile = i.sweepSeaMonster(seaMonster)
+      tile != i
+    }).map(_.sweepSeaMonster(seaMonster)).get
+    res.grid.sum(v => if (v.v == '#') 1 else 0) shouldEqual 273
+
+  }
+}
+
