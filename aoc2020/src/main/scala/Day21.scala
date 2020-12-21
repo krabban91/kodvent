0f58c6d3478010adc3b3fd6c35961ece02770bde
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-21 07:45:07 +0100

    :sparkles: Completed day 21 part 1

--- aoc2020/src/main/scala/Day21.scala
+++ aoc2020/src/main/scala/Day21.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day21 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,35 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
-
+  override def part1(strings: Seq[String]): Long = {
+    val inp = strings.map(Recipe(_)).sortBy(-_.allergens.size)
+    val ingredients = inp.flatMap(_.ingredients).toSet
+    val safeIngredients = inertIngredients(inp, ingredients)
+    inp.map(_.ingredients).map(is => is.count(i => safeIngredients.contains(i))).sum
+  }
   override def part2(strings: Seq[String]): Long = -1
+  
+  private def inertIngredients(inp: Seq[Recipe], ingredients: Set[String]): Set[String] = {
+    val vv = inp.flatMap(r => r.allergens.map(a => a -> r)).groupBy(_._1)
+    val vvi = inp.flatMap(r => r.ingredients.map(a => a -> r)).groupBy(_._1)
+    ingredients.filterNot(i => {
+      val recipes = vvi(i).map(_._2)
+      val as = recipes.flatMap(r => r.allergens).toSet
+      as.exists(a => {
+        val value = vv(a).map(_._2)
+        value.forall(r => r.ingredients.contains(i))
+      })
+    })
+  }
+
+  case class Recipe(ingredients: Seq[String], allergens: Seq[String])
+
+  object Recipe {
+    def apply(string: String): Recipe = {
+      val spl = string.split("\\(contains ")
+
+      Recipe(spl(0).split(" ").map(_.trim), spl(1).replace(")", "").split(",").map(_.trim))
+    }
+  }
+
 }
--- aoc2020/src/test/scala/Day21Spec.scala
+++ aoc2020/src/test/scala/Day21Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day21Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day21.part1TestResult shouldEqual -1
+    Day21.part1TestResult shouldEqual 5
   }
   "Part1" should "be correct" in {
-    Day21.part1Result shouldEqual -1
+    Day21.part1Result shouldEqual 2595
   }
   "Part2 Test" should "be correct" in {
     Day21.part2TestResult shouldEqual -1
