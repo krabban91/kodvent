923826d290b63ca46c6e4cdf4125960dc763802b
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-21 09:15:37 +0100

    :bug: fixed typo in String aoc structure

--- aoc2020/src/main/scala/aoc/string/AoCPart2StringTest.scala
+++ aoc2020/src/main/scala/aoc/string/AoCPart2StringTest.scala
@@ -4,5 +4,5 @@ import aoc.input.TestInputs
 
 trait AoCPart2StringTest extends AoCPart2String with TestInputs {
   def part2TestResult: String = part2(getInputTest)
-  def printResultPart2Test: Unit = println(s"${this.getClass.getName} - Test:\tPart 1 -> ${this.part2TestResult}")
+  def printResultPart2Test: Unit = println(s"${this.getClass.getName} - Test:\tPart 2 -> ${this.part2TestResult}")
 }
