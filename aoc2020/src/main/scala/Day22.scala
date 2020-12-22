a933e43a1ed7cb1c73c57d829062b73cb3163b2d
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-22 07:39:20 +0100

    :sparkles: Completed day 22 part 1

--- aoc2020/src/main/scala/Day22.scala
+++ aoc2020/src/main/scala/Day22.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day22 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,33 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val result = Option(input(strings)).map(t => combat(t._1, t._2)).map(t => t._1 ++ t._2).get
+    result.indices.map(i => (result.size - i) * result(i)).sum
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  def combat(initialP1: Seq[Long], initialP2: Seq[Long]): (Seq[Long], Seq[Long]) = {
+    var (p1, p2) = (initialP1, initialP2)
+    while (p1.nonEmpty && p2.nonEmpty) {
+      if (p1.head > p2.head) {
+        p1 = p1.tail ++ Seq(p1.head, p2.head)
+        p2 = p2.tail
+      } else {
+        p2 = p2.tail ++ Seq(p2.head, p1.head)
+        p1 = p1.tail
+      }
+    }
+    (p1, p2)
+  }
+
+  def input(strings: Seq[String]): (Seq[Long], Seq[Long]) = {
+    val v = groupsSeparatedByTwoNewlines(strings)
+      .map(s => s.split("\n").filterNot(_.isBlank))
+      .map(_.tail)
+      .map(v => v.map(_.toLong))
+      .map(_.toSeq)
+    (v.head, v.tail.head)
+  }
 }
