6859fd997a6f206e6b52e143b252a5704e68408b
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-22 07:40:10 +0100

    :sparkles: Completed day 22 part 2

--- aoc2020/src/main/scala/Day22.scala
+++ aoc2020/src/main/scala/Day22.scala
@@ -10,16 +10,33 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    val result = Option(input(strings)).map(t => combat(t._1, t._2)).map(t => t._1 ++ t._2).get
+    val result = Option(input(strings)).map(t => combat(t._1, t._2, recursive = false)).map(t => t._1 ++ t._2).get
     result.indices.map(i => (result.size - i) * result(i)).sum
   }
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    val result = Option(input(strings)).map(t => combat(t._1, t._2, recursive = true)).map(t => t._1 ++ t._2).get
+    result.indices.map(i => (result.size - i) * result(i)).sum
+  }
 
-  def combat(initialP1: Seq[Long], initialP2: Seq[Long]): (Seq[Long], Seq[Long]) = {
+  def combat(initialP1: Seq[Long], initialP2: Seq[Long], recursive: Boolean): (Seq[Long], Seq[Long]) = {
     var (p1, p2) = (initialP1, initialP2)
+    val handHistory = mutable.Set[Seq[Long]]()
     while (p1.nonEmpty && p2.nonEmpty) {
-      if (p1.head > p2.head) {
+      if(!handHistory.add(p1) || !handHistory.add(p2)){
+        return (p1, Seq())
+      }
+      if (recursive && p1.head <= p1.tail.size && p2.head <= p2.tail.size) {
+        val (rec1, rec2) = combat(p1.tail.take(p1.head.toInt), p2.tail.take(p2.head.toInt), recursive)
+        if (rec1.size > rec2.size) {
+          p1 = p1.tail ++ Seq(p1.head, p2.head)
+          p2 = p2.tail
+        } else {
+          p2 = (p2.tail ++ Seq(p2.head, p1.head))
+          p1 = p1.tail
+        }
+      }
+      else if (p1.head > p2.head) {
         p1 = p1.tail ++ Seq(p1.head, p2.head)
         p2 = p2.tail
       } else {
