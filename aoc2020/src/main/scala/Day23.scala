1abbdc3cbde47e529608fe5913aca76d118faba8
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-23 08:58:46 +0100

    :art: Rewrote day 23 to use a wrapper class

--- aoc2020/src/main/scala/Day23.scala
+++ aoc2020/src/main/scala/Day23.scala
@@ -7,45 +7,52 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
   override def part1(strings: Seq[String]): Long = {
     val N = strings.head.length
     val moves = 100
-    val (cups, current) = getCups(strings, N)
-    moveCupsNTimes(cups, current, moves)
-    Range(1, strings.head.length).foldLeft(("", cups(1)))((t, _) => (s"${t._1}${t._2}", cups(t._2)))._1.toLong
+    val cups = new Cups(strings, N)
+    cups.moveNTimes(moves)
+    cups.next(strings.head.length - 1, 1)
+      .map(_.toString)
+      .reduce((l, r) => l + r).toLong
   }
 
   override def part2(strings: Seq[String]): Long = {
     val N = 1_000_000
     val moves = 10_000_000
-    val (cups, current) = getCups(strings, N)
-    moveCupsNTimes(cups, current, moves)
-    cups(1).toLong * cups(cups(1))
+    val cups = new Cups(strings, N)
+    cups.moveNTimes(moves)
+    cups.next(2, 1).product
   }
 
-  def getCups(strings: Seq[String], size: Int): (mutable.HashMap[Int, Int], Int) = {
-    val inp: Seq[Int] = strings.head.map(c => Integer.parseInt(c.toString)) ++ Range(strings.head.length + 1, size + 1)
-    (mutable.HashMap() ++ inp.indices.map(i => inp(i) -> inp((i + 1) % inp.size)).toMap, inp.head)
-  }
+  class Cups(strings: Seq[String], size: Int) {
+    private val inp: Seq[Long] = strings.head.map(c => Integer.parseInt(c.toString).toLong) ++ Range(strings.head.length + 1, size + 1).map(_.toLong)
+    private val minV: Long = inp.min
+    private val maxV: Long = inp.max
+    private val map: mutable.Map[Long, Long] = mutable.HashMap() ++ inp.indices.map(i => inp(i) -> inp((i + 1) % inp.size)).toMap
+    private var head: Long = inp.head
 
-  def moveCupsNTimes(cups: mutable.HashMap[Int, Int], current: Int, moves: Int): Unit = {
-    var curr = current
-    val maxV = cups.values.max
-    val minV = cups.values.min
-    for (move <- Range(0, moves)) {
-      curr = moveCups(cups, curr, move, minV, maxV)
-    }
-  }
+    def apply(i: Long): Long = map(i)
 
-  def moveCups(cups: mutable.HashMap[Int, Int], current: Int, move: Int, minV: Int, maxV: Int): Int = {
-    val pickedup = Seq(cups(current), cups(cups(current)), cups(cups(cups(current))))
-    cups.update(current, cups(cups(cups(cups(current)))))
-    var destination = current - 1
-    while (pickedup.contains(destination) || destination < minV) {
-      destination -= 1
-      if (destination < minV) {
-        destination = maxV
+    def moveNTimes(N: Int): Unit = Range(0, N)
+      .foreach(_ => move)
+
+    def next(n: Long): Seq[Long] = next(n, head)
+
+    def next(n: Long, after: Long): Seq[Long] = Range(0, n.toInt)
+      .foldLeft(Seq(after))((l, _) => l ++ Seq(this (l.last))).tail
+
+    def move(): Unit = {
+      val pickedup = next(3)
+      map.update(head, map(pickedup.last))
+      var destination = head - 1
+      while (pickedup.contains(destination) || destination < minV) {
+        destination -= 1
+        if (destination < minV) {
+          destination = maxV
+        }
       }
+      map.update(pickedup.last, map(destination))
+      map.update(destination, pickedup.head)
+      head = map(head)
     }
-    cups.update(pickedup(2), cups(destination))
-    cups.update(destination, pickedup(0))
-    cups(current)
   }
+
 }
