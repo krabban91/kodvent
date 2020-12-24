28715b2b4a9633929a63e33cd95a912f369ad3a6
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-24 08:46:59 +0100

    :construction: copied the templates for day 25

---
+++ aoc2020/src/main/scala/Day25.scala
@@ -0,0 +1,10 @@
+import aoc.numeric.AoCPart1Test
+
+object Day25 extends App with AoCPart1Test {
+
+  printResultPart1Test
+  printResultPart1
+
+  override def part1(strings: Seq[String]): Long = -1
+
+}
---
+++ aoc2020/src/test/scala/Day25Spec.scala
@@ -0,0 +1,11 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day25Spec extends AnyFlatSpec with Matchers {
+  "Part1 Test" should "be correct" in {
+    Day25.part1TestResult shouldEqual -1
+  }
+  "Part1" should "be correct" in {
+    Day25.part1Result shouldEqual -1
+  }
+}
