f61e2fa4a6cf19906d9e738c075c7c225096de53
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-25 07:03:58 +0100

    :sparkles: Completed day 25 part 1
    * Since part 2 of the 25th is to complete all
      other days, it is omitted from the code.

--- aoc2020/src/main/scala/Day25.scala
+++ aoc2020/src/main/scala/Day25.scala
@@ -5,6 +5,29 @@ object Day25 extends App with AoCPart1Test {
   printResultPart1Test
   printResultPart1
 
-  override def part1(strings: Seq[String]): Long = -1
+  /**
+   * cardPublicKey = pow(7, cardLoopsize) mod 20201227
+   * doorPublicKey = pow(7, doorLoopsize) mod 20201227
+   * encryptionKey = pow(cardPublicKey, doorLoopsize) mod 20201227 = pow(doorPublicKey, cardLoopsize) mod 20201227
+   * @param strings the public keys
+   * @return the shared encryptionKey
+   */
+  override def part1(strings: Seq[String]): Long = {
+    val inp = strings.map(_.toLong)
+    transform(inp.tail.head, bruteForce(inp.head))
+  }
 
+  def bruteForce(publicKey: Long): Int = {
+    var test = -1L
+    var loopSize = 0
+    while (test != publicKey) {
+      loopSize += 1
+      test = transform(7, loopSize)
+    }
+    loopSize
+  }
+
+  def transform(subject: Long, loopSize: Int): Long = {
+    BigInt(subject).modPow(loopSize, 20201227).toLong
+  }
 }
--- aoc2020/src/test/scala/Day25Spec.scala
+++ aoc2020/src/test/scala/Day25Spec.scala
@@ -3,9 +3,9 @@ import org.scalatest.matchers.should.Matchers
 
 class Day25Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day25.part1TestResult shouldEqual -1
+    Day25.part1TestResult shouldEqual 14897079
   }
   "Part1" should "be correct" in {
-    Day25.part1Result shouldEqual -1
+    Day25.part1Result shouldEqual 18608573
   }
 }
