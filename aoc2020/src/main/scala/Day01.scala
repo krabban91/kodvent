4c2e332ee317073bf620d7b6cdb63f007ac255da
gandersson <gabriel.andersson@recordedfuture.com>
2020-12-28 11:00:57 +0100

    :art: refactor year 2020 day 1
    * Takes more time due to lack of complexity in
      problem. But it is easier to read

--- aoc2020/src/main/scala/Day01.scala
+++ aoc2020/src/main/scala/Day01.scala
@@ -3,19 +3,7 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day01 extends App with AoCPart1Test with AoCPart2Test {
 
-  override def part1(strings: Seq[String]): Long = {
-    val input = strings.map(_.toLong)
-    for (i <- input; j <- input) {
-      if (i + j == 2020) return i * j
-    }
-    -1
-  }
+  override def part1(strings: Seq[String]): Long = strings.map(_.toLong).combinations(2).find(_.sum == 2020).map(_.product).getOrElse(-1)
 
-  override def part2(strings: Seq[String]): Long = {
-    val input = strings.map(_.toLong)
-    for (i <- input; j <- input; k <- input) {
-      if (i + j + k == 2020) return i * j * k
-    }
-    -1
-  }
+  override def part2(strings: Seq[String]): Long = strings.map(_.toLong).combinations(3).find(_.sum == 2020).map(_.product).getOrElse(-1)
 }
