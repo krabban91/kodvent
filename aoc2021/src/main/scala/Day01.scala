f0054624099484170a60086930af30f7af8b1138
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-01 08:13:41 +0100

    :art: Solve y2021d01 using sliding

--- aoc2021/src/main/scala/Day01.scala
+++ aoc2021/src/main/scala/Day01.scala
@@ -2,22 +2,15 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day01 extends App with AoCPart1Test with AoCPart2Test {
 
-  override def part1(strings: Seq[String]): Long = {
-    val in = strings.map(_.toInt)
-    in.indices
-      .filter(_ >= 1)
-      .filter(i => in(i) > in(i - 1))
-      .count(_ => true)
-  }
+  override def part1(strings: Seq[String]): Long = strings.map(_.toInt)
+    .sliding(2)
+    .count(t => t.head < t.last)
 
-  override def part2(strings: Seq[String]): Long = {
-    val in = strings.map(_.toInt)
-    in.indices
-      .filter(_ >= 3)
-      .filter(i => {
-        val prev = in(i - 1) + in(i - 2) + in(i - 3)
-        val curr = in(i) + in(i - 1) + in(i - 2)
-        prev < curr
-      }).count(_ => true)
-  }
+  override def part2(strings: Seq[String]): Long = strings.map(_.toInt)
+    .sliding(4)
+    .count(t => {
+      val prev = t.head + t(1) + t(2)
+      val curr = t(1) + t(2) + t.last
+      prev < curr
+    })
 }
