a69c98bf3bc8698335f1d3013492f7cdfc5683a1
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-01 06:29:20 +0100

    :sparkles: year 2021 day 1

--- aoc2021/src/main/scala/Day01.scala
+++ aoc2021/src/main/scala/Day01.scala
@@ -2,13 +2,22 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day01 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
+  override def part1(strings: Seq[String]): Long = {
+    val in = strings.map(_.toInt)
+    in.indices
+      .filter(_ >= 1)
+      .filter(i => in(i) > in(i - 1))
+      .count(_ => true)
+  }
 
-
-  override def part1(strings: Seq[String]): Long = -1
-
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    val in = strings.map(_.toInt)
+    in.indices
+      .filter(_ >= 3)
+      .filter(i => {
+        val prev = in(i - 1) + in(i - 2) + in(i - 3)
+        val curr = in(i) + in(i - 1) + in(i - 2)
+        prev < curr
+      }).count(_ => true)
+  }
 }
--- aoc2021/src/test/scala/Day01Spec.scala
+++ aoc2021/src/test/scala/Day01Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day01Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day01.part1TestResult shouldEqual -1
+    Day01.part1TestResult shouldEqual 7
   }
   "Part1" should "be correct" in {
-    Day01.part1Result shouldEqual -1
+    Day01.part1Result shouldEqual 1448
   }
   "Part2 Test" should "be correct" in {
-    Day01.part2TestResult shouldEqual -1
+    Day01.part2TestResult shouldEqual 5
   }
   "Part2" should "be correct" in {
-    Day01.part2Result shouldEqual -1
+    Day01.part2Result shouldEqual 1471
   }
 }
