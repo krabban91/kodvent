40d04795eee084103ce607453453e5bc728c43af
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-03 06:23:18 +0100

    :sparkles: Year 2021 day 03 part 1

--- aoc2021/src/main/scala/Day03.scala
+++ aoc2021/src/main/scala/Day03.scala
@@ -1,5 +1,8 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable.ListBuffer
+import scala.util.chaining.scalaUtilChainingOps
+
 object Day03 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +10,21 @@ object Day03 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val input = strings.map(s => s.toCharArray)
+
+    val out = input.foldLeft(input.head.indices.map(_ => (0, 0)))((res, in) => {
+      val v = ListBuffer[(Int, Int)]()
+      for (i <- res.indices) {
+        v.append((res(i)._1 + (if (in(i) == '1') 1 else 0), res(i)._2 + (if (in(i) == '0') 1 else 0)))
+      }
+      v.toSeq.toIndexedSeq
+    })
+    val gamma = out.map(t => if (t._1 > t._2) "1" else "0").reduce((a, b) => a + b).pipe(Integer.parseInt(_, 2))
+    val epsilon = out.map(t => if (t._1 < t._2) "1" else "0").reduce((a, b) => a + b).pipe(Integer.parseInt(_, 2))
+    gamma * epsilon
+
+  }
 
   override def part2(strings: Seq[String]): Long = -1
 }
