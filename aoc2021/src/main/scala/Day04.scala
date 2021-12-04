aef4bf2a755411394fdea8951fdc546111abf717
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-04 02:18:09 +0100

    :sparkles: Completed Day 04 part 1 and 2

--- aoc2021/src/main/scala/Day04.scala
+++ aoc2021/src/main/scala/Day04.scala
@@ -1,5 +1,8 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable.ListBuffer
+import scala.util.chaining.scalaUtilChainingOps
+
 object Day04 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +10,66 @@ object Day04 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val numbers: Seq[Int] = strings.head.pipe(s => s.split(",")).map(_.toInt).toSeq
+    val boards: Seq[BingoBoard] = extractBoards(strings)
+    var hasWon = false
+    var currentNumber = 0
+    val runNumbers = ListBuffer[Int]()
 
-  override def part2(strings: Seq[String]): Long = -1
+    while (!hasWon){
+      val curr = numbers(currentNumber)
+      runNumbers.append(curr)
+      if(boards.exists(_.bingo(runNumbers.toSeq))){
+        hasWon = true
+      }else {
+        currentNumber += 1
+      }
+    }
+    val winner = boards.find(_.bingo(runNumbers.toSeq))
+
+    winner.get.missingNumbers(runNumbers.toSeq).sum * numbers(currentNumber)
+  }
+
+
+  override def part2(strings: Seq[String]): Long = {
+    val numbers: Seq[Int] = strings.head.pipe(s => s.split(",")).map(_.toInt).toSeq
+    val boards: Seq[BingoBoard] = extractBoards(strings)
+    var hasWon = false
+    var currentNumber = 0
+    val runNumbers = ListBuffer[Int]()
+    var previousWinners = Seq[BingoBoard]()
+    while (!hasWon){
+      val curr = numbers(currentNumber)
+      runNumbers.append(curr)
+      if(boards.forall(_.bingo(runNumbers.toSeq))){
+        hasWon = true
+      }else {
+        currentNumber += 1
+        previousWinners = boards.filter(_.bingo(runNumbers.toSeq))
+      }
+    }
+    val winner = boards.find(b => b.bingo(runNumbers.toSeq) && !previousWinners.contains(b))
+
+    winner.get.missingNumbers(runNumbers.toSeq).sum * numbers(currentNumber)
+  }
+
+  private def extractBoards(strings: Seq[String]): Seq[BingoBoard] = {
+    strings.tail.tail.map(_.split(" ").filterNot(_.isBlank).map(_.toInt).toSeq).filterNot(_.isEmpty).grouped(5).toSeq.map(v => new BingoBoard(v))
+  }
+
+
+  class BingoBoard(board: Seq[Seq[Int]]){
+
+    private val columns = board.transpose
+
+    def bingo(runNumbers: Seq[Int]): Boolean = {
+      this.board.exists(row => row.forall(i => runNumbers.contains(i))) ||
+        this.columns.exists(row => row.forall(i => runNumbers.contains(i)))
+    }
+
+    def missingNumbers(runNumbers: Seq[Int]): Seq[Int] = {
+      this.board.flatten.filterNot(runNumbers.contains(_))
+    }
+  }
 }
--- aoc2021/src/test/scala/Day04Spec.scala
+++ aoc2021/src/test/scala/Day04Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day04Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day04.part1TestResult shouldEqual -1
+    Day04.part1TestResult shouldEqual 4512
   }
   "Part1" should "be correct" in {
-    Day04.part1Result shouldEqual -1
+    Day04.part1Result shouldEqual 60368
   }
   "Part2 Test" should "be correct" in {
-    Day04.part2TestResult shouldEqual -1
+    Day04.part2TestResult shouldEqual 1924
   }
   "Part2" should "be correct" in {
-    Day04.part2Result shouldEqual -1
+    Day04.part2Result shouldEqual 17435
   }
 }
