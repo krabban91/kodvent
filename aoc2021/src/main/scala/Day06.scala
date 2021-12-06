b176c7d6075d6b57af9731aa5cc778d903ae6e27
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-06 06:56:36 +0100

    :mute: remove logging from day 6

--- aoc2021/src/main/scala/Day06.scala
+++ aoc2021/src/main/scala/Day06.scala
@@ -4,11 +4,6 @@ import scala.collection.mutable
 
 object Day06 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     val input = strings.head.split(",").map(_.toInt)
     var fishes = mutable.HashMap[Int, Long]()
