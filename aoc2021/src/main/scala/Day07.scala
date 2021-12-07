784a133e44df508be5770751767dbf01054211f0
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-07 06:18:32 +0100

    :mute: Day 07 - Remove logging

--- aoc2021/src/main/scala/Day07.scala
+++ aoc2021/src/main/scala/Day07.scala
@@ -2,11 +2,6 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day07 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     val input = strings.head.split(",").map(_.toInt)
     (input.min to input.max).foldLeft(Long.MaxValue)((cheapest, i) => {
