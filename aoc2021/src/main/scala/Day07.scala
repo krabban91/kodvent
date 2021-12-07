c89fa8ef20ee3cd2696dce0cf9be5575529f8e4d
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-07 06:07:57 +0100

    :sparkles: Day 07 - Completed part 1

--- aoc2021/src/main/scala/Day07.scala
+++ aoc2021/src/main/scala/Day07.scala
@@ -7,7 +7,19 @@ object Day07 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val input = strings.head.split(",").map(_.toInt)
+    val min = input.min
+    val max = input.max
+    var cheapest = Long.MaxValue
+    for (i <- min to max) {
+      val sum = input.map(v => Math.abs(v - i)).sum
+      if (sum < cheapest) {
+        cheapest = sum
+      }
+    }
+    cheapest
+  }
 
   override def part2(strings: Seq[String]): Long = -1
 }
