073d6af67a8b6da3339452a866d4eac87f79ff72
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-07 08:20:18 +0100

    :package: Day 08 - Preparations

---
+++ aoc2021/src/main/scala/Day08.scala
@@ -0,0 +1,31 @@
+import Day08.CavePath
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import com.sun.tools.javac.code.Preview
+import krabban91.kodvent.kodvent.utilities.Distances
+import krabban91.kodvent.kodvent.utilities.grid.Grid
+import krabban91.kodvent.kodvent.utilities.search.{Edge, Graph, Path}
+
+import java.awt.Point
+import java.util
+import java.util.Comparator
+import java.util.stream.Collectors
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.jdk.CollectionConverters._
+
+object Day08 extends App with AoCPart1Test with AoCPart2Test {
+
+  printResultPart1Test
+  printResultPart1
+  printResultPart2Test
+  printResultPart2
+
+  override def part1(strings: Seq[String]): Long = {
+    -1
+  }
+
+  override def part2(strings: Seq[String]): Long = -1
+
+
+
+}
