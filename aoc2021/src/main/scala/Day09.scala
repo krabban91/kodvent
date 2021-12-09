75454a34ff5db972b442b42e55d8d4a89faf1e56
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-09 06:12:47 +0100

    :sparkles: Day 09 - Completed part 1

--- aoc2021/src/main/scala/Day09.scala
+++ aoc2021/src/main/scala/Day09.scala
@@ -1,4 +1,8 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.grid.Grid
+
+import java.util.stream.Collectors
+import scala.jdk.CollectionConverters._
 
 object Day09 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -7,7 +11,16 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val inp = strings.map(s => s.chars().mapToObj(_.toChar.toString.toInt).collect(Collectors.toList[Int]))
+    val g = new Grid[Int](inp.toList.asJava)
+    g.map((i, p) => {
+      val around = g.getAdjacentTiles(p)
+      if (around.stream().allMatch(v => v > i)) {
+        i + 1
+      } else 0
+    }).sum(_.toLong)
+  }
 
   override def part2(strings: Seq[String]): Long = -1
 }
