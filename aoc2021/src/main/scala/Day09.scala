3f383ea953721d900e1a091ca32b62ade22be303
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-09 20:52:38 +0100

    :recycle: Day 09 - Reuse code between parts
    * Rename some variables + run  reformat

--- aoc2021/src/main/scala/Day09.scala
+++ aoc2021/src/main/scala/Day09.scala
@@ -16,50 +16,63 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    val inp = strings.map(s => s.chars().mapToObj(_.toChar.toString.toInt).collect(Collectors.toList[Int]))
-    val g = new Grid[Int](inp.toList.asJava)
-    g.map((i, p) => {
-      val around = g.getAdjacentTiles(p)
-      if (around.stream().allMatch(v => v > i)) {
-        i + 1
-      } else 0
-    }).sum(_.toLong)
+    val grid: Grid[Int] = extractMap(strings)
+    val points: Set[Point] = getLowPoints(grid)
+    points.toSeq
+      .map(grid.get(_).get().toLong + 1)
+      .sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-    val inp = strings.map(s => s.chars().mapToObj(_.toChar.toString.toInt).collect(Collectors.toList[Int]))
-    val g = new Grid[Int](inp.toList.asJava)
-    val points = ListBuffer[Point]()
+    val grid: Grid[Int] = extractMap(strings)
+    val points: Set[Point] = getLowPoints(grid)
+    val basins: Map[Point, Set[Point]] = exploreBasins(grid, points)
+    basins.values.map(_.size.toLong).toSeq
+      .sortWith((a, b) => a > b)
+      .take(3)
+      .product
+  }
 
-    g.forEach((i, p) => {
-      val around = g.getAdjacentTiles(p)
+  private def getLowPoints(grid: Grid[Int]) = {
+    val points = ListBuffer[Point]()
+    grid.forEach((i, p) => {
+      val around = grid.getAdjacentTiles(p)
       if (around.stream().allMatch(v => v > i)) {
         points.append(p)
       }
     })
+    points.toSet
+  }
+
+  private def extractMap(strings: Seq[String]): Grid[Int] = {
+    val input = strings.map(s => s.chars().mapToObj(_.toChar.toString.toInt).collect(Collectors.toList[Int]))
+    new Grid[Int](input.toList.asJava)
+  }
+
+  private def exploreBasins(g: Grid[Int], points: Set[Point]): Map[Point, Set[Point]] = {
     val basins = mutable.HashMap[Point, mutable.HashSet[Point]]()
     points.foreach(p => basins.put(p, mutable.HashSet(p)))
     basins.foreach(t => {
       val (p, l) = t
 
       val frontier = mutable.PriorityQueue[Point]()(Ordering.Int.reverse.on(v => g.get(v).get()))
-      frontier.addAll(adjacents(p, g))
+      frontier.addAll(adjacentPoints(p, g))
       val checked = mutable.HashSet(p)
       while (frontier.nonEmpty) {
         val v = frontier.dequeue()
         if (g.get(v).get() != 9) {
           l.add(v)
-          val as = adjacents(v, g).filter(!checked.contains(_))
+          val as = adjacentPoints(v, g).filter(!checked.contains(_))
           frontier.addAll(as)
         }
         checked.add(v)
       }
 
     })
-    basins.values.map(_.size).toSeq.sortWith((a, b) => a > b).take(3).map(_.toLong).product
+    basins.map(kv => (kv._1, kv._2.toSet)).toMap
   }
 
-  def adjacents(p: Point, g: Grid[Int]): Seq[Point] = {
+  def adjacentPoints(p: Point, g: Grid[Int]): Seq[Point] = {
     g.getSurroundingTilesWithPoints(p, false).asScala.map(_.getKey).filter(o => Distances.manhattan(o, p) == 1).toSeq
   }
 }
