92dcee3bff3d36d71be0f8869c55537ff4c46ea0
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-10 06:42:47 +0100

    :sparkles: Day 10 - Completed part 1

--- aoc2021/src/test/scala/Day10Spec.scala
+++ aoc2021/src/test/scala/Day10Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day10Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day10.part1TestResult shouldEqual -1
+    Day10.part1TestResult shouldEqual 26397
   }
   "Part1" should "be correct" in {
-    Day10.part1Result shouldEqual -1
+    Day10.part1Result shouldEqual 364389
   }
   "Part2 Test" should "be correct" in {
     Day10.part2TestResult shouldEqual -1
