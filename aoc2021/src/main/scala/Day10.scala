ad37e6595c175494db21951d755c166b2c4ad91c
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-10 06:42:47 +0100

    :sparkles: Day 10 - Completed part 1

--- aoc2021/src/main/scala/Day10.scala
+++ aoc2021/src/main/scala/Day10.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day10 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,34 @@ object Day10 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    strings.map(syntaxCheck).sum
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  def syntaxCheck(string: String): Long = {
+    val m = Map(')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+    var corrupted = 0
+    string.foldLeft(mutable.Stack[Char]())((stack, c) => {
+      c match {
+        case '<' =>
+          stack.push('>')
+        case '(' =>
+          stack.push(')')
+        case '[' =>
+          stack.push(']')
+        case '{' =>
+          stack.push('}')
+        case _ =>
+          val exp = stack.pop()
+          if (corrupted == 0 && exp != c) {
+            corrupted = m(c)
+          }
+      }
+      stack
+    })
+    corrupted
+  }
+
 }
