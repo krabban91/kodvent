d1942230599aaecb3f793807d23939c2ba026020
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-11 07:33:57 +0100

    :sparkles: Completed Day 11 part 1

--- aoc2021/src/main/scala/Day11.scala
+++ aoc2021/src/main/scala/Day11.scala
@@ -1,4 +1,11 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.grid.Grid
+
+import java.awt.Point
+import java.util
+import java.util.stream.Collectors
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.{CollectionHasAsScala, IterableHasAsJava}
 
 object Day11 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -8,9 +15,46 @@ object Day11 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    val input = strings.head
-    -1
+    var curr: Grid[Int] = parseDay(strings)
+    var flashes = 0L
+    for (step <- 1 to 100) {
+      val (next, newFlashes) = stepDay(curr)
+      flashes += newFlashes
+      curr = next
+    }
+    flashes
+  }
+
+  def parseDay(strings: Seq[String]): Grid[Int] = {
+    val input = strings.map(_.chars().mapToObj(_.toChar.toString.toInt).collect(Collectors.toList[Int]))
+    new Grid[Int](new util.ArrayList(input.asJavaCollection))
   }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  def stepDay(curr: Grid[Int]): (Grid[Int], Long) = {
+    var next = curr.map((v, p) => v + 1)
+    val toFlash = mutable.Stack[Point]()
+    next.forEach((v, p) => if (v > 9) toFlash.push(p))
+    val hasFlashed = mutable.HashSet[Point]()
+
+    while (toFlash.nonEmpty) {
+      val c = toFlash.pop()
+      if (!hasFlashed.contains(c)) {
+        hasFlashed.add(c)
+        next = next.map((v, p) => {
+          if (next.getSurroundingTilesWithPoints(p, false).asScala.exists(kv => kv.getKey.equals(c))) {
+            if (v == 9) {
+              toFlash.push(p)
+            }
+            v + 1
+          } else v
+        })
+      }
+    }
+    if (hasFlashed.nonEmpty) {
+      next = next.map((v, p) => if (v > 9) 0 else v)
+    }
+    (next, hasFlashed.size.toLong)
+  }
 }
--- aoc2021/src/test/scala/Day11Spec.scala
+++ aoc2021/src/test/scala/Day11Spec.scala
@@ -2,11 +2,75 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day11Spec extends AnyFlatSpec with Matchers {
+  "stepDay" should "flash correctly" in {
+    val d1 = Day11.parseDay("""11111
+               |19991
+               |19191
+               |19991
+               |11111""".stripMargin.split("\n"))
+    val d2 = Day11.parseDay("""34543
+               |40004
+               |50005
+               |40004
+               |34543""".stripMargin.split("\n"))
+    Day11.stepDay(d1)._1 shouldBe d2
+    Day11.stepDay(d1)._2 shouldBe 9
+  }
+  "stepDay" should "flash correctly part 1 day 1" in {
+    val d1 = Day11.parseDay("""5483143223
+                              |2745854711
+                              |5264556173
+                              |6141336146
+                              |6357385478
+                              |4167524645
+                              |2176841721
+                              |6882881134
+                              |4846848554
+                              |5283751526""".stripMargin.split("\n"))
+    val d2 = Day11.parseDay("""6594254334
+                              |3856965822
+                              |6375667284
+                              |7252447257
+                              |7468496589
+                              |5278635756
+                              |3287952832
+                              |7993992245
+                              |5957959665
+                              |6394862637""".stripMargin.split("\n"))
+    Day11.stepDay(d1)._2 shouldBe 0
+    Day11.stepDay(d1)._1 shouldBe d2
+  }
+
+  "stepDay" should "flash correctly part 1 day 2" in {
+    val d1 = Day11.parseDay("""6594254334
+                              |3856965822
+                              |6375667284
+                              |7252447257
+                              |7468496589
+                              |5278635756
+                              |3287952832
+                              |7993992245
+                              |5957959665
+                              |6394862637""".stripMargin.split("\n"))
+    val d2 = Day11.parseDay("""8807476555
+                              |5089087054
+                              |8597889608
+                              |8485769600
+                              |8700908800
+                              |6600088989
+                              |6800005943
+                              |0000007456
+                              |9000000876
+                              |8700006848""".stripMargin.split("\n"))
+    Day11.stepDay(d1)._2 shouldBe 35
+    Day11.stepDay(d1)._1 shouldBe d2
+  }
+
   "Part1 Test" should "be correct" in {
-    Day11.part1TestResult shouldEqual -1
+    Day11.part1TestResult shouldEqual 1656
   }
   "Part1" should "be correct" in {
-    Day11.part1Result shouldEqual -1
+    Day11.part1Result shouldEqual 1729
   }
   "Part2 Test" should "be correct" in {
     Day11.part2TestResult shouldEqual -1
