1dae3a79c52d6fac61f0261dc0f6d71460dc52ef
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-13 06:31:38 +0100

    :sparkles: Completed Day 13 part 1

--- aoc2021/src/main/scala/Day13.scala
+++ aoc2021/src/main/scala/Day13.scala
@@ -7,7 +7,30 @@ object Day13 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val points: Seq[(Int, Int)] = strings.takeWhile(s => !s.isEmpty).map(s => s.strip().split(",").map(_.toInt)).map(l => (l.head, l.last))
+    val folds = strings.filter(s => s.startsWith("fold along")).map(s => s.split(" ").last).map(s => (s.split("=").head, s.split("=").last.toInt))
+    var output = points.toSet
+    for ((axis, value) <- Seq(folds.head)) {
+      output = output.map(t => {
+        if (axis == "x") {
+          if (t._1 < value) {
+            t
+          } else {
+            (t._1 - (t._1 - value) * 2, t._2)
+          }
+        } else {
+          if (t._2 < value) {
+            t
+          } else {
+            (t._1, t._2 - (t._2 - value) * 2)
+          }
+        }
+      })
+    }
+
+    output.size
+  }
 
   override def part2(strings: Seq[String]): Long = -1
 }
