a960d8e4dade06aa10bcb762064720a4184e1eb9
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-15 09:44:55 +0100

    :card_file_box: Day 16 - Added input of day

--- aoc2021/src/main/scala/Day16.scala
+++ aoc2021/src/main/scala/Day16.scala
@@ -1,10 +1,8 @@
-import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import aoc.numeric.{AoCPart1, AoCPart2}
 
-object Day16 extends App with AoCPart1Test with AoCPart2Test {
+object Day16 extends App with AoCPart1 with AoCPart2 {
 
-  printResultPart1Test
   printResultPart1
-  printResultPart2Test
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = -1
