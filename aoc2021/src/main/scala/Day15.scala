ac74e0b7922170338be40be8fcbcb4b8390d9f5b
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-18 21:52:40 +0100

    :zap: Day 15 - Speed up search by not keeping path-history

--- aoc2021/src/main/scala/Day15.scala
+++ aoc2021/src/main/scala/Day15.scala
@@ -10,18 +10,14 @@ object Day15 extends App with AoCPart1Test with AoCPart2Test {
     val maps: Map[(Int, Int), Long] = extractMap(strings, 1)
     val start = (maps.keys.map(_._1).min, maps.keys.map(_._2).min)
     val end = (maps.keys.map(_._1).max, maps.keys.map(_._2).max)
-
-    val path: (Seq[(Int, Int)], Long) = findPath(start, end, maps)
-    path._2
+    costBetween(start, end, maps)
   }
 
   override def part2(strings: Seq[String]): Long = {
     val maps: Map[(Int, Int), Long] = extractMap(strings, 5)
     val start = (maps.keys.map(_._1).min, maps.keys.map(_._2).min)
     val end = (maps.keys.map(_._1).max, maps.keys.map(_._2).max)
-
-    val path: (Seq[(Int, Int)], Long) = findPath(start, end, maps)
-    path._2
+    costBetween(start, end, maps)
   }
 
   private def extractMap(strings: Seq[String], tiles: Int): Map[(Int, Int), Long] = {
@@ -37,28 +33,27 @@ object Day15 extends App with AoCPart1Test with AoCPart2Test {
           .map(t => ((t._1._1 + vv.head.size * x, t._1._2 + vv.size * y), (t._2 + x + y) % 10 + (t._2 + x + y) / 10)))).toMap
   }
 
-  def findPath(start: (Int, Int), end: (Int, Int), maps: Map[(Int, Int), Long]): (Seq[(Int, Int)], Long) = {
+  def costBetween(start: (Int, Int), end: (Int, Int), maps: Map[(Int, Int), Long]): Long = {
     val directions = Seq((-1, 0), (0, -1), (1, 0), (0, 1))
-    val frontier = mutable.PriorityQueue[(Seq[(Int, Int)], Long, Long)]()(Ordering.Long.reverse.on(_._3))
+    val frontier = mutable.PriorityQueue[((Int, Int), Long, Long)]()(Ordering.Long.reverse.on(_._3))
     val checked = mutable.HashSet[(Int, Int)]()
-    var path: (Seq[(Int, Int)], Long) = null
-    frontier.addOne((Seq(start), 0, 0 + manhattan(start, end)))
-    while (frontier.nonEmpty && path == null) {
+    var cost: Option[Long] = None
+    frontier.addOne((start, 0, 0 + manhattan(start, end)))
+    while (frontier.nonEmpty && cost.isEmpty) {
       val curr = frontier.dequeue()
-      if (curr._1.last == end) {
-        path = (curr._1, curr._2)
+      if (curr._1 == end) {
+        cost = Option(curr._2)
       }
-      if (!checked.contains(curr._1.last)) {
-        checked.add(curr._1.last)
+      if (!checked.contains(curr._1)) {
+        checked.add(curr._1)
         val around = directions
-          .map(d => (curr._1.last._1 + d._1, curr._1.last._2 + d._2))
+          .map(d => (curr._1._1 + d._1, curr._1._2 + d._2))
           .filterNot(p => checked.contains(p))
-          .map(p => curr._1 ++ Seq(p))
-          .flatMap(p => maps.get(p.last).map(_ + curr._2).map(r => (p, r, r + manhattan(p.last, end))))
+          .flatMap(p => maps.get(p).map(_ + curr._2).map(r => (p, r, r + manhattan(p, end))))
         frontier.addAll(around)
       }
     }
-    path
+    cost.get
   }
 
   private def manhattan(a: (Int, Int), b: (Int, Int)): Long = math.abs(a._1 - b._1) + math.abs(a._2 - b._2)
