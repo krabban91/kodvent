73119ae3e2aad09a83575c1082a988ac7989081b
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-18 06:13:01 +0100

    :recycle: Added case classes for SnailExp

--- aoc2021/src/main/scala/Day18.scala
+++ aoc2021/src/main/scala/Day18.scala
@@ -8,9 +8,156 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    val vv: Seq[Seq[String]] = strings.map(_.split(",").toSeq)
+    SnailPair(strings.head)
+    val vv: Seq[SnailPair] = strings.map(SnailPair(_))
     -1
   }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  trait SnailExp {
+    def magnitude: Long
+
+    def add(snailPair: SnailExp): SnailExp = Tree(this.reduceAll, snailPair.reduceAll).reduceAll
+
+    def addAll(snails: Seq[SnailExp]): SnailExp = snails.foldLeft(this)((sp, o) => sp.add(o))
+
+    def reduceAll: SnailExp
+
+    def reduce: Option[SnailExp]
+
+    def explode(level: Int): Option[(SnailExp, (Option[Long], Option[Long]))]
+
+    def passRight(value: Long): Option[SnailExp]
+
+    def passLeft(value: Long): Option[SnailExp]
+
+    def split: Option[SnailExp]
+
+    def asLeaf: Option[Leaf]
+  }
+
+  case class Leaf(value: Long) extends SnailExp {
+
+    override def toString: String = value.toString
+
+    override def magnitude: Long = value
+
+    override def reduceAll: SnailExp = this
+
+    override def reduce: Option[SnailExp] = None
+
+    override def explode(level: Int): Option[(SnailExp, (Option[Long], Option[Long]))] = None
+
+    override def passRight(value: Long): Option[SnailExp] = Option(Leaf(this.value + value))
+
+    override def passLeft(value: Long): Option[SnailExp] = Option(Leaf(this.value + value))
+
+    override def split: Option[SnailExp] = {
+      Option(Tree(Leaf(value / 2), Leaf(value / 2 + value % 2)))
+        .filter(_ => value > 9)
+    }
+
+    override def asLeaf: Option[Leaf] = Option(this)
+  }
+
+  case class Tree(left: SnailExp, right: SnailExp) extends SnailExp {
+    override def toString: String = s"[${left},${right}]"
+
+    override def magnitude: Long = 3 * left.magnitude + 2 * right.magnitude
+
+    override def reduceAll: SnailExp = {
+      var curr: Option[SnailExp] = Option(this)
+      while (curr.isDefined) {
+        val next = curr.get.reduce
+        if (next.isEmpty)
+          return curr.get
+        curr = next
+      }
+      this
+    }
+
+    override def reduce: Option[SnailExp] = {
+      explode(0).map(_._1).orElse(split)
+    }
+
+    override def explode(level: Int): Option[(SnailExp, (Option[Long], Option[Long]))] = {
+      Option(Leaf(0), (left.asLeaf.map(_.value), right.asLeaf.map(_.value)))
+        .filter(v => level >= 4 & v._2._1.isDefined && v._2._2.isDefined)
+        .orElse(left.explode(level + 1)
+          .map(l => l._2._2.flatMap(right.passRight)
+            .map(passed => (Tree(l._1, passed), (l._2._1, None)))
+            .getOrElse((Tree(l._1, right), l._2)))
+          .orElse(right.explode(level + 1)
+            .map(r => r._2._1.flatMap(left.passLeft)
+              .map(passed => (Tree(passed, r._1), (None, r._2._2)))
+              .getOrElse((Tree(left, r._1), r._2)))))
+    }
+
+    override def passRight(value: Long): Option[SnailExp] = {
+      left.passRight(value)
+        .map(Tree(_, right))
+        .orElse(right.passRight(value)
+          .map(Tree(left, _)))
+    }
+
+    override def passLeft(value: Long): Option[SnailExp] = {
+      right.passLeft(value)
+        .map(Tree(left, _))
+        .orElse(left.passLeft(value)
+          .map(Tree(_, right)))
+    }
+
+
+    override def split: Option[SnailExp] = {
+      left.split.map(Tree(_, right))
+        .orElse(right.split
+          .map(Tree(left, _)))
+    }
+
+    override def asLeaf: Option[Leaf] = None
+  }
+
+  object SnailExp {
+    def apply(string: String): SnailExp = {
+      if (string.head == '[') {
+        val in = string.substring(1, string.length - 1)
+        val split = in.split(",")
+        val splL = split.tail
+          .foldLeft(initial(split.head))(collector)._2
+        val rIn = split.drop(splL.size)
+        val splR = rIn.tail
+          .foldLeft(initial(rIn.head))(collector)._2
+        Tree(splL.reduce(_ + "," + _), splR.reduce(_ + "," + _))
+      } else {
+        Leaf(string)
+      }
+    }
+
+    private def initial(head: String): (Int, Seq[String]) = {
+      (head.count(_ == '['), Seq(head))
+    }
+
+    private def collector(t: (Int, Seq[String]), s: String): (Int, Seq[String]) = {
+      if (t._1 == 0) {
+        t
+      } else if (s.contains('[')) {
+        (t._1 + s.count(_ == '['), t._2 ++ Seq(s))
+      } else {
+        (t._1 - s.count(_ == ']'), t._2 ++ Seq(s))
+      }
+    }
+  }
+
+  object Leaf {
+    def apply(string: String): Leaf = {
+      Leaf(string.takeWhile(_.isDigit).toLong)
+    }
+  }
+
+  object Tree {
+    def apply(l: String, r: String): Tree = {
+      Tree(SnailExp(l), SnailExp(r))
+    }
+  }
 }
--- aoc2021/src/test/scala/Day18Spec.scala
+++ aoc2021/src/test/scala/Day18Spec.scala
@@ -2,6 +2,179 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day18Spec extends AnyFlatSpec with Matchers {
+
+  behavior of "SnailExp(string)"
+  it should "parse to same e1" in {
+    val i = "[[[[[9,8],1],2],3],4]"
+    Day18.SnailExp(i).toString shouldBe i
+  }
+  it should "parse to same e2" in {
+    val i = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+    Day18.SnailExp(i).toString shouldBe i
+  }
+  it should "parse to same e3" in {
+    val i = "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
+    Day18.SnailExp(i).toString shouldBe i
+  }
+
+  behavior of "explode"
+  it should "work e1" in {
+    val i = "[[[[[9,8],1],2],3],4]"
+    val e = "[[[[0,9],2],3],4]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.explode(0)
+    res.isDefined shouldBe true
+    res.get._1 shouldBe expected
+  }
+
+  it should "work e2" in {
+    val i = "[7,[6,[5,[4,[3,2]]]]]"
+    val e = "[7,[6,[5,[7,0]]]]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.explode(0)
+    res.isDefined shouldBe true
+    res.get._1 shouldBe expected
+  }
+  it should "work e3" in {
+    val i = "[[6,[5,[4,[3,2]]]],1]"
+    val e = "[[6,[5,[7,0]]],3]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.explode(0)
+    res.isDefined shouldBe true
+    res.get._1 shouldBe expected
+  }
+  it should "work e4" in {
+    val i = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+    val e = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.explode(0)
+    res.isDefined shouldBe true
+    res.get._1 shouldBe expected
+  }
+  it should "work e5" in {
+    val i = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    val e = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.explode(0)
+    res.isDefined shouldBe true
+    res.get._1 shouldBe expected
+  }
+
+  behavior of "split"
+  it should "work e1" in {
+    val i = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+    val e = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.split
+    res.isDefined shouldBe true
+    res.get shouldBe expected
+  }
+
+  behavior of "magnitude"
+  it should "work e1" in {
+    val i = "[9,1]"
+    val input = Day18.SnailExp(i)
+    val expected = 29
+    input.magnitude shouldBe expected
+  }
+
+  behavior of "reduceAll"
+  it should "work e1" in {
+    val i = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+    val e = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+    val input = Day18.SnailExp(i)
+    val expected = Day18.SnailExp(e)
+    val res = input.reduceAll
+    res shouldBe expected
+  }
+
+  behavior of "add"
+  it should "work e1" in {
+    val i = "[[[[4,3],4],4],[7,[[8,4],9]]]"
+    val add = "[1,1]"
+    val e = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+    val input = Day18.SnailExp(i)
+    val added = Day18.SnailExp(add)
+    val expected = Day18.SnailExp(e)
+    val res = input.add(added)
+    res shouldBe expected
+  }
+  it should "work e2" in {
+    val i = "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
+    val add = "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+    val e = "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
+    val input = Day18.SnailExp(i)
+    val added = Day18.SnailExp(add)
+    val expected = Day18.SnailExp(e)
+    val res = input.add(added)
+    res shouldBe expected
+  }
+
+  behavior of "addAll"
+  it should "work e1" in {
+    val i = """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]""".stripMargin.split("\n")
+    val e = "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+
+    val inputs = i.map(Day18.SnailExp(_))
+    val expected = Day18.SnailExp(e)
+    val res = inputs.head.addAll(inputs.tail)
+    res shouldBe expected
+  }
+  it should "work e2" in {
+    val i = """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]
+              |[5,5]""".stripMargin.split("\n")
+    val e = "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+
+    val inputs = i.map(Day18.SnailExp(_))
+    val expected = Day18.SnailExp(e)
+    val res = inputs.head.addAll(inputs.tail)
+    res shouldBe expected
+  }
+  it should "work e3" in {
+    val i = """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]
+              |[5,5]
+              |[6,6]""".stripMargin.split("\n")
+    val e = "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+
+    val inputs = i.map(Day18.SnailExp(_))
+    val expected = Day18.SnailExp(e)
+    val res = inputs.head.addAll(inputs.tail)
+    res shouldBe expected
+  }
+  it should "work e4" in {
+    val i = """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+              |[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+              |[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+              |[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+              |[7,[5,[[3,8],[1,4]]]]
+              |[[2,[2,2]],[8,[8,1]]]
+              |[2,9]
+              |[1,[[[9,3],9],[[9,0],[0,7]]]]
+              |[[[5,[7,4]],7],1]
+              |[[[[4,2],2],6],[8,7]]""".stripMargin.split("\n")
+    val e = "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
+
+    val inputs = i.map(Day18.SnailExp(_))
+    val expected = Day18.SnailExp(e)
+    val res = inputs.head.addAll(inputs.tail)
+    res shouldBe expected
+  }
+
   behavior of "Part 1"
   it should "test input" in {
     Day18.part1TestResult shouldEqual -1
