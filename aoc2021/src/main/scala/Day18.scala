490e0a0c8a583369fde664968372791d8555e5f1
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-18 18:45:47 +0100

    :mute: Day 18 - remove logging

--- aoc2021/src/main/scala/Day18.scala
+++ aoc2021/src/main/scala/Day18.scala
@@ -2,11 +2,6 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day18 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     SnailExp(strings.head)
     val vv: Seq[SnailExp] = strings.map(SnailExp(_))
