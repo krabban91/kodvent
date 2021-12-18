4a58d2309c4ff8a971b65ef12bec7d729e05f076
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-18 21:26:08 +0100

    :package: Year 2021 Day 19 - Preparations

---
+++ aoc2021/src/test/scala/Day19Spec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day19Spec extends AnyFlatSpec with Matchers {
+  behavior of "Part 1"
+  it should "Test" in {
+    Day19.part1TestResult shouldEqual -1
+  }
+  it should "Real" in {
+    Day19.part1Result shouldEqual -1
+  }
+
+  behavior of "Part 2"
+  it should "Test" in {
+    Day19.part2TestResult shouldEqual -1
+  }
+  it should "Real" in {
+    Day19.part2Result shouldEqual -1
+  }
+}
