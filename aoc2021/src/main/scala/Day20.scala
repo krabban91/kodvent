ea412dad5ac6e2db54ea5323e11f0f1948ed99d8
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-20 06:25:52 +0100

    :sparkles: Day 20 - Completed part 1

--- aoc2021/src/main/scala/Day20.scala
+++ aoc2021/src/main/scala/Day20.scala
@@ -1,4 +1,10 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.grid.Grid
+
+import java.awt.Point
+import java.util.stream.Collectors
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
 
 object Day20 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -7,7 +13,36 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val imageEnhancer = strings.head.chars().mapToObj(c => if (c == '#') 1 else 0)
+      .collect(Collectors.toList[Int]()).asScala
+    val initImage: Grid[Int] = new Grid[Int](strings.tail.tail
+      .map(s => s.chars().mapToObj(c => if (c == '#') 1 else 0)
+        .collect(Collectors.toList[Int]())).toList.asJava)
+    var curr = initImage
+    for (i <- 1 to 2) {
+      curr = curr.expand(p => 0, 10)
+      val next = curr
+        .map((i, p) => {
+          val theNine = curr.getSurroundingTilesWithPoints(p, true)
+          val s = theNine.asScala.map(_.getValue.toString).reduce(_ + _)
+          imageEnhancer(Integer.parseInt(s, 2))
+        })
+      curr = next
+    }
+    val hm = mutable.HashMap[Point, Int]()
+
+    curr.forEach((i, p) => {
+      val w = curr.width()
+      val h = curr.height()
+      if (p.x > 10 && p.x < w - 10 - 1 && p.y > 10 && p.y < h - 10 - 1) {
+        hm.put(p, i)
+      }
+    })
+    hm.values.sum
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+
 }
--- aoc2021/src/test/scala/Day20Spec.scala
+++ aoc2021/src/test/scala/Day20Spec.scala
@@ -4,10 +4,10 @@ import org.scalatest.matchers.should.Matchers
 class Day20Spec extends AnyFlatSpec with Matchers {
   behavior of "Part 1"
   it should "Test" in {
-    Day20.part1TestResult shouldEqual -1
+    Day20.part1TestResult shouldEqual 35
   }
   it should "Real" in {
-    Day20.part1Result shouldEqual -1
+    Day20.part1Result shouldEqual 5275
   }
 
   behavior of "Part 2"
