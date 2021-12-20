7273ace8fa7fa0eb37ee973d1c4d7aa6c548e6eb
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-20 07:18:24 +0100

    :sparkles: Day 20 - completed part 2

--- aoc2021/src/main/scala/Day20.scala
+++ aoc2021/src/main/scala/Day20.scala
@@ -42,7 +42,33 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
     hm.values.sum
   }
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    val imageEnhancer = strings.head.chars().mapToObj(c => if (c == '#') 1 else 0)
+      .collect(Collectors.toList[Int]()).asScala
+    val initImage: Grid[Int] = new Grid[Int](strings.tail.tail
+      .map(s => s.chars().mapToObj(c =>if (c == '#') 1 else 0)
+        .collect(Collectors.toList[Int]())).toList.asJava)
+    var curr = initImage
+    val step = 10
+    for (i <- 1 to 50){
+      curr = curr.expand(p => 0, step)
+      val next = curr
+        .map((i, p) =>{
+          val theNine = curr.getSurroundingTilesWithPoints(p, true)
+          val s = theNine.asScala.map(_.getValue.toString).reduce(_+_)
+          imageEnhancer(Integer.parseInt(s, 2))
+        })
+
+      if(i % 2 == 0){
+        val ls = next.getRaw.asScala.map(_.asScala)
+        val minLs = ls.dropRight(step+1).drop(step).map(l => l.dropRight(step+1).drop(step).asJava).asJava
+        curr = new Grid[Int](minLs)
+      } else {
+        curr = next
+      }
+    }
+    curr.sum(i => i)
+  }
 
 
 }
