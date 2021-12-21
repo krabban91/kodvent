8b057694718f2ecae2d49ea055a9b5129d25b335
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-21 06:40:47 +0100

    :sparkles: Day 21 - Completed part 1

--- aoc2021/src/test/scala/Day21Spec.scala
+++ aoc2021/src/test/scala/Day21Spec.scala
@@ -4,10 +4,10 @@ import org.scalatest.matchers.should.Matchers
 class Day21Spec extends AnyFlatSpec with Matchers {
   behavior of "Part 1"
   it should "Test" in {
-    Day21.part1TestResult shouldEqual -1
+    Day21.part1TestResult shouldEqual 739785
   }
   it should "Real" in {
-    Day21.part1Result shouldEqual -1
+    Day21.part1Result shouldEqual 512442
   }
 
   behavior of "Part 2"
