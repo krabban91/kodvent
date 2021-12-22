0b13a7f2e28fb1318d6884f771382526f01ae1d1
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-22 06:12:29 +0100

    :sparkles: Day 22 - completed part 1

--- aoc2021/src/main/scala/Day22.scala
+++ aoc2021/src/main/scala/Day22.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day22 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,7 +9,33 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = -1
+  override def part1(strings: Seq[String]): Long = {
+    val v = strings.map(Instruction(_))
+    val map = v.foldLeft(mutable.HashMap[(Int, Int, Int), Boolean]())((m, instr) => {
+      (math.max(instr.xs._1, -50) to math.min(50, instr.xs._2))
+        .foreach(x => (math.max(-50, instr.ys._1) to math.min(50, instr.ys._2))
+          .foreach(y => (math.max(-50, instr.zs._1) to math.min(50, instr.zs._2))
+            .foreach(z => m.put((x, y, z), instr.on))))
+      m
+    })
+    map.count(x => x._2)
+  }
 
   override def part2(strings: Seq[String]): Long = -1
+
+  case class Instruction(on: Boolean, xs: (Int, Int), ys: (Int, Int), zs: (Int, Int)) {
+
+  }
+
+  object Instruction {
+    def apply(string: String): Instruction = {
+      val s = string.split(" ")
+      val on = s.head == "on"
+      val ss = s.last.split(",")
+      val xs = ss.head.split("=").last.split("\\.\\.").map(_.toInt)
+      val ys = ss.tail.head.split("=").last.split("\\.\\.").map(_.toInt)
+      val zs = ss.last.split("=").last.split("\\.\\.").map(_.toInt)
+      Instruction(on, (xs.head, xs.last), (ys.head, ys.last), (zs.head, zs.last))
+    }
+  }
 }
