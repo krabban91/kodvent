400ccc1af7ce1667fe9a384053e095462a99e7d0
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-22 06:52:28 +0100

    :sparkles: Day 22 - completed part 2
    * In order to know how to perform this,
      I had to spend some time on google
      - to subtract the intersection of existing
        is actually rather smart.

--- aoc2021/src/main/scala/Day22.scala
+++ aoc2021/src/main/scala/Day22.scala
@@ -21,10 +21,25 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
     map.count(x => x._2)
   }
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = countActive(strings.map(Instruction(_)))
+
+  def countActive(instructions: Seq[Instruction]): Long = instructions
+    .foldLeft(Seq[Instruction]())((cubes, instr) => cubes ++ cubes.flatMap(b => instr.intersection(b, !b.on)) ++ (if (instr.on) Seq(instr) else Seq()))
+    .map(_.value)
+    .sum
 
   case class Instruction(on: Boolean, xs: (Int, Int), ys: (Int, Int), zs: (Int, Int)) {
+    def intersection(other: Instruction, on: Boolean): Option[Instruction] = Option(Instruction(on,
+      (math.max(this.xs._1, other.xs._1), math.min(this.xs._2, other.xs._2)),
+      (math.max(this.ys._1, other.ys._1), math.min(this.ys._2, other.ys._2)),
+      (math.max(this.zs._1, other.zs._1), math.min(this.zs._2, other.zs._2))
+    )).filter(_.isValid)
 
+    def value: Long = {
+      (if (on) 1 else -1) * (math.abs(this.xs._2 - this.xs._1) + 1).toLong * (math.abs(this.ys._2 - this.ys._1) + 1).toLong * (math.abs(this.zs._2 - this.zs._1).toLong + 1)
+    }
+
+    private def isValid: Boolean = xs._1 <= xs._2 && ys._1 <= ys._2 && zs._1 <= zs._2
   }
 
   object Instruction {
--- aoc2021/src/test/scala/Day22Spec.scala
+++ aoc2021/src/test/scala/Day22Spec.scala
@@ -1,7 +1,9 @@
+import Day22.Instruction
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day22Spec extends AnyFlatSpec with Matchers {
+
   behavior of "Part 1"
   it should "Test" in {
     Day22.part1TestResult shouldEqual 474140
@@ -12,9 +14,60 @@ class Day22Spec extends AnyFlatSpec with Matchers {
 
   behavior of "Part 2"
   it should "Test" in {
-    Day22.part2TestResult shouldEqual -1
+    Day22.part2TestResult shouldEqual 2758514936282235L
   }
   it should "Real" in {
-    Day22.part2Result shouldEqual -1
+    Day22.part2Result shouldEqual 1233304599156793L
   }
+
+  behavior of "Day22.countActive"
+  it should "handle multiple overlapping on instructions in 1D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 0), (0, 0)),
+      Instruction(on = true, (0, 4), (0, 0), (0, 0)),
+      Instruction(on = true, (0, 4), (0, 0), (0, 0))
+    )
+    Day22.countActive(ins) shouldBe 5
+  }
+  it should "handle off instructions in 1D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 0), (0, 0)),
+      Instruction(on = false, (0, 4), (0, 0), (0, 0)),
+      Instruction(on = true, (0, 3), (0, 0), (0, 0))
+    )
+    Day22.countActive(ins) shouldBe 4
+  }
+  it should "handle multiple overlapping on instructions in 2D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 1), (0, 0)),
+      Instruction(on = true, (0, 4), (0, 1), (0, 0)),
+      Instruction(on = true, (0, 4), (0, 0), (0, 0))
+    )
+    Day22.countActive(ins) shouldBe 10
+  }
+  it should "handle off instructions in 2D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 1), (0, 0)),
+      Instruction(on = false, (0, 4), (0, 1), (0, 0)),
+      Instruction(on = true, (0, 3), (0, 0), (0, 0))
+    )
+    Day22.countActive(ins) shouldBe 4
+  }
+  it should "handle multiple overlapping on instructions in 3D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 1), (0, 1)),
+      Instruction(on = true, (0, 4), (0, 1), (0, 1)),
+      Instruction(on = true, (0, 4), (0, 0), (0, 1))
+    )
+    Day22.countActive(ins) shouldBe 20
+  }
+  it should "handle off instructions in 3D" in {
+    val ins = Seq(
+      Instruction(on = true, (0, 4), (0, 1), (0, 1)),
+      Instruction(on = false, (0, 4), (0, 1), (0, 1)),
+      Instruction(on = true, (0, 3), (0, 0), (0, 1))
+    )
+    Day22.countActive(ins) shouldBe 8
+  }
+
 }
