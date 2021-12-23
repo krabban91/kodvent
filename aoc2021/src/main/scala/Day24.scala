8f379bc5b557410208fb5bee4226f07caa35bc10
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-23 21:06:38 +0100

    :package: Day 24 - Preparations

---
+++ aoc2021/src/main/scala/Day24.scala
@@ -0,0 +1,12 @@
+import aoc.string.{AoCPart1String, AoCPart2String}
+
+object Day24 extends App with AoCPart1String with AoCPart2String {
+
+  printResultPart1
+  printResultPart2
+
+  override def part1(strings: Seq[String]): String = "N/A"
+
+
+  override def part2(strings: Seq[String]): String = "N/A"
+}
