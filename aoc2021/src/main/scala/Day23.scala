ee5eac9bd4abc25300eaf8fc615bcd97747aee48
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-23 11:04:12 +0100

    :sparkles: Day 23 - Completed part 2

--- aoc2021/src/main/scala/Day23.scala
+++ aoc2021/src/main/scala/Day23.scala
@@ -2,6 +2,7 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 import krabban91.kodvent.kodvent.utilities.logging.{LogUtils, Loggable}
 
 import java.awt.Point
+import java.util.Date
 import scala.collection.mutable
 import scala.jdk.CollectionConverters.MapHasAsJava
 
@@ -29,7 +30,7 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
           reachedGoal = Option((pods, cost))
         } else {
           val next = pods
-            .flatMap(_.possibleMoves(pods))
+            .flatMap(_.possibleMoves(pods, part2 = false))
             .filterNot(t => checked.contains(t._1))
             .map(t => (t._1, cost + t._2, cost + t._2 + t._1.map(_.heuristic(t._1)).sum, pods))
           pq.addAll(next)
@@ -53,27 +54,69 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
   }
 
   private def logLocs(pods: Seq[Amphipod], cost: Long, heuristic: Long) = {
+    println(new Date().toString)
     println(s"cost: $cost, heuristic: $heuristic, pods: ")
     val mm = pods.map(p => (new Point(p.location._1, p.location._2), p)).toMap
     val logger = new LogUtils[Amphipod]()
     println(logger.mapToText(mm.asJava, p => if (p == null) "." else p.name))
   }
 
-  private def getAmphipods(strings: Seq[String]): Seq[Amphipod] = {
-    strings.zipWithIndex.flatMap(ty => ty._1.zipWithIndex.filterNot(tx => Seq('#', '.', ' ').contains(tx._1)).map(tx => Amphipod(tx._1.toString, (tx._2, ty._2))))
+  private def getAmphipods(strings: Seq[String], part2: Boolean = false): Seq[Amphipod] = {
+    (if (part2) (strings.take(3) ++ Seq("  #D#C#B#A#  ", "  #D#B#A#C#  ") ++ strings.takeRight(2)) else strings)
+      .zipWithIndex.flatMap(ty => ty._1.zipWithIndex.filterNot(tx => Seq('#', '.', ' ').contains(tx._1)).map(tx => Amphipod(tx._1.toString, (tx._2, ty._2))))
   }
 
-  override def part2(strings: Seq[String]): Long = -1
+  override def part2(strings: Seq[String]): Long = {
+    val s = getAmphipods(strings, part2 = true)
+    val pq = mutable.PriorityQueue[(Seq[Amphipod], Long, Long)]()(Ordering.Long.reverse.on(_._3))
+    val checked = mutable.HashSet[Seq[Amphipod]]()
+    pq.addOne((s, 0, s.map(_.heuristic(s, part2 = true)).sum))
+    var reachedGoal: Option[(Seq[Amphipod], Long)] = None
+    while (pq.nonEmpty && reachedGoal.isEmpty) {
+      val (pods, cost, heur) = pq.dequeue()
+      //logLocs(pods, cost, heur)
+      if (!checked.contains(pods)) {
+        checked.add(pods)
+        if(checked.size %100000 == 0){
 
-  def isGoal(locations: Seq[Amphipod]): Boolean = {
+          logLocs(pods, cost, heur)
+        }
+        if (isGoal(pods, part2 = true)) {
+          reachedGoal = Option((pods, cost))
+        } else {
+          val next = pods
+            .flatMap(_.possibleMoves(pods, part2 = true))
+            .filterNot(t => checked.contains(t._1))
+            .map(t => (t._1, cost + t._2, cost + t._2 + t._1.map(_.heuristic(t._1, part2 = true)).sum))
+          pq.addAll(next)
+        }
+      }
+    }
+    //logPath(reachedGoal.get._1, prevMap.toMap, 0, reachedGoal.get._2)
+
+    reachedGoal.get._2
+  }
+
+
+  def wantedLocations(name: String, part2: Boolean = false): Seq[(Int, Int)] = {
+    name match {
+      case "A" => Seq((3, 2), (3, 3)) ++ (if (part2) Seq((3, 4), (3, 5)) else Seq())
+      case "B" => Seq((5, 2), (5, 3)) ++ (if (part2) Seq((5, 4), (5, 5)) else Seq())
+      case "C" => Seq((7, 2), (7, 3)) ++ (if (part2) Seq((7, 4), (7, 5)) else Seq())
+      case "D" => Seq((9, 2), (9, 3)) ++ (if (part2) Seq((9, 4), (9, 5)) else Seq())
+    }
+  }
+
+  def isGoal(locations: Seq[Amphipod], part2: Boolean = false): Boolean = {
     val as = locations.filter(_.name == "A").map(_.location).sortBy(_._2)
     val bs = locations.filter(_.name == "B").map(_.location).sortBy(_._2)
     val cs = locations.filter(_.name == "C").map(_.location).sortBy(_._2)
     val ds = locations.filter(_.name == "D").map(_.location).sortBy(_._2)
-    as == Seq((3, 2), (3, 3)) &&
-      bs == Seq((5, 2), (5, 3)) &&
-      cs == Seq((7, 2), (7, 3)) &&
-      ds == Seq((9, 2), (9, 3))
+
+    as == wantedLocations("A", part2) &&
+      bs == wantedLocations("B", part2) &&
+      cs == wantedLocations("C", part2) &&
+      ds == wantedLocations("D", part2)
   }
 
   case class Amphipod(name: String, location: (Int, Int)) extends Loggable {
@@ -87,9 +130,10 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
       }
     }
 
-    def heuristic(currentLocations: Seq[Amphipod]): Long = {
+    def heuristic(currentLocations: Seq[Amphipod], part2: Boolean = false): Long = {
       val wantedX = Map("A" -> 3, "B" -> 5, "C" -> 7, "D" -> 9)
       val x = wantedX(name)
+      //val missing = wantedLocations(name, part2).count(p => currentLocations.find(a => a.location == p).forall(_.name!=name))
       val steps = if (location._1 != x) {
         if (location._2 == 1) {
           1 + math.abs(x - location._1)
@@ -97,8 +141,10 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
           location._2 - 1 + 1 + math.abs(x - location._1)
         }
       } else {
-        if (location._2 == 2 && currentLocations.find(p => p.location == (location._1, location._2 + 1)).forall(_.name != name)) {
-          2 + 2
+        val under = (location._2 + 1 to (if (part2) 5 else 3))
+          .map(y => currentLocations.find(p => p.location == (location._1, y)))
+        if (under.exists(o => o.exists(ot => ot.name != name))) {
+          ((location._2 - 1) + 1) + 2
         } else {
           0
         }
@@ -106,63 +152,78 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
       energyToMove * steps
     }
 
-    def possibleMoves(currentLocations: Seq[Amphipod]): Seq[(Seq[Amphipod], Long)] = {
+    def possibleMoves(currentLocations: Seq[Amphipod], part2: Boolean): Seq[(Seq[Amphipod], Long)] = {
       val currY = this.location._2
       val currX = this.location._1
       val without = currentLocations.filterNot(_ == this)
       val potential = mutable.ListBuffer[(Amphipod, Long)]()
       val hallwayLocs = Seq(1, 2, 4, 6, 8, 10, 11)
       if (currY == 1) {
-        val w = wantedLocations(name)
+        val w = wantedLocations(name, part2 = part2)
         w.foreach(t => {
-          val up = without.find(p => p.location == (t._1, 2))
-          val down = without.find(p => p.location == (t._1, 3))
-          if (t._2 == 2) {
-            if (down.exists(p => p.name == name)) {
-              if (t._1 < currX) {
-                val inWay = hallwayLocs.filter(v => v >= t._1 && v <= currX)
-                if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-                  potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
-                }
-              } else {
-                val inWay = hallwayLocs.filter(v => v >= currX && v <= t._1)
-                if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-                  potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
-                }
-              }
+          val one = without.find(p => p.location == (t._1, 2))
+          val two = without.find(p => p.location == (t._1, 3))
+          val three = without.find(p => p.location == (t._1, 4))
+          val four = without.find(p => p.location == (t._1, 5))
+          val (over, goal, under) = if (part2) {
+            if (t._2 == 2) {
+              (Seq(), one, Seq(two, three, four))
+            } else if (t._2 == 3) {
+              (Seq(one), two, Seq(three, four))
+            }
+            else if (t._2 == 4) {
+              (Seq(one, two), three, Seq(four))
+            }
+            else {
+              (Seq(one, two, three), four, Seq())
             }
           } else {
-            if (up.isEmpty) {
-              if (t._1 < currX) {
-                val inWay = hallwayLocs.filter(v => v >= t._1 && v <= currX)
-                if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-                  potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
-                }
-              } else {
-                val inWay = hallwayLocs.filter(v => v >= currX && v <= t._1)
-                if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-                  potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
-                }
+            if (t._2 == 2) {
+              (Seq(), one, Seq(two))
+            } else {
+              (Seq(one), two, Seq())
+            }
+          }
+
+          if (goal.isEmpty && over.flatten.isEmpty && under.forall(_.exists(p => p.name == name))) {
+            if (t._1 < currX) {
+              val inWay = hallwayLocs.filter(v => v >= t._1 && v <= currX)
+              if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
+                potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
+              }
+            } else {
+              val inWay = hallwayLocs.filter(v => v >= currX && v <= t._1)
+              if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
+                potential.addOne((Amphipod(name, t), (math.abs(currX - t._1) + math.abs(currY - t._2)) * energyToMove))
               }
             }
           }
         })
-      } else if (currY == 2) {
-        for (x <- hallwayLocs) {
-          if (x < currX) {
-            val inWay = hallwayLocs.filter(v => v >= x && v <= currX)
-            if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-              potential.addOne((Amphipod(name, (x, 1)), (math.abs(currX - x) + math.abs(currY - 1)) * energyToMove))
-            }
+      } else {
+        val one = without.find(p => p.location == (location._1, 2))
+        val two = without.find(p => p.location == (location._1, 3))
+        val three = without.find(p => p.location == (location._1, 4))
+        val four = without.find(p => p.location == (location._1, 5))
+        val (over, under) = if (part2) {
+          if (currY == 2) {
+            (Seq(), Seq(two, three, four))
+          } else if (currY == 3) {
+            (Seq(one), Seq(three, four))
+          }
+          else if (currY == 4) {
+            (Seq(one, two), Seq(four))
+          }
+          else {
+            (Seq(one, three, four), Seq())
+          }
+        } else {
+          if (currY == 2) {
+            (Seq(), Seq(two))
           } else {
-            val inWay = hallwayLocs.filter(v => v >= currX && v <= x)
-            if (inWay.forall(ox => !without.exists(_.location == (ox, 1)))) {
-              potential.addOne((Amphipod(name, (x, 1)), (math.abs(currX - x) + math.abs(currY - 1)) * energyToMove))
-            }
+            (Seq(one), Seq())
           }
         }
-      } else {
-        if (!wantedLocations(name).contains(location) && !without.exists(_.location == (currX, currY - 1))) {
+        if (over.forall(_.isEmpty)) {
           for (x <- hallwayLocs) {
             if (x < currX) {
               val inWay = hallwayLocs.filter(v => v >= x && v <= currX)
@@ -180,15 +241,6 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
       }
       val value = potential.toSeq.filterNot(p => currentLocations.exists(o => p._1.location == o.location))
       value.map(t => (without ++ Seq(t._1), t._2))
-    }
-
-    def wantedLocations(name: String): Seq[(Int, Int)] = {
-      name match {
-        case "A" => Seq((3, 2), (3, 3))
-        case "B" => Seq((5, 2), (5, 3))
-        case "C" => Seq((7, 2), (7, 3))
-        case "D" => Seq((9, 2), (9, 3))
-      }
     }
 
     override def showTile(): String = this.name
