37fe7facf3989f7b0e87f60795fc1840fbf30ae1
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-24 23:19:55 +0100

    :package: Year 2021 Day 25 - Preparations

---
+++ aoc2021/src/main/scala/Day25.scala
@@ -0,0 +1,13 @@
+import aoc.numeric.AoCPart1Test
+
+object Day25 extends App with AoCPart1Test {
+
+  printResultPart1Test
+  printResultPart1
+
+  override def part1(strings: Seq[String]): Long = {
+    val v = strings.map(_ => 1)
+    -1
+  }
+
+}
---
+++ aoc2021/src/test/scala/Day25Spec.scala
@@ -0,0 +1,12 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day25Spec extends AnyFlatSpec with Matchers {
+  behavior of "Part 1"
+  it should "Test" in {
+    Day25.part1TestResult shouldEqual -1
+  }
+  it should "Real" in {
+    Day25.part1Result shouldEqual -1
+  }
+}
