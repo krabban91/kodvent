e97cd9b17cbda218ec763f7e9b95b3a0cd365c9a
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-25 06:56:42 +0100

    :sparkles: Year 2021 Day 25 - Completed part 1

--- aoc2021/src/main/scala/Day25.scala
+++ aoc2021/src/main/scala/Day25.scala
@@ -1,13 +1,68 @@
 import aoc.numeric.AoCPart1Test
+import krabban91.kodvent.kodvent.utilities.logging.LogUtils
+
+import java.awt.Point
+import scala.collection.mutable
+import scala.jdk.CollectionConverters.MapHasAsJava
 
 object Day25 extends App with AoCPart1Test {
 
   printResultPart1Test
   printResultPart1
+  private val debug = false
 
   override def part1(strings: Seq[String]): Long = {
-    val v = strings.map(_ => 1)
-    -1
+    val v = strings.zipWithIndex.flatMap(ty => ty._1.zipWithIndex.filterNot(_._1 == '.').map(tx => ((tx._2, ty._2), if(tx._1 == 'v') (0,1) else (1,0)))).toMap
+    val maxY = strings.size - 1
+    val maxX = strings.head.length - 1
+    var curr = v
+    var noSteps = 1
+    if(debug){
+      println(noSteps)
+      println(new LogUtils[(Int, Int)]().mapToText(curr.map(t => (new Point(t._1._1, t._1._2), t._2)).asJava, v => if(v == (0,1)) "v" else if(v == (1,0)) ">" else "."))
+    }
+    var next = step(curr, maxX, maxY)
+    while (curr != next) {
+      curr = next
+      if(debug){
+        println(noSteps)
+        println(new LogUtils[(Int, Int)]().mapToText(curr.map(t => (new Point(t._1._1, t._1._2), t._2)).asJava, v => if(v == (0,1)) "v" else if(v == (1,0)) ">" else "."))
+      }
+      noSteps += 1
+      next = step(curr, maxX, maxY)
+
+    }
+    noSteps
+  }
+
+  def step(curr: Map[(Int, Int), (Int, Int)], maxX: Int, maxY: Int): Map[(Int, Int), (Int, Int)] = {
+    val next = mutable.HashMap[(Int, Int), (Int, Int)]()
+    val preSouth = mutable.HashMap[(Int, Int), (Int, Int)]()
+    val east = curr.filter(t => t._2 == (1,0))
+    val south = curr.filter(t => t._2 == (0,1))
+
+    east.foreach(t => {
+      val nt = ((t._1._1+1)%(maxX+1), (t._1._2)%(maxY + 1))
+      if (!curr.contains(nt)) {
+        preSouth.put(nt,t._2)
+        next.put(nt, t._2)
+      } else {
+        preSouth.put(t._1,t._2)
+        next.put(t._1, t._2)
+      }
+    })
+    south.foreach(t => preSouth.put(t._1, t._2))
+
+    south.foreach(t => {
+      val nt = ((t._1._1)%(maxX+1), (t._1._2+1)%(maxY + 1))
+      if (!preSouth.contains(nt)) {
+        next.put(nt, t._2)
+      } else {
+        next.put(t._1, t._2)
+      }
+    })
+
+    next.toMap
   }
 
 }
--- aoc2021/src/test/scala/Day25Spec.scala
+++ aoc2021/src/test/scala/Day25Spec.scala
@@ -4,9 +4,9 @@ import org.scalatest.matchers.should.Matchers
 class Day25Spec extends AnyFlatSpec with Matchers {
   behavior of "Part 1"
   it should "Test" in {
-    Day25.part1TestResult shouldEqual -1
+    Day25.part1TestResult shouldEqual 58
   }
   it should "Real" in {
-    Day25.part1Result shouldEqual -1
+    Day25.part1Result shouldEqual 579
   }
 }
