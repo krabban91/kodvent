f6de8123552d87c1e1c506be5cf34a4c59be61fa
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-26 19:59:10 +0100

    :recycle: Day 21 - Create constant ROLL_SCENARIOS

--- aoc2021/src/main/scala/Day21.scala
+++ aoc2021/src/main/scala/Day21.scala
@@ -56,21 +56,10 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
 
   case class QuantumDirac(player1: DiracPlayer, player2: DiracPlayer, isP1: Boolean) {
 
-    /**
-     * 1+1+1,1+1+2,1+1+3,1+2+1,1+2+2,1+2+3,1+3+1,1+3+2,1+3+3,
-     * 2+1+1,2+1+2,2+1+3,2+2+1,2+2+2,2+2+3,2+3+1,2+3+2,2+3+3,
-     * 3+1+1,3+1+2,3+1+3,3+2+1,3+2+2,3+2+3,3+3+1,3+3+2,3+3+3,
-     * =>
-     * 3,4,5, 4,5,6, 5,6,7
-     * 4,5,6, 5,6,7, 6,7,8
-     * 5,6,7, 6,7,8, 7,8,9
-     * =>
-     * 27 dimensions
-     * only 7 unique
-     * [1x3, 3x4, 6x5, 7x6, 6x7, 3x8, 1x9]
-     */
-    def rollQuantumRound(count: Long): Seq[(QuantumDirac, Long)] = Seq((3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1))
-      .map(t => (if (isP1) QuantumDirac(player1.move(t._1).calcScore, player2, !isP1) else QuantumDirac(player1, player2.move(t._1).calcScore, !isP1), t._2 * count))
+    def rollQuantumRound(count: Long): Seq[(QuantumDirac, Long)] = nextStates.map(t => (t._1, t._2 * count))
+
+    def nextStates: Seq[(QuantumDirac, Long)] = QuantumDirac.ROLL_SCENARIOS
+      .map(t => (if (isP1) QuantumDirac(player1.move(t._1).calcScore, player2, !isP1) else QuantumDirac(player1, player2.move(t._1).calcScore, !isP1), t._2))
 
     def winner(winsAt: Int): Option[DiracPlayer] = Option(player1).filter(_.hasWon(winsAt)).orElse(Option(player2).filter(_.hasWon(winsAt)))
 
@@ -98,5 +87,21 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
       DiracPlayer(1, strings.head.split("starting position:").last.trim.toLong),
       DiracPlayer(2, strings.last.split("starting position:").last.trim.toLong), isP1 = true)
 
+    /**
+     * 27 dimensions
+     * only 7 unique
+     * [1x3, 3x4, 6x5, 7x6, 6x7, 3x8, 1x9]
+     *
+     * 1+1+1,1+1+2,1+1+3,1+2+1,1+2+2,1+2+3,1+3+1,1+3+2,1+3+3,
+     * 2+1+1,2+1+2,2+1+3,2+2+1,2+2+2,2+2+3,2+3+1,2+3+2,2+3+3,
+     * 3+1+1,3+1+2,3+1+3,3+2+1,3+2+2,3+2+3,3+3+1,3+3+2,3+3+3,
+     *
+     * <code>
+     * 3,4,5, 4,5,6, 5,6,7
+     * 4,5,6, 5,6,7, 6,7,8
+     * 5,6,7, 6,7,8, 7,8,9
+     * </code>
+     */
+    val ROLL_SCENARIOS = Seq((3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1))
   }
 }
