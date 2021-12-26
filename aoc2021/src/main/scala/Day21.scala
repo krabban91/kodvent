0b17d4a72ca3b3bb4770f02399de971b71c0b564
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-26 20:33:17 +0100

    :mute: Day 21 - remove logging

--- aoc2021/src/main/scala/Day21.scala
+++ aoc2021/src/main/scala/Day21.scala
@@ -4,11 +4,6 @@ import scala.collection.mutable
 
 object Day21 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     var dirac = Dirac(strings)
     val winsAt = 1000
