ea27ba5581a72a34bf10eeea6a4d4e583860c64f
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-26 17:20:08 +0100

    :bug: systematicCheck now executes the code in question

--- scala-common/src/main/scala/aoc/Timed.scala
+++ scala-common/src/main/scala/aoc/Timed.scala
@@ -9,16 +9,16 @@ trait Timed {
     result
   }
 
-  def systematicCheck[V](method: => V, times: Int):Unit = {
+  def systematicCheck[V](method: () => V, times: Int): Unit = {
     var totalTime = 0L
-    for (t <- Range(0,times)){
+    for (t <- Range(0, times)) {
       val start = System.nanoTime()
-      val result = method
+      val result = method()
       val time = System.nanoTime() - start
       totalTime += time
-      println(s"Run ${t+1}, Result: $result, time: ${time * 1e-9}s")
+      println(s"Run ${t + 1}, Result: $result, time: ${time * 1e-9}s")
     }
-    println(s"Times: $times, \t avg time: ${(totalTime/times) * 1e-9}}")
+    println(s"Times: $times, \t avg time: ${(totalTime / times) * 1e-9}}")
   }
 
 }
