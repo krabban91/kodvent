322792093780225e5a4ecbd4144a18b2741708e3
gandersson <gabriel.andersson@recordedfuture.com>
2021-12-26 18:24:20 +0100

    :recycle: Day 22 - Rewrite part 1 to use same methodology

--- aoc2021/src/main/scala/Day22.scala
+++ aoc2021/src/main/scala/Day22.scala
@@ -1,7 +1,5 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
-import scala.collection.mutable
-
 object Day22 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -9,17 +7,7 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2Test
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    val v = strings.map(Instruction(_))
-    val map = v.foldLeft(mutable.HashMap[(Int, Int, Int), Boolean]())((m, instr) => {
-      (math.max(instr.xs._1, -50) to math.min(50, instr.xs._2))
-        .foreach(x => (math.max(-50, instr.ys._1) to math.min(50, instr.ys._2))
-          .foreach(y => (math.max(-50, instr.zs._1) to math.min(50, instr.zs._2))
-            .foreach(z => m.put((x, y, z), instr.on))))
-      m
-    })
-    map.count(x => x._2)
-  }
+  override def part1(strings: Seq[String]): Long = countActive(strings.map(Instruction(_)).flatMap(_.limited((-50, 50), (-50, 50), (-50, 50))))
 
   override def part2(strings: Seq[String]): Long = countActive(strings.map(Instruction(_)))
 
@@ -37,6 +25,14 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
 
     def value: Long = {
       (if (on) 1 else -1) * (math.abs(this.xs._2 - this.xs._1) + 1).toLong * (math.abs(this.ys._2 - this.ys._1) + 1).toLong * (math.abs(this.zs._2 - this.zs._1).toLong + 1)
+    }
+
+    def limited(xs: (Int, Int), ys: (Int, Int), zs: (Int, Int)): Option[Instruction] = {
+      intersection(Instruction(on,
+        (math.max(this.xs._1, xs._1), math.min(this.xs._2, xs._2)),
+        (math.max(this.ys._1, ys._1), math.min(this.ys._2, ys._2)),
+        (math.max(this.zs._1, zs._1), math.min(this.zs._2, zs._2))
+      ), on)
     }
 
     private def isValid: Boolean = xs._1 <= xs._2 && ys._1 <= ys._2 && zs._1 <= zs._2
