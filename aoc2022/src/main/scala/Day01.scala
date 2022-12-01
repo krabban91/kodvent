be35a4f2a994ec939edf4b534a4fad2bd4a5cc38
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-01 11:47:03 +0100

    :recycle: Day 1 - Rewrote to parse input smarter and map result

--- aoc2022/src/main/scala/Day01.scala
+++ aoc2022/src/main/scala/Day01.scala
@@ -1,32 +1,17 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
-import scala.collection.mutable
 
 object Day01 extends App with AoCPart1Test with AoCPart2Test {
 
   override def part1(strings: Seq[String]): Long = {
-
-    strings.foldLeft((0, 0)) { case ((sum, max), row) =>
-      if (row == "") {
-        if (sum > max) (0, sum) else (0, max)
-      } else {
-        (sum + row.toInt, max)
-      }
-    }._2
+    groupsSeparatedByTwoNewlines(strings)
+      .map(_.split("\n").filter(_.nonEmpty).map(_.toInt).sum)
+      .sorted.takeRight(1).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-
-    strings.foldLeft((0, Seq[Int]())) { case ((sum, max), row) =>
-      if (row == "") {
-        if (max.size < 3) {
-          (0, max ++ Seq(sum))
-        } else if (max.exists(v => v < sum)) {
-          (0, (max diff Seq(max.min)) ++ Seq(sum))
-        } else (0, max)
-      } else {
-        (sum + row.toInt, max)
-      }
-    }._2.sum
+    groupsSeparatedByTwoNewlines(strings)
+      .map(_.split("\n").filter(_.nonEmpty).map(_.toInt).sum)
+      .sorted.takeRight(3).sum
   }
 }
