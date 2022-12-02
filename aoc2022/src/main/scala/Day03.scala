32d72b039202f18e9b77bd7824f0d405e33a4216
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-02 16:46:17 +0100

    :package: Year 2022 Day 03 preparations

---
+++ aoc2022/src/main/scala/Day03.scala
@@ -0,0 +1,17 @@
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+
+object Day03 extends App with AoCPart1Test with AoCPart2Test {
+
+  printResultPart1
+  printResultPart1Test
+  printResultPart2
+  printResultPart2Test
+
+  override def part1(strings: Seq[String]): Long = {
+    -1
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    -1
+  }
+}
