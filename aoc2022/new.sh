154ec747e76474c52c6fb00e8dab282ca8c02ce0
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-03 10:34:28 +0100

    also checkout new branch when running setup script

--- aoc2022/new.sh
+++ aoc2022/new.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 set -euxo pipefail
 
+git stash
+git checkout -b y2022d$1
+
 cat src/main/scala/DayXX.scala | sed "s/XX/$1/" > src/main/scala/Day$1.scala
 cat src/test/scala/DayXXSpec.scala | sed "s/XX/$1/" > src/test/scala/Day$1Spec.scala
 cat src/main/resources/dayXX\$.txt | sed "s/XX/$1/" > src/main/resources/day$1\$.txt
