82aa20e16b096cfd3ee35a54aafb9e7f7c365198
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-03 06:51:39 +0100

    :recycle: Day 3 - Use string operations instead of seq operations

--- aoc2022/src/main/scala/Day03.scala
+++ aoc2022/src/main/scala/Day03.scala
@@ -2,25 +2,21 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day03 extends App with AoCPart1Test with AoCPart2Test {
 
-  val A = "A".head.asInstanceOf[Int]
-  val a = "a".head.asInstanceOf[Int]
-  printResultPart1Test
-  printResultPart1
-  printResultPart2Test
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
-    val v = strings.map(s => (s.take(s.length / 2), s.drop(s.length / 2)))
-    val v2 = v.map { case (l, r) => l.find(c => r.contains(c)).get }
-    val prios = v2.map(c => if (c.isUpper) (c.asInstanceOf[Int] - A) + 27 else c.asInstanceOf[Int] - a + 1)
-    prios.sum
+    strings.map(s => s.splitAt(s.length / 2))
+      .map { case (l, r) => matched(l, r).head }
+      .map(priority).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-    val g = strings.grouped(3).toSeq
-    val ps = g.map(l => l.last.find(v => l.head.filter(c => l.tail.head.contains(c)).contains(v)).get)
-
-    val prios = ps.map(c => if (c.isUpper) (c.asInstanceOf[Int] - A) + 27 else c.asInstanceOf[Int] - a + 1)
-    prios.sum
+    strings.grouped(3).toSeq
+      .map(l => matched(l.head, l.tail.head, l.last).head)
+      .map(priority).sum
   }
+
+  private def matched(l: String, r: String): String = l intersect r
+
+  private def matched(a: String, b: String, c: String): String = matched(a, matched(b, c))
+
+  private def priority(c: Char): Int = if (c.isUpper) (c - 'A') + 27 else c - 'a' + 1
 }
