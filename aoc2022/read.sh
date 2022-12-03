d58904208002ce1040b1f3a91501e9184766d053
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-03 10:49:34 +0100

    Added a script to run at problem release

---
+++ aoc2022/read.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+set -euxo pipefail
+
+d=$( echo $1 | sed "s/^0//")
+curl https://adventofcode.com/2022/day/$d/input -H "cookie: session=$AOC_ID" > src/main/resources/day$1\$.txt
+open https://adventofcode.com/2022/day/$d
