9afd8f76de34bfe5a22048842104ee5832b5e8b6
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-04 06:37:05 +0100

    :sparkles: Day 4 - Completed part 1

--- aoc2022/src/test/scala/Day04Spec.scala
+++ aoc2022/src/test/scala/Day04Spec.scala
@@ -1,17 +1,22 @@
+import Day04.Assignment
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day04Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day04.part1TestResult shouldEqual -1
+    Day04.part1TestResult shouldEqual 2
   }
   "Part1" should "be correct" in {
-    Day04.part1Result shouldEqual -1
+    Day04.part1Result shouldEqual 471
   }
   "Part2 Test" should "be correct" in {
     Day04.part2TestResult shouldEqual -1
   }
   "Part2" should "be correct" in {
     Day04.part2Result shouldEqual -1
+  }
+
+  "Assignments" should "fully contain other" in {
+
   }
 }
