77132682db11df655f2f77d1239fa58c89d02d3d
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-04 06:37:05 +0100

    :sparkles: Day 4 - Completed part 1

--- aoc2022/src/main/scala/Day04.scala
+++ aoc2022/src/main/scala/Day04.scala
@@ -2,11 +2,35 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day04 extends App with AoCPart1Test with AoCPart2Test {
 
+  printResultPart1Test
+  printResultPart1
+  printResultPart2Test
+  printResultPart2
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    strings.map(_.split(",")
+      .map(_.split("-"))
+      .map(l => Work(l.head.toInt, l.last.toInt)).toSeq)
+      .map(l => Assignment(l.head, l.last))
+      .count(_.fullyContained)
   }
 
   override def part2(strings: Seq[String]): Long = {
     -1
   }
+
+
+  case class Assignment(l: Work, r: Work) {
+
+    def fullyContained: Boolean = {
+      l.fullyContained(r) || r.fullyContained(l)
+    }
+  }
+
+  case class Work(from: Int, to: Int) {
+    def fullyContained(other: Work): Boolean = {
+      from <= other.from && other.to <= to
+    }
+  }
+
 }
