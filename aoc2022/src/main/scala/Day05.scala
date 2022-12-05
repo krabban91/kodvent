0c9bf5d85f244e8bdb86cfb4e387deb86f4ac1d8
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-05 06:35:01 +0100

    :sparkles: Day 5 - Completed part 1

--- aoc2022/src/main/scala/Day05.scala
+++ aoc2022/src/main/scala/Day05.scala
@@ -1,12 +1,57 @@
-import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import aoc.string.{AoCPart1StringTest, AoCPart2StringTest}
 
-object Day05 extends App with AoCPart1Test with AoCPart2Test {
+import scala.collection.mutable
 
-  override def part1(strings: Seq[String]): Long = {
-    -1
+object Day05 extends App with AoCPart1StringTest with AoCPart2StringTest {
+
+  printResultPart1Test
+  printResultPart2Test
+  printResultPart1
+  printResultPart2
+
+  override def part1(strings: Seq[String]): String = {
+    val grouped = groupsSeparatedByTwoNewlines(strings)
+    val stacks = grouped.head.split("\n").filter(_.nonEmpty)
+    val map = mutable.HashMap[Int, mutable.Stack[String]]()
+    val stackCount = stacks.last.split(" ").last.toInt
+    stacks.dropRight(1).reverse.foreach(row => {
+      val boxes = row.grouped(4).toSeq
+      boxes.indices.map(_ + 1).foreach(i => {
+        if (!map.contains(i)) {
+          map.put(i, mutable.Stack())
+        }
+        if (boxes(i - 1).startsWith("[")) {
+          map(i).push(boxes(i - 1).drop(1).split("]").head)
+        }
+      })
+    })
+    val moves = grouped.last.split("\n").filter(_.nonEmpty).map(s => {
+
+      val spl = s.split(" from ")
+      val pair = spl.last.split(" to ").map(_.toInt)
+      (spl.head.split("move ").last.toInt, (pair.head, pair.last))
+    })
+    moves.foreach(m => {
+
+      (0 until m._1).foreach(i => {
+        if (map.contains(m._2._1)) {
+          if (!map.contains(m._2._2)) {
+            map.put(m._2._2, mutable.Stack())
+          }
+          val from = map(m._2._1)
+          val to = map(m._2._2)
+          if (from.nonEmpty) {
+            to.push(from.pop)
+          }
+        }
+      })
+    })
+
+    map.keys.toSeq.sorted.map(map(_).head).reduce(_ + _)
   }
 
-  override def part2(strings: Seq[String]): Long = {
-    -1
+  override def part2(strings: Seq[String]): String = {
+    ""
   }
+
 }
