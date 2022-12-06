46f4353ed6748321a1db3bda16a04e902a2bb4b5
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-06 06:25:00 +0100

    :sparkles: Day 6 - Completed part 1 and 2

--- aoc2022/src/main/scala/Day06.scala
+++ aoc2022/src/main/scala/Day06.scala
@@ -2,11 +2,14 @@ import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 object Day06 extends App with AoCPart1Test with AoCPart2Test {
 
-  override def part1(strings: Seq[String]): Long = {
-    -1
-  }
+  override def part1(strings: Seq[String]): Long = packetStart(strings.head, 4)
 
-  override def part2(strings: Seq[String]): Long = {
-    -1
-  }
+  override def part2(strings: Seq[String]): Long = packetStart(strings.head, 14)
+
+  def packetStart(string: String, minSize: Int): Int = string.foldLeft(("", 0, false)) { case ((str, count, seen), c) => if (seen)
+    (str, count, seen)
+  else if (str.contains(c))
+    (str.drop(str.indexOf(c) + 1) + c, count + 1, seen)
+  else (str + c, count + 1, str.length + 1 >= minSize)
+  }._2
 }
