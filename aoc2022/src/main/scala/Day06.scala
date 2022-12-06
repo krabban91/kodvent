d1f5bca0bdf380b12774b200a0ec0a0f69546260
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-06 06:53:56 +0100

    :recycle: Day 6 - slightly shrunk the mapping statement

--- aoc2022/src/main/scala/Day06.scala
+++ aoc2022/src/main/scala/Day06.scala
@@ -9,6 +9,6 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
   def packetStart(string: String, minSize: Int): Int = string.sliding(minSize).zipWithIndex
     .filter(_._1.length >= minSize)
     .find(p => p._1 == p._1.distinct)
-    .map(p => p._1.length + p._2)
+    .map(_._2 + minSize)
     .get
 }
