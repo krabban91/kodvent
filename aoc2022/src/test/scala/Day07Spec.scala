af322281c859c5d0c4842a39d555f493722d95da
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-07 07:05:31 +0100

    :sparkles: Day 7 - Completed part 1
    Keeping the original commit messages below
    - WIP. started at part 1
    - wip. using case classes
    - added logging
    - moved code

--- aoc2022/src/test/scala/Day07Spec.scala
+++ aoc2022/src/test/scala/Day07Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day07Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day07.part1TestResult shouldEqual -1
+    Day07.part1TestResult shouldEqual 95437
   }
   "Part1" should "be correct" in {
-    Day07.part1Result shouldEqual -1
+    Day07.part1Result shouldEqual 1348005
   }
   "Part2 Test" should "be correct" in {
     Day07.part2TestResult shouldEqual -1
