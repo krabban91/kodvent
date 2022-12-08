606a1219cd3232d647962d0e4b41e62987ca8dd0
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-08 07:07:39 +0100

    :sparkles: Day 08 - Completed part 2

--- aoc2022/src/main/scala/Day08.scala
+++ aoc2022/src/main/scala/Day08.scala
@@ -1,6 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 import scala.collection.mutable
+
 object Day08 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -26,18 +27,43 @@ object Day08 extends App with AoCPart1Test with AoCPart2Test {
           mutDown.append(o)
         }
       })
-      //val up = zipped.flatMap(t => t._1.filter(_._2 < y).map(_._1))
-      //val down = zipped.flatMap(t => t._1.filter(_._2 > y).map(_._1))
-
-      Seq(left, right, mutUp, mutDown).exists(other => other.forall(p =>p.height < tree.height))
+      Seq(left, right, mutUp, mutDown).exists(other => other.forall(p => p.height < tree.height))
     }
     }
-    visible.map(_.count(_=>true)).sum
+    visible.map(_.count(_ => true)).sum
     // 1300 is wrong
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val v = strings.map(_.map(_.toInt).map(_ - 48).map(Tree))
+    val zipped = v.zipWithIndex.map(t => (t._1.zipWithIndex, t._2))
+    val view = zipped.map { case (l, y) => l.map { case (tree, x) =>
+      val left = l.filter(_._2 < x).map(_._1)
+      val right = l.filter(_._2 > x).map(_._1)
+      val mutUp = mutable.ListBuffer[Tree]()
+      val mutDown = mutable.ListBuffer[Tree]()
+      zipped.foreach(t => {
+        val o = t._1(x)._1
+        if (t._2 < y) {
+          mutUp.append(o)
+        } else if (t._2 > y) {
+          mutDown.append(o)
+        }
+      })
+      val leftVisible = left.reverse.takeWhile(_.height < tree.height).size
+      val rightVisible = right.takeWhile(_.height < tree.height).size
+      val upVisible = mutUp.reverse.takeWhile(_.height < tree.height).size
+      val downVisible = mutDown.takeWhile(_.height < tree.height).size
+      val lScore = (leftVisible + (if (leftVisible == left.size) 0 else 1))
+      val rScore = rightVisible + (if (rightVisible == right.size) 0 else 1)
+      val uScore = upVisible + (if (upVisible == mutUp.size) 0 else 1)
+      val dScore = downVisible + (if (downVisible == mutDown.size) 0 else 1)
+      lScore * rScore * uScore * dScore
+
+    }
+
+    }
+    view.map(_.max).max
   }
 
   case class Tree(height: Int)
