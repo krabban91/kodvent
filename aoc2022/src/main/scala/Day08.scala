dfe818ce56849fd7fab3b0d0cc2d59b28783a526
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-08 07:08:01 +0100

    :recycle: Day 08 - Use the Tree case class more

--- aoc2022/src/main/scala/Day08.scala
+++ aoc2022/src/main/scala/Day08.scala
@@ -1,7 +1,5 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
-import scala.collection.mutable
-
 object Day08 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -10,61 +8,53 @@ object Day08 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-
-
-    val v = strings.map(_.map(_.toInt).map(_ - 48).map(Tree))
-    val zipped = v.zipWithIndex.map(t => (t._1.zipWithIndex, t._2))
-    val visible = zipped.map { case (l, y) => l.filter { case (tree, x) =>
-      val left = l.zipWithIndex.filter(_._2 < x).map(_._1).map(_._1)
-      val right = l.zipWithIndex.filter(_._2 > x).map(_._1).map(_._1)
-      val mutUp = mutable.ListBuffer[Tree]()
-      val mutDown = mutable.ListBuffer[Tree]()
-      zipped.foreach(t => {
-        val o = t._1(x)._1
-        if (t._2 < y) {
-          mutUp.append(o)
-        } else if (t._2 > y) {
-          mutDown.append(o)
-        }
-      })
-      Seq(left, right, mutUp, mutDown).exists(other => other.forall(p => p.height < tree.height))
-    }
-    }
-    visible.map(_.count(_ => true)).sum
-    // 1300 is wrong
+    val trees: Seq[Seq[Tree]] = Tree.parse(strings)
+    val transposed = trees.head.indices.map(x => trees.map(_ (x)))
+    trees.flatten
+      .count(tree => tree.isVisible(transposed(tree.point._1), trees(tree.point._2)))
   }
 
   override def part2(strings: Seq[String]): Long = {
-    val v = strings.map(_.map(_.toInt).map(_ - 48).map(Tree))
-    val zipped = v.zipWithIndex.map(t => (t._1.zipWithIndex, t._2))
-    val view = zipped.map { case (l, y) => l.map { case (tree, x) =>
-      val left = l.filter(_._2 < x).map(_._1)
-      val right = l.filter(_._2 > x).map(_._1)
-      val mutUp = mutable.ListBuffer[Tree]()
-      val mutDown = mutable.ListBuffer[Tree]()
-      zipped.foreach(t => {
-        val o = t._1(x)._1
-        if (t._2 < y) {
-          mutUp.append(o)
-        } else if (t._2 > y) {
-          mutDown.append(o)
-        }
-      })
-      val leftVisible = left.reverse.takeWhile(_.height < tree.height).size
-      val rightVisible = right.takeWhile(_.height < tree.height).size
-      val upVisible = mutUp.reverse.takeWhile(_.height < tree.height).size
-      val downVisible = mutDown.takeWhile(_.height < tree.height).size
-      val lScore = (leftVisible + (if (leftVisible == left.size) 0 else 1))
-      val rScore = rightVisible + (if (rightVisible == right.size) 0 else 1)
-      val uScore = upVisible + (if (upVisible == mutUp.size) 0 else 1)
-      val dScore = downVisible + (if (downVisible == mutDown.size) 0 else 1)
-      lScore * rScore * uScore * dScore
-
-    }
-
-    }
-    view.map(_.max).max
+    val trees: Seq[Seq[Tree]] = Tree.parse(strings)
+    val transposed = trees.head.indices.map(x => trees.map(_ (x)))
+    trees.flatten
+      .map(tree => tree.visibilityScore(transposed(tree.point._1), trees(tree.point._2)))
+      .max
   }
 
-  case class Tree(height: Int)
+  case class Tree(height: Int, point: (Int, Int)) {
+
+    def isVisible(column: Seq[Tree], row: Seq[Tree]): Boolean = directions(column, row)
+      .exists(_.forall(_.height < height))
+
+    def visibilityScore(column: Seq[Tree], row: Seq[Tree]): Int = directions(column, row)
+      .map(visibleTrees).product
+
+    private def isRightOf(other: Tree): Boolean = other.point._1 < point._1
+
+    private def isLeftOf(other: Tree): Boolean = other.point._1 > point._1
+
+    private def isBelow(other: Tree): Boolean = other.point._2 < point._2
+
+    private def isAbove(other: Tree): Boolean = other.point._2 > point._2
+
+    private def visibleTrees(other: Seq[Tree]): Int = {
+      val filtered = other.takeWhile(_.height < height).size
+      filtered + math.signum(other.size - filtered)
+    }
+
+    private def directions(column: Seq[Tree], row: Seq[Tree]): Seq[Seq[Tree]] = {
+      Seq(row.filter(_.isLeftOf(this)).reverse,
+        row.filter(_.isRightOf(this)),
+        column.filter(_.isAbove(this)).reverse,
+        column.filter(_.isBelow(this)))
+    }
+
+  }
+
+  object Tree {
+    def parse(strings: Seq[String]): Seq[Seq[Tree]] = strings.map(_.map(_.toInt).map(_ - 48))
+      .zipWithIndex.map { case (l, y) => l.zipWithIndex.map { case (height, x) => Tree(height, (x, y)) } }
+  }
+
 }
