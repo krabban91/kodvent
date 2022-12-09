1261acc5a8249d017d86e4e9a6917965a6f5ff02
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-09 07:04:29 +0100

    :recycle: Day 09 - use same code for part 1 and 2

--- aoc2022/src/main/scala/Day09.scala
+++ aoc2022/src/main/scala/Day09.scala
@@ -1,10 +1,8 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 import krabban91.kodvent.kodvent.utilities.Distances
-import krabban91.kodvent.kodvent.utilities.logging.LogUtils
 
 import java.awt.Point
 import scala.collection.mutable
-import scala.jdk.CollectionConverters.MapHasAsJava
 
 object Day09 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -13,10 +11,11 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  // Distances
+  override def part1(strings: Seq[String]): Long = followTail(strings, 2).size
 
+  override def part2(strings: Seq[String]): Long = followTail(strings, 10).size
 
-  override def part1(strings: Seq[String]): Long = {
+  private def followTail(strings: Seq[String], length: Int): Set[Point] = {
     val directions = Map(
       "L" -> new Point(-1, 0),
       "R" -> new Point(1, 0),
@@ -24,59 +23,7 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
       "D" -> new Point(0, 1),
     )
 
-    val tail = new Point(0, 0)
-    val head = new Point(0, 0)
-    val tailPositions = mutable.HashMap[Point, String]()
-    tailPositions.put(new Point(tail.x, tail.y), "#")
-    val inputPattern = """(.+) (.+)""".r
-    strings.foreach { case inputPattern(d, times) =>
-      val dir = directions(d)
-      (1 to times.toInt).foreach(i => {
-        head.move(head.x + dir.x, head.y + dir.y)
-        val dist = Distances.manhattan(tail, head)
-        if (dist > 1) {
-          if (tail.x != head.x && tail.y != head.y && dist > 2) {
-            if (tail.x < head.x && tail.y < head.y) {
-              tail.move(tail.x + 1, tail.y + 1)
-            }
-            else if (tail.x < head.x && tail.y > head.y) {
-              tail.move(tail.x + 1, tail.y + -1)
-            }
-            else if (tail.x > head.x && tail.y < head.y) {
-              tail.move(tail.x + -1, tail.y + 1)
-            }
-            else if (tail.x > head.x && tail.y > head.y) {
-              tail.move(tail.x + -1, tail.y + -1)
-            }
-            else {
-              println(tail + " and " + head)
-            }
-            tailPositions.put(new Point(tail.x, tail.y), "#")
-          }
-          else if (tail.x != head.x) {
-            tail.move(tail.x + (head.x - tail.x) / 2, tail.y + (head.y - tail.y) / 2)
-            tailPositions.put(new Point(tail.x, tail.y), "#")
-
-          } else if (tail.y != head.y) {
-            tail.move(tail.x + (head.x - tail.x) / 2, tail.y + (head.y - tail.y) / 2)
-            tailPositions.put(new Point(tail.x, tail.y), "#")
-          }
-        }
-        // println(new LogUtils[String].mapToText(tailPositions.asJava, v => if(v == null) "." else v))
-      })
-    }
-    tailPositions.keys.size
-  }
-
-  override def part2(strings: Seq[String]): Long = {
-    val directions = Map(
-      "L" -> new Point(-1, 0),
-      "R" -> new Point(1, 0),
-      "U" -> new Point(0, -1),
-      "D" -> new Point(0, 1),
-    )
-
-    val snake = (1 to 10).map(_ => new Point(0, 0))
+    val snake = (1 to length).map(_ => new Point(0, 0))
     val tailPositions = mutable.HashMap[Point, String]()
     tailPositions.put(new Point(snake.last.x, snake.last.y), "#")
     val inputPattern = """(.+) (.+)""".r
@@ -119,6 +66,6 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
       })
     }
     //println(new LogUtils[String].mapToText(tailPositions.asJava, v => if(v == null) "." else v))
-    tailPositions.keys.size
+    tailPositions.keys.toSet
   }
 }
