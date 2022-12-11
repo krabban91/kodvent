bbfb04c2e075f177bd7716529dee83c08dc35a9c
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-11 08:34:31 +0100

    :recycle: Day 11 - cleaned up the parsing and removed redundancies

--- aoc2022/src/test/scala/Day11Spec.scala
+++ aoc2022/src/test/scala/Day11Spec.scala
@@ -14,4 +14,9 @@ class Day11Spec extends AnyFlatSpec with Matchers {
   "Part2" should "be correct" in {
     Day11.part2Result shouldEqual 30616425600L
   }
+
+  "Monkey" should "be parsed " in {
+    val monkey = Day11.Monkey("Monkey 0:\n  Starting items: 22,11\n  Operation: new = old * 11\n  Test: divisible by 13\n    If true: throw to monkey 4\n    If false: throw to monkey 7")
+    monkey.items shouldBe Seq(22,11)
+  }
 }
