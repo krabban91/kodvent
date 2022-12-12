e17cd7a25cc106e3660a588e1e1e205038014a7f
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-12 08:31:27 +0100

    :mute: Day 12 - remove logging

--- aoc2022/src/main/scala/Day12.scala
+++ aoc2022/src/main/scala/Day12.scala
@@ -4,11 +4,6 @@ import scala.collection.mutable
 
 object Day12 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart2Test
-  printResultPart1
-  printResultPart2
-
   override def part1(strings: Seq[String]): Long = {
     val mountain = parseMap(strings)
     val start = mountain.find(t => t._2 == 'S').get._1
