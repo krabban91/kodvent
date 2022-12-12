a0bb509362e759e692cfb534128fd36828a7b332
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-12 14:52:26 +0100

    move shortestPath to Generic method

--- aoc2022/src/main/scala/Day12.scala
+++ aoc2022/src/main/scala/Day12.scala
@@ -1,14 +1,12 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
-import scala.collection.mutable
-
 object Day12 extends App with AoCPart1Test with AoCPart2Test {
 
   override def part1(strings: Seq[String]): Long = {
     val mountain = parseMap(strings)
     val start = mountain.find { case (_, v) => v == 'S' }.get._1
     val end = mountain.find { case (_, v) => v == 'E' }.get._1
-    shortestPath(start,
+    Graph.shortestPath[(Int, Int)](start,
       { case p@(_, _) => p == end },
       { case p@(_, _) => manhattan(p, end) },
       { case p@(_, _) => moves1(mountain, p) })
@@ -19,7 +17,7 @@ object Day12 extends App with AoCPart1Test with AoCPart2Test {
     val xMax = mountain.keys.map(_._1).max
     val yMax = mountain.keys.map(_._2).max
     val end = mountain.find { case (_, v) => v == 'E' }.get._1
-    shortestPath(end,
+    Graph.shortestPath[(Int, Int)](end,
       { case (x, y) => (x == 0 || y == 0 || x == xMax || y == yMax) && Seq('S', 'a').contains(mountain(x, y)) },
       { case (x, y) => math.min(math.min(x, xMax - x), math.min(y, yMax - y)) },
       { case p@(_, _) => moves2(mountain, p) })
@@ -29,23 +27,6 @@ object Day12 extends App with AoCPart1Test with AoCPart2Test {
   private def parseMap(strings: Seq[String]): Map[(Int, Int), Char] = strings
     .zipWithIndex.flatMap { case (l, y) => l.zipWithIndex.map { case (height, x) => ((x, y), height) } }
     .toMap
-
-  private def shortestPath(start: (Int, Int), end: ((Int, Int)) => Boolean, heuristic: ((Int, Int)) => Long, neighbors: ((Int, Int)) => Seq[((Int, Int), Long)]): Long = {
-    val queue = mutable.PriorityQueue[((Int, Int), Long, Long)]((start, 0L, heuristic(start._1, start._2)))(Ordering.by(v => -(v._2 + v._3)))
-    val visited = mutable.HashSet[(Int, Int)]()
-    while (queue.nonEmpty) {
-      val pop = queue.dequeue()
-      if (end.apply(pop._1)) {
-        return pop._2
-      }
-      if (visited.add(pop._1)) {
-        neighbors.apply(pop._1)
-          .filterNot(t => visited.contains(t._1))
-          .foreach(p => queue.enqueue((p._1, pop._2 + p._2, heuristic(p._1._1, p._1._2))))
-      }
-    }
-    Long.MaxValue
-  }
 
   private def moves1(v: Map[(Int, Int), Char], pop: (Int, Int)): Seq[((Int, Int), Long)] = Seq((-1, 0), (1, 0), (0, -1), (0, 1))
     .map(t => (pop._1 + t._1, pop._2 + t._2))
