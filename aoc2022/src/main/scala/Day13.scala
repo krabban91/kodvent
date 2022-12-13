37f5a35c1f43bc5bb81b4702d5d18723a8cf17af
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-13 08:26:23 +0100

    :recycle: Day 13 - Flatten the compare method to only handle one level

--- aoc2022/src/main/scala/Day13.scala
+++ aoc2022/src/main/scala/Day13.scala
@@ -9,71 +9,25 @@ object Day13 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    groupsSeparatedByTwoNewlines(strings).map(_.split("\n").filter(_.nonEmpty)
-      .map(Packet(_)))
-      .zipWithIndex.map { case (l, i) => (i + 1, PacketOrdering.compare(l.head, l.last)) }
-      .map { case (i, i1) => i * (if (i1 >= 0) 1 else 0) }.sum
-  }
+  override def part1(strings: Seq[String]): Long = groupsSeparatedByTwoNewlines(strings)
+    .map(_.split("\n").filter(_.nonEmpty).map(Packet(_)))
+    .map(l => PacketOrdering.compare(l.head, l.last)).zipWithIndex
+    .filter(_._1 >= 0)
+    .map(_._2 + 1).sum
 
   override def part2(strings: Seq[String]): Long = {
-    val v = strings.filter(_.nonEmpty).map(Packet(_))
-    val div1 = PacketList(Seq(PacketList(Seq(PacketValue(2L)))))
-    val div2 = PacketList(Seq(PacketList(Seq(PacketValue(6L)))))
-    val s = (Seq(div1, div2) ++ v).sorted(PacketOrdering).reverse
-    (s.indexOf(div1) + 1) * (s.indexOf(div2) + 1)
+    val divider = PacketList(Seq(PacketList(Seq(PacketValue(2L)))))
+    val divider2 = PacketList(Seq(PacketList(Seq(PacketValue(6L)))))
+    val dividers = Seq(divider, divider2)
+    val packets = (strings.filter(_.nonEmpty).map(Packet(_)) ++ dividers).sorted.reverse
+    dividers.map(packets.indexOf(_) + 1).product
   }
 
-  trait Packet {
-    def value: Long
-  }
+  trait Packet
 
-  case class PacketList(list: Seq[Packet]) extends Packet {
-    override def value: Long = list.map(_.value).sum
+  case class PacketList(list: Seq[Packet]) extends Packet
 
-  }
-
-  implicit object PacketOrdering extends Ordering[Packet] {
-
-    override def compare(x: Packet, y: Packet): Int = (x, y) match {
-      case (PacketList(left), PacketList(right)) =>
-        left.indices.foreach(i => {
-          val l = left(i)
-          if (right.length <= i) {
-            return -1
-          }
-          val r = right(i)
-          (l, r) match {
-            case (PacketValue(lv), PacketValue(rv)) =>
-              if (rv < lv) return -1
-              else if (rv > lv) return 1
-            case (PacketList(_), PacketList(_)) =>
-              val cmp = compare(l, r)
-              if (cmp != 0) {
-                return cmp
-              }
-
-            case (PacketList(lv), PacketValue(rv)) =>
-              val cmp = compare(l, PacketList(Seq(r)))
-              if (cmp != 0) {
-                return cmp
-              }
-            case (PacketValue(lv), PacketList(rv)) =>
-              val cmp = compare(PacketList(Seq(l)), r)
-              if (cmp != 0) {
-                return cmp
-              }
-          }
-        })
-        if (right.length > left.length) {
-          return 1
-        }
-        0
-    }
-  }
-
-  case class PacketValue(value: Long) extends Packet {
-  }
+  case class PacketValue(value: Long) extends Packet
 
   object Packet extends RegexParsers {
     def token: Parser[Packet] = value | list
@@ -85,4 +39,17 @@ object Day13 extends App with AoCPart1Test with AoCPart2Test {
     def apply(string: String): Packet = parseAll(token, string).get
   }
 
+  implicit object PacketOrdering extends Ordering[Packet] {
+    override def compare(x: Packet, y: Packet): Int = (x, y) match {
+      case (PacketValue(lv), PacketValue(rv)) =>
+        math.signum(rv - lv).toInt
+      case (PacketList(left), PacketList(right)) =>
+        left.zip(right).map { case (l, r) => compare(l, r) }.find(_ != 0).getOrElse(
+          math.signum(right.length - left.length))
+      case (l@PacketList(_), r@PacketValue(_)) =>
+        compare(l, PacketList(Seq(r)))
+      case (l@PacketValue(_), r@PacketList(_)) =>
+        compare(PacketList(Seq(l)), r)
+    }
+  }
 }
