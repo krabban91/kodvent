fb2a3c7c081826ba7d0a442f889866ecbfe1c865
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-16 13:24:37 +0100

    :sparkles: Day 16 - Completed part 1

--- aoc2022/src/test/scala/Day16Spec.scala
+++ aoc2022/src/test/scala/Day16Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day16Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day16.part1TestResult shouldEqual -1
+    Day16.part1TestResult shouldEqual 1651
   }
   "Part1" should "be correct" in {
-    Day16.part1Result shouldEqual -1
+    Day16.part1Result shouldEqual 1775
   }
   "Part2 Test" should "be correct" in {
     Day16.part2TestResult shouldEqual -1
