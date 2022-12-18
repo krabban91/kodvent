e00d647888d073339749b9acb74fb8906c871d20
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-18 06:15:31 +0100

    :sparkles: Day 18 - Completed part 1

--- aoc2022/src/main/scala/Day18.scala
+++ aoc2022/src/main/scala/Day18.scala
@@ -1,4 +1,5 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.grid.CubeGrid
 
 object Day18 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -8,7 +9,23 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val points = strings.map(_.split(",")).map(l => (l.head.toInt, l.tail.head.toInt, l.last.toInt))
+    val directions = Seq(
+      (-1,  0,  0),
+      ( 1,   0,  0),
+      ( 0,  -1, 0),
+      ( 0,   1,  0),
+      ( 0,   0, -1),
+      ( 0,   0,  1),
+
+    )
+    points.map{ case current@(x, y, z) =>
+      val others = points.filterNot(_==current)
+      val neighbors = directions
+        .map{ case (dx, dy, dz) => (x+dx, y+dy, z+dz)}
+      val freeSurfaces = neighbors.filterNot(others.contains)
+      freeSurfaces.size
+    }.sum
   }
 
   override def part2(strings: Seq[String]): Long = {
--- aoc2022/src/test/scala/Day18Spec.scala
+++ aoc2022/src/test/scala/Day18Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day18Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day18.part1TestResult shouldEqual -1
+    Day18.part1TestResult shouldEqual 64
   }
   "Part1" should "be correct" in {
-    Day18.part1Result shouldEqual -1
+    Day18.part1Result shouldEqual 4474
   }
   "Part2 Test" should "be correct" in {
     Day18.part2TestResult shouldEqual -1
