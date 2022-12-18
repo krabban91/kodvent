4c5f060e8b584cda43287a00e8bd91e598e4a6c1
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-18 08:02:59 +0100

    :recycle: Day 18 - Some minor refactoring

--- aoc2022/src/main/scala/Day18.scala
+++ aoc2022/src/main/scala/Day18.scala
@@ -9,87 +9,67 @@ object Day18 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    val points = strings.map(_.split(",")).map(l => (l.head.toInt, l.tail.head.toInt, l.last.toInt))
-    val directions = Seq(
-      (-1, 0, 0),
-      (1, 0, 0),
-      (0, -1, 0),
-      (0, 1, 0),
-      (0, 0, -1),
-      (0, 0, 1),
+  override def part1(strings: Seq[String]): Long = exposedSides(readInput(strings)).map(_.size).sum
 
-    )
+  override def part2(strings: Seq[String]): Long = {
+    val points = readInput(strings)
+    val (xmin, xmax) = (points.map(_._1).min, points.map(_._1).max)
+    val (ymin, ymax) = (points.map(_._2).min, points.map(_._2).max)
+    val (zmin, zmax) = (points.map(_._3).min, points.map(_._3).max)
+    val limits = (xmin, xmax, ymin, ymax, zmin, zmax)
+    val sides = exposedSides(points).flatten
+
+    val escapesFrom = mutable.HashMap[(Int, Int, Int), Boolean]()
+    val sideSet = sides.toSet.filter(p => reachesExterior(p, points, escapesFrom, limits))
+    sides.count(sideSet.contains)
+  }
+
+  private def exposedSides(points: Seq[(Int, Int, Int)]): Seq[Seq[(Int, Int, Int)]] = {
     points.map { case current@(x, y, z) =>
       val others = points.filterNot(_ == current)
       val neighbors = directions
         .map { case (dx, dy, dz) => (x + dx, y + dy, z + dz) }
-      val freeSurfaces = neighbors.filterNot(others.contains)
-      freeSurfaces.size
-    }.sum
-  }
-
-  def manhattan(p: (Int, Int, Int), o: (Int, Int, Int)): Int = {
-    math.abs(p._1 - o._1) +
-      math.abs(p._2 - o._2) +
-      math.abs(p._3 - o._3)
-  }
-
-  override def part2(strings: Seq[String]): Long = {
-    val points = strings.map(_.split(",")).map(l => (l.head.toInt, l.tail.head.toInt, l.last.toInt))
-    val directions = Seq(
-      (-1, 0, 0),
-      (1, 0, 0),
-      (0, -1, 0),
-      (0, 1, 0),
-      (0, 0, -1),
-      (0, 0, 1),
-    )
-    val (xmin, xmax) = (points.map(_._1).min, points.map(_._1).max)
-    val (ymin, ymax) = (points.map(_._2).min, points.map(_._2).max)
-    val (zmin, zmax) = (points.map(_._3).min, points.map(_._3).max)
-    val sides = points.map { case current@(x, y, z) =>
-      val others = points.filterNot(_ == current)
-      val neighbors = directions
-        .map { case (dx, dy, dz) => ((x + dx, y + dy, z + dz)) }
-      val freeSurfaces = neighbors.filterNot(others.contains)
-      val nonTrapped = freeSurfaces
-      nonTrapped
+      neighbors.filterNot(others.contains)
     }
+  }
 
-    val flatten = sides.flatten
-    val escapesFrom = mutable.HashMap[(Int, Int, Int), Boolean]()
-    val sideSet = flatten.distinct
-      .filter(p => {
-        val frontier = mutable.PriorityQueue[(Int, Int, Int)]()
-        frontier.enqueue(p)
-        var escaped = false
-        val visited = mutable.HashSet[(Int, Int, Int)]()
-        while (frontier.nonEmpty) {
-          val pop@(x,y,z) = frontier.dequeue()
-          if (x < xmin || y < ymin || z < zmin || x>xmax || y > ymax || z > zmax) {
-            escaped = true
-            frontier.clear()
-          } else if(escapesFrom.contains(pop)) {
-            escaped = escapesFrom(pop)
-            frontier.clear()
-          } else if (visited.add(pop)) {
-            val moves = directions
-              .map { case (dx, dy, dz) => (x + dx, y + dy, z + dz) }
-              .filterNot(visited.contains)
-              .filterNot(points.contains)
-            frontier.addAll(moves)
-          }
-        }
-        visited.foreach(v => escapesFrom.put(v, escaped))
-        escaped
-      })
+  private def readInput(strings: Seq[String]) = {
+    strings.map(_.split(",")).map(l => (l.head.toInt, l.tail.head.toInt, l.last.toInt))
+  }
 
-    flatten.filter(sideSet.contains).size
-    // 4132 is wrong -> six sides
-    // 3682 is wrong -> 5 sides (was hoping for a tip)
-    // 4143 is too high
-    // 4138 is wrong
-    // 4108 is wrong
+  def directions = Seq(
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, -1),
+    (0, 0, 1),
+  )
+
+  def reachesExterior(p: (Int, Int, Int), droplets: Seq[(Int, Int, Int)], escapesFrom: mutable.HashMap[(Int, Int, Int), Boolean], limits: (Int, Int, Int, Int, Int, Int)) = {
+    val (xmin, xmax, ymin, ymax, zmin, zmax) = limits
+
+    val frontier = mutable.PriorityQueue[(Int, Int, Int)]()
+    frontier.enqueue(p)
+    var escaped = false
+    val visited = mutable.HashSet[(Int, Int, Int)]()
+    while (frontier.nonEmpty) {
+      val pop@(x, y, z) = frontier.dequeue()
+      if (x < xmin || y < ymin || z < zmin || x > xmax || y > ymax || z > zmax) {
+        escaped = true
+        frontier.clear()
+      } else if (escapesFrom.contains(pop)) {
+        escaped = escapesFrom(pop)
+        frontier.clear()
+      } else if (visited.add(pop)) {
+        val moves = directions
+          .map { case (dx, dy, dz) => (x + dx, y + dy, z + dz) }
+          .filterNot(visited.contains)
+          .filterNot(droplets.contains)
+        frontier.addAll(moves)
+      }
+    }
+    visited.foreach(v => escapesFrom.put(v, escaped))
+    escaped
   }
 }
