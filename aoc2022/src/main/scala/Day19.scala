a4a5a92d570238cfdb2c73b39ccdbdf482949dcb
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-19 07:04:01 +0100

    :sparkles: Day 19 - Completed part 1

--- aoc2022/src/main/scala/Day19.scala
+++ aoc2022/src/main/scala/Day19.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day19 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -8,10 +10,110 @@ object Day19 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val v = strings.map(Blueprint(_))
+    val maxMinutes = 24
+    val out = v.map(blueprint => {
+      val start = State(
+        Map("ore" ->0, "clay" -> 0, "obsidian"-> 0, "geode" -> 0),
+        Map("ore" ->1, "clay" -> 0, "obsidian"-> 0, "geode" -> 0))
+      // (ore,clay obsidian, geode), (ore, clayRobots, obsidian, geode)
+      // State
+
+      val frontier = mutable.PriorityQueue[(State, Int)]()(Ordering.by(v => (-v._2, v._1.material("geode"))))
+      val visited = mutable.HashSet[State]()
+      frontier.enqueue((start, 0))
+      var best: State = null
+      while (frontier.nonEmpty){
+        val pop@(state, minute) = frontier.dequeue()
+        if (minute == maxMinutes) {
+          best = state
+          frontier.clear()
+        } else{
+          if (visited.add(state)){
+            val affords = blueprint.nextStates(state)
+            val nextStates = affords
+              .map(next => State(next.material.map{case (k, v) => (k, v + state.robots(k))}, next.robots))
+              .map(s => (s, minute + 1))
+            frontier.addAll(nextStates)
+          }
+        }
+      }
+      val score = best.material("geode") * blueprint.id
+      println(s"Blueprint ${blueprint.id} score=$score, state=$best")
+      score
+    })
+    out.sum
   }
 
   override def part2(strings: Seq[String]): Long = {
     -1
   }
+
+  case class State(material: Map[String, Int], robots: Map[String,Int])
+
+  case class Blueprint(id: Int, oreRobotCost: Seq[(Int, String)], clayRobotCost: Seq[(Int, String)], obsidianRobotCost: Seq[(Int, String)], geodeRobotCost: Seq[(Int, String)]) {
+    def nextStates(state: State): Seq[State] = {
+      val start = state
+
+      val frontier = mutable.PriorityQueue[State]()(Ordering.by(v =>v.material.head._2))
+      val visited = mutable.HashSet[State]()
+      frontier.enqueue(start)
+      while (frontier.nonEmpty) {
+        val pop = frontier.dequeue()
+
+        if (visited.add(pop)) {
+          val affordable = buildStates(pop)
+          //println(affordable)
+          // todo add more states. done
+          val next = affordable.reverse.take(2)
+          frontier.addAll(next)
+          visited.addAll(next)
+        }
+
+      }
+      visited.toSeq
+    }
+
+    def buildStates(state: State): Seq[State] = {
+      Seq(
+        Some(oreRobotCost.map{ case (count, material) => state.material(material) - count})
+          .filter(_.forall(_ >=0))
+          .map(_ => ("ore", oreRobotCost)),
+        Some(clayRobotCost.map{ case (count, material) => state.material(material) - count})
+          .filter(_.forall(_ >=0))
+          .map(_ => ("clay", clayRobotCost)),
+        Some(obsidianRobotCost.map{ case (count, material) => state.material(material) - count})
+          .filter(_.forall(_ >=0))
+          .map(_ => ("obsidian", obsidianRobotCost)),
+        Some(geodeRobotCost.map{ case (count, material) => state.material(material) - count})
+          .filter(_.forall(_ >=0))
+          .map(_ => ("geode", geodeRobotCost))
+      ).flatten
+        .map{case (mat, costs) =>
+          val nextMaterials = state.material.map{case (k,v) => (k, v - costs.find(_._2 == k).map(_._1).getOrElse(0))}
+          val nextRobots = state.robots.map{ case (k,v) => (k, v + (if(k == mat) 1 else 0))}
+          State(nextMaterials, nextRobots)
+        }
+    }
+  }
+
+  object Blueprint {
+    val pattern = """Blueprint (.+): Each ore robot costs (.+). Each clay robot costs (.+). Each obsidian robot costs (.+). Each geode robot costs (.+).""".r
+
+    def apply(string: String): Blueprint = {
+      string match {
+        case pattern(id, ore, clay, obsidian, geode) =>
+          val ores = extractCosts(ore)
+          val clays = extractCosts(clay)
+          val obsidians = extractCosts(obsidian)
+          val geodes = extractCosts(geode)
+          Blueprint(id.toInt, ores, clays, obsidians, geodes)
+      }
+    }
+
+    private def extractCosts(ore: String) = {
+      ore.split("and").map(_.trim).map(_.split(" ")).map(l => (l.head.toInt, l.last)).toSeq
+    }
+  }
+
 }
--- aoc2022/src/test/scala/Day19Spec.scala
+++ aoc2022/src/test/scala/Day19Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day19Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day19.part1TestResult shouldEqual -1
+    Day19.part1TestResult shouldEqual 33
   }
   "Part1" should "be correct" in {
-    Day19.part1Result shouldEqual -1
+    Day19.part1Result shouldEqual 1650
   }
   "Part2 Test" should "be correct" in {
     Day19.part2TestResult shouldEqual -1
