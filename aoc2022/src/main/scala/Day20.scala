c4b4edfe018e5b6e8bdbe831ef70964f788c4160
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-20 07:03:17 +0100

    :sparkles: Day 20 - Completed part 1

--- aoc2022/src/main/scala/Day20.scala
+++ aoc2022/src/main/scala/Day20.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day20 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -8,7 +10,53 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val v = strings.map(v => v.toInt)
+    val l = mixNumbers(v)
+    val i0 = l.indexOf(0)
+    val i1000 = (i0 + 1000)
+    val i2000 = (i0 + 2000)
+    val i3000 = (i0 + 3000)
+
+    val i = l(i1000 % v.size)
+    val i1 = l(i2000 % v.size)
+    val i2 = l(i3000 % v.size)
+    i + i1 + i2
+    // -27706 is wrong
+    // 5195 is wrong
+  }
+
+  private def mixNumbers(v: Seq[Int]): mutable.ArrayDeque[Int] = {
+
+    val l = mutable.ArrayDeque[(Int, Int)]()
+    val initial =v.zipWithIndex
+    l.addAll(initial)
+    initial.foreach(x => {
+      // move to item
+      while (l.head != x) {
+        l.append(l.removeFirst(_ => true).get)
+      }
+      //move item
+      val o = l.removeFirst(_ => true).get
+      if (x != o) {
+        println(s"moved wrong item x=${x}, o=$o")
+      }
+      val times = x._1
+      if (times > 0) {
+        (1 to times).foreach(t => l.append(l.removeFirst(_ => true).get))
+        l.prepend(x)
+        //move back
+        (1 to (times)).foreach(t => l.prepend(l.removeLast()))
+      } else {
+        (1 to math.abs(times)).foreach(t => l.prepend(l.removeLast()))
+        l.prepend(x)
+        //move back
+        (1 to math.abs(times)).foreach(t => l.append(l.removeFirst(_ => true).get))
+
+      }
+
+    })
+    l.append(l.removeFirst(_ => true).get)
+    l.map(_._1)
   }
 
   override def part2(strings: Seq[String]): Long = {
--- aoc2022/src/test/scala/Day20Spec.scala
+++ aoc2022/src/test/scala/Day20Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day20Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day20.part1TestResult shouldEqual -1
+    Day20.part1TestResult shouldEqual 3
   }
   "Part1" should "be correct" in {
-    Day20.part1Result shouldEqual -1
+    Day20.part1Result shouldEqual 2622
   }
   "Part2 Test" should "be correct" in {
     Day20.part2TestResult shouldEqual -1
