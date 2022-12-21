2a9949a39aef59a7c23f2683a98c86d7655472e8
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-21 08:01:54 +0100

    :recycle: Day 21 - Completed part 2 programatically

--- aoc2022/src/main/scala/Day21.scala
+++ aoc2022/src/main/scala/Day21.scala
@@ -1,10 +1,11 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+
 object Day21 extends App with AoCPart1Test with AoCPart2Test {
 
-  //printResultPart1Test
-  //printResultPart2Test
-  //printResultPart1
+  printResultPart1Test
+  printResultPart2Test
+  printResultPart1
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
@@ -15,21 +16,31 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
   override def part2(strings: Seq[String]): Long = {
     val v = strings.map(v => Expression(v)).map(v => (v.name, v)).toMap
 
+    var test = 0L
+    var stepSize = (1e12).toLong
+    var result = tryYelling(0L, v)
+    val direction = math.signum(result - tryYelling(1L, v)).toLong
+    while (result != 0) {
+      while (result < 0) {
+        test -= stepSize * direction
+        result = tryYelling(test, v)
+      }
+      while (result > 0) {
+        test += stepSize * direction
+        result = tryYelling(test, v)
+      }
+      stepSize /= 10
+    }
+    test
+  }
+
+  def tryYelling(number: Long, original: Map[String, Expression]): Double = {
     val me = "humn"
-    var lookup = v ++ Map(me -> Value(me, 0))
-    val root = lookup("root").asInstanceOf[Operation]
-
-    val out = (3 * 1e12).toLong + (3 * 1e11).toLong + (4 * 1e10).toLong + (9 * 1e9).toLong +
-      (1 * 1e8).toLong + (3 * 1e7).toLong + (6 * 1e6).toLong +
-      (3 * 1e5).toLong + (8 * 1e4).toLong + (4 * 1e3).toLong +
-      (4 * 1e2).toLong + (4 * 1e1).toLong + 1L
-    lookup = v ++ Map(me -> Value(me, out))
-    val l = lookup(root.l).value(lookup)
-    val r = lookup(root.r).value(lookup)
-    val diff = r - l
-    println(s"diff: $diff")
-    out
-
+    val exps = original ++ Map(me -> Value(me, number))
+    val root = exps("root").asInstanceOf[Operation]
+    val l = exps(root.l).value(exps)
+    val r = exps(root.r).value(exps)
+    (r - l)
   }
 
 
@@ -39,10 +50,10 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
     def value(lookup: Map[String, Expression]): Double
   }
 
-  case class Operation(name: String, l: String, op: String, r: String) extends Expression {
+  case class Operation(name: String, l: String, r: String, op: String) extends Expression {
     override def value(lookup: Map[String, Expression]): Double = {
-      val left = lookup.getOrElse(l, Value(l, l.toLong)).value(lookup)
-      val right = lookup.getOrElse(r, Value(r, r.toLong)).value(lookup)
+      val left = lookup(l).value(lookup)
+      val right = lookup(r).value(lookup)
 
       op match {
         case "+" => left + right
@@ -64,7 +75,7 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
     def apply(string: String): Expression = {
       string match {
         case valPattern(name, v) => Value(name, v.toLong)
-        case expPattern(name, l, op, r) => Operation(name, l, op, r)
+        case expPattern(name, l, op, r) => Operation(name, l, r, op)
       }
     }
   }
--- aoc2022/src/test/scala/Day21Spec.scala
+++ aoc2022/src/test/scala/Day21Spec.scala
@@ -9,9 +9,9 @@ class Day21Spec extends AnyFlatSpec with Matchers {
     Day21.part1Result shouldEqual 81075092088442L
   }
   "Part2 Test" should "be correct" in {
-    Day21.part2TestResult shouldEqual -1
+    Day21.part2TestResult shouldEqual 301
   }
   "Part2" should "be correct" in {
-    Day21.part2Result shouldEqual -1
+    Day21.part2Result shouldEqual 3349136384441L
   }
 }
