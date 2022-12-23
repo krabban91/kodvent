370801fc7b3cd02900cd5d97ed5f9fa26d8468dc
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-23 08:10:47 +0100

    :recycle: Day 23 - Reordered the code to reduce number of mappings

--- aoc2022/src/main/scala/Day23.scala
+++ aoc2022/src/main/scala/Day23.scala
@@ -13,162 +13,116 @@ object Day23 extends App with AoCPart1Test with AoCPart2Test {
 
 
   override def part1(strings: Seq[String]): Long = {
-    val N = (0, -1)
-    val E = (1, 0)
-    val S = (0, 1)
-    val W = (-1, 0)
-
-    val NE = (1, -1)
-    val NW = (-1, -1)
-
-    val SE = (1, 1)
-    val SW = (-1, 1)
-    val order = Seq(N, S, W, E)
-    val check = Map(
-      N -> Seq(NW, N, NE),
-      S -> Seq(SW, S, SE),
-      E -> Seq(SE, E, NE),
-      W -> Seq(NW, W, SW)
-    )
-    //10 rounds
-
-    var rounds = 10
-
-    val initial = strings.zipWithIndex.flatMap { case (s, y) => s.zipWithIndex.filter(_._1 == '#').map { case (c, x) => (x, y) } }
-      .map(p => (p, Elf(p, order)))
-      .toMap
-    var start = initial
-    val after = (1 to 10).foldLeft(initial) { case (before, i) =>
-      println(s"before $i")
-      //logMap(before)
-
-      val firstHalf = before.toSeq.map { case (p@(x, y), elf) =>
-        //first half: next location
-        val suggestion = elf.suggestMove(before.keySet, check)
-
-        (suggestion, elf)
-      }
-      val secondHalf = firstHalf.map { case (suggested, current) =>
-        val collission = firstHalf.exists(o => o._1.pos == suggested.pos && o._2.pos != current.pos)
-        if (collission) {
-          val nextSuggestions = current.suggestionOrder.tail ++ Seq(current.suggestionOrder.head)
-          val fallBack = Elf(current.pos, nextSuggestions)
-          fallBack
-        } else {
-          suggested
-        }
-      }
-      val value = secondHalf
-        .map(elf => (elf.pos, elf))
-      value.toMap
+    val initial = Elf.parse(strings)
+    val after = (1 to 10).foldLeft((initial, Elf.initialOrder)) { case ((before, orderToUse), i) =>
+      Elf.performRound(before, orderToUse)
     }
-    //logMap(after)
-
-    val minX = after.keySet.map(_._1).min
-    val minY = after.keySet.map(_._2).min
-    val maxX = after.keySet.map(_._1).max
-    val maxY = after.keySet.map(_._2).max
-    (minX to maxX).map(x => (minY to maxY).map(y => (x, y)).filterNot(after.contains).size).sum
+    Elf.emptySlots(after._1)
   }
 
   override def part2(strings: Seq[String]): Long = {
-    val N = (0, -1)
-    val E = (1, 0)
-    val S = (0, 1)
-    val W = (-1, 0)
-
-    val NE = (1, -1)
-    val NW = (-1, -1)
-
-    val SE = (1, 1)
-    val SW = (-1, 1)
-    val order = Seq(N, S, W, E)
-    val check = Map(
-      N -> Seq(NW, N, NE),
-      S -> Seq(SW, S, SE),
-      E -> Seq(SE, E, NE),
-      W -> Seq(NW, W, SW)
-    )
-    //10 rounds
-
-    var rounds = 10
-
-    val initial = strings.zipWithIndex.flatMap { case (s, y) => s.zipWithIndex.filter(_._1 == '#').map { case (c, x) => (x, y) } }
-      .map(p => (p, Elf(p, order)))
-      .toMap
-    var start = initial
-    val after = (1 to 100000).foldLeft((initial, -1)) { case ((before, res), i) =>
+    val initial = Elf.parse(strings)
+    val arbitrarilyLarge = 100000
+    val after = (1 to arbitrarilyLarge).foldLeft((initial, Elf.initialOrder, -1)) { case ((before, orderToUse, res), i) =>
       if (res > 0) {
-        (before, res)
+        (before, orderToUse, res)
       } else {
-        if (before.values.forall(e => e.noMoves(before.keySet, check))) {
-          (before, i)
+        if (Elf.noMoves(before)) {
+          (before, orderToUse, i)
         } else {
-
-          println(s"before $i")
-          //logMap(before)
-
-          val firstHalf = before.toSeq.map { case (p@(x, y), elf) =>
-            //first half: next location
-            val suggestion = elf.suggestMove(before.keySet, check)
-
-            (suggestion, elf)
-          }
-          val secondHalf = firstHalf.map { case (suggested, current) =>
-            val collission = firstHalf.exists(o => o._1.pos == suggested.pos && o._2.pos != current.pos)
-            if (collission) {
-              val nextSuggestions = current.suggestionOrder.tail ++ Seq(current.suggestionOrder.head)
-              val fallBack = Elf(current.pos, nextSuggestions)
-              fallBack
-            } else {
-              suggested
-            }
-          }
-          val value = secondHalf
-            .map(elf => (elf.pos, elf))
-          (value.toMap, res)
-
+          val (after, nextOrder) = Elf.performRound(before, orderToUse)
+          (after, nextOrder, res)
         }
       }
     }
     //logMap(after._1)
-    after._2
+    after._3
   }
 
-  private def logMap(elfMap: Map[(Int, Int), Elf]) = {
-    val map = elfMap.keySet.map(p => (p, '#')).toMap
+
+  private def logMap(elves: Set[Elf]): Unit = {
+    val map = elves.map(_.pos).map(p => (p, '#')).toMap
     val java = map.map(kv => (new Point(kv._1._1, kv._1._2), kv._2)).asJava
     println(new LogUtils[Char].mapToText(java, v => if (v == 0) "." else s"$v"))
   }
 
-  case class Elf(pos: (Int, Int), suggestionOrder: Seq[(Int, Int)]) {
+  case class Elf(pos: (Int, Int)) {
 
-    def noMoves(elfPositions: Set[(Int, Int)], check: Map[(Int, Int), Seq[(Int, Int)]]): Boolean = {
-      check.values.flatten.map { case (dx, dy) => (pos._1 + dx, pos._2 + dy) }.forall(v => !elfPositions.contains(v))
+    def noMoves(elfPositions: Set[Elf], check: Map[(Int, Int), Seq[(Int, Int)]]): Boolean = {
+      check.values.flatten.map { case (dx, dy) => (pos._1 + dx, pos._2 + dy) }.forall(v => !elfPositions.contains(Elf(v)))
     }
 
-    def suggestMove(elfPositions: Set[(Int, Int)], check: Map[(Int, Int), Seq[(Int, Int)]]): Elf = {
-
-      val nextSuggestions = suggestionOrder.tail ++ Seq(suggestionOrder.head)
-      val fallBack = Elf(this.pos, nextSuggestions)
-      if (noMoves(elfPositions, check)) {
-        fallBack
+    def suggestMove(allElves: Set[Elf], suggestionOrder: Seq[(Int, Int)]): Elf = {
+      if (noMoves(allElves, Elf.check)) {
+        this
       } else {
-        val options = suggestionOrder
-          .filter(dir => {
-            val value = check(dir)
-            val bool = value.forall { case (dx, dy) => !elfPositions.contains((pos._1 + dx, pos._2 + dy)) }
-            bool
-          })
-
-        val first = options.headOption
-        first.map { case p@(dx, dy) =>
-          val nextPos = (pos._1 + dx, pos._2 + dy)
-          Elf(nextPos, nextSuggestions)
-        }.getOrElse(fallBack)
+        suggestionOrder
+          .find(Elf.check(_).forall { case (dx, dy) => !allElves.contains(Elf(pos._1 + dx, pos._2 + dy)) })
+          .map{ case (dx, dy) => (pos._1 + dx, pos._2 + dy)}
+          .map(Elf(_))
+          .getOrElse(this)
       }
     }
+  }
 
+  object Elf {
+
+    val N: (Int, Int) = (0, -1)
+    val E: (Int, Int) = (1, 0)
+    val S: (Int, Int) = (0, 1)
+    val W: (Int, Int) = (-1, 0)
+
+    val NE: (Int, Int) = (1, -1)
+    val NW: (Int, Int) = (-1, -1)
+
+    val SE: (Int, Int) = (1, 1)
+    val SW: (Int, Int) = (-1, 1)
+    val initialOrder: Seq[(Int, Int)] = Seq(N, S, W, E)
+    val check: Map[(Int, Int), Seq[(Int, Int)]] = Map(
+      N -> Seq(NW, N, NE),
+      S -> Seq(SW, S, SE),
+      E -> Seq(SE, E, NE),
+      W -> Seq(NW, W, SW)
+    )
+
+
+    def parse(strings: Seq[String]): Set[Elf] = {
+      strings.zipWithIndex.flatMap { case (s, y) => s.zipWithIndex.filter(_._1 == '#').map { case (c, x) => (x, y) } }
+        .map(p => Elf(p))
+        .toSet
+    }
+
+    def nextSuggestions(order: Seq[(Int, Int)]): Seq[(Int, Int)] = order.tail ++ Set(order.head)
+
+    def noMoves(elves: Set[Elf]): Boolean = elves.forall(_.noMoves(elves, check))
+
+    def performRound(elves: Set[Elf], order: Seq[(Int, Int)]): (Set[Elf], Seq[(Int, Int)]) = {
+      val firstHalf = Elf.firstHalf(elves, order)
+      val secondHalf = Elf.secondHalf(firstHalf)
+      (secondHalf, nextSuggestions(order))
+    }
+
+    def firstHalf(elves: Set[Elf], order: Seq[(Int, Int)]): Seq[(Elf, Elf)] = elves.toSeq.map(elf => (elf.suggestMove(elves, order), elf))
+
+    def secondHalf(suggestions: Seq[(Elf, Elf)]): Set[Elf] = {
+      suggestions.map{ case (suggested, current) =>
+        val collission = suggestions.exists(o => o._1 == suggested && o._2 != current)
+        if (collission) {
+          current
+        } else {
+          suggested
+        }
+      }.toSet
+    }
+
+    def emptySlots(elves: Set[Elf]): Long = {
+      val map = elves.map(e => (e.pos, e)).toMap
+      val minX = map.keySet.map(_._1).min
+      val minY = map.keySet.map(_._2).min
+      val maxX = map.keySet.map(_._1).max
+      val maxY = map.keySet.map(_._2).max
+      (minX to maxX).map(x => (minY to maxY).map(y => (x, y)).filterNot(map.contains).size).sum
+    }
   }
 
 
