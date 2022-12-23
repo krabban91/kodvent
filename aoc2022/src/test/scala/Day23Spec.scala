e6b242dea250a53297fe209c2efd7ea77216f2e4
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-23 07:23:41 +0100

    :sparkles: Day 23 - Completed part 2

--- aoc2022/src/test/scala/Day23Spec.scala
+++ aoc2022/src/test/scala/Day23Spec.scala
@@ -9,9 +9,9 @@ class Day23Spec extends AnyFlatSpec with Matchers {
     Day23.part1Result shouldEqual 4049
   }
   "Part2 Test" should "be correct" in {
-    Day23.part2TestResult shouldEqual -1
+    Day23.part2TestResult shouldEqual 20
   }
   "Part2" should "be correct" in {
-    Day23.part2Result shouldEqual -1
+    Day23.part2Result shouldEqual 1021
   }
 }
