be64b6b3c56516b87f83dd9588637abcbaaa7f4e
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-25 07:12:57 +0100

    :sparkles: Day 25 - Completed part 1 and 2

--- aoc2022/src/main/scala/Day25.scala
+++ aoc2022/src/main/scala/Day25.scala
@@ -1,17 +1,46 @@
-import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import aoc.string.AoCPart1StringTest
 
-object Day25 extends App with AoCPart1Test with AoCPart2Test {
+object Day25 extends App with AoCPart1StringTest {
 
   printResultPart1Test
-  printResultPart2Test
   printResultPart1
-  printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    -1
+  override def part1(strings: Seq[String]): String = {
+    toSNAFU(strings.map(fromSNAFU).sum)
   }
 
-  override def part2(strings: Seq[String]): Long = {
-    -1
+  def toSNAFU(v: Long): String = {
+    var divider = math.pow(5, 20).toLong
+
+    while (v / divider == 0) {
+      divider = divider / 5
+    }
+    var tests = Seq((v, ""))
+    while (divider != 0) {
+      tests = tests.flatMap { case (inValue, outS) => Seq("=", "-", "0", "1", "2")
+        .map {
+          case "=" => (inValue + (2 * divider), outS + "=")
+          case "-" => (inValue + (1 * divider), outS + "-")
+          case s => (inValue - (s.toLong * divider), outS + s)
+        }
+        .filter(kv => math.abs(kv._1) <= divider)
+      }
+      divider = divider / 5
+    }
+    tests
+      .find(_._1 == 0)
+      .map(_._2).get
+  }
+
+  def fromSNAFU(snafu: String): Long = {
+    snafu.zipWithIndex.map { case (c, i) =>
+      val v = math.pow(5, snafu.length - 1 - i)
+      val out = c match {
+        case '=' => -2 * v
+        case '-' => -1 * v
+        case _ => s"$c".toLong * v
+      }
+      out
+    }.map(_.toLong).sum
   }
 }
--- aoc2022/src/test/scala/Day25Spec.scala
+++ aoc2022/src/test/scala/Day25Spec.scala
@@ -3,15 +3,26 @@ import org.scalatest.matchers.should.Matchers
 
 class Day25Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day25.part1TestResult shouldEqual -1
+    Day25.part1TestResult shouldEqual "2=-1=0"
   }
   "Part1" should "be correct" in {
-    Day25.part1Result shouldEqual -1
+    Day25.part1Result shouldEqual "2=001=-2=--0212-22-2"
   }
-  "Part2 Test" should "be correct" in {
-    Day25.part2TestResult shouldEqual -1
+
+  "snafu" should "parse 2=-01" in {
+    Day25.fromSNAFU("2=-01") shouldEqual 976L
   }
-  "Part2" should "be correct" in {
-    Day25.part2Result shouldEqual -1
+
+  "snafu" should "parse 1121-1110-1=0" in {
+    Day25.fromSNAFU("1121-1110-1=0") shouldEqual 314159265L
   }
+
+  "to snafu" should "parse 2=-01" in {
+    Day25.toSNAFU(976L) shouldEqual "2=-01"
+  }
+
+  "to snafu" should "parse 1121-1110-1=0" in {
+    Day25.toSNAFU(314159265) shouldEqual "1121-1110-1=0"
+  }
+
 }
