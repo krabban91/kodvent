c67f15590396a8fb92f2e083bf8015d1cab304f2
gandersson <gabriel.andersson@recordedfuture.com>
2022-12-29 14:16:24 +0100

    :mute: Day 16 - remove logging

--- aoc2022/src/main/scala/Day16.scala
+++ aoc2022/src/main/scala/Day16.scala
@@ -4,12 +4,6 @@ import scala.collection.mutable
 
 object Day16 extends App with AoCPart1Test with AoCPart2Test {
 
-  printResultPart1Test
-  printResultPart2Test
-  printResultPart1
-  printResultPart2
-
-
   override def part1(strings: Seq[String]): Long = {
     val in = strings.map(Valve(_)).map(i => (i.name, i)).toMap
     val minutes = 30
