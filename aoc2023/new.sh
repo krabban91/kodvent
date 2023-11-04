69ddf511fe8ecc85c15ec56f47c27964af69cfc5
gandersson <gabriel.andersson@recordedfuture.com>
2023-11-04 20:20:26 +0100

    Setup sbt project for aoc2023

---
+++ aoc2023/new.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+set -euxo pipefail
+
+git stash
+git checkout -b y2023d$1
+
+cat src/main/scala/DayXX.scala | sed "s/XX/$1/" > src/main/scala/Day$1.scala
+cat src/test/scala/DayXXSpec.scala | sed "s/XX/$1/" > src/test/scala/Day$1Spec.scala
+cat src/main/resources/dayXX\$.txt | sed "s/XX/$1/" > src/main/resources/day$1\$.txt
+cat src/main/resources/test/dayXX\$.txt | sed "s/XX/$1/" > src/main/resources/test/day$1\$.txt
+
+git add src
+git commit -m ":package: Year 2023 Day $1 preparations"
