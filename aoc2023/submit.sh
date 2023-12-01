29c42bffeafb56edb09bb9bdd9fc59171eba7070
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-01 09:20:37 +0100

    :wrench: use the pip package advent-of-code-data for submissions

--- aoc2023/submit.sh
+++ aoc2023/submit.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env zsh
 set -euxo pipefail
 
-d=$( echo $1 | sed "s/^0//")
-l=$2
-r=$3
+l=${2:-1}
+r=$1
+dd=${3:-$(gdate +%d)}
+d=$( echo $dd | sed "s/^0//")
 y=${4:-2023}
-echo "{\"level\": $l, \"answer\": \"$r\"}" | curl https://adventofcode.com/$y/day/$d/answer -H "cookie: session=$AOC_ID"
+python ./submit.py $d $l $r $y
 res=$?
 if test "$res" != "0"; then
   echo "failed with $res"
   exit $res
 fi
+
 git add -u
-git commit -m ":sparkles: Year $y Day $1 - Completed part $l"
+git commit -m ":sparkles: Year $y Day $dd - Completed part $l"
 
 open https://adventofcode.com/$y/day/$d
