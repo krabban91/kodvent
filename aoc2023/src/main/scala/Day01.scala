09213bf52bb3baf8b9000ea4658a37bb4a10a68b
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-01 06:07:57 +0100

    :sparkles: Year 2023 Day 01 - Completed part 1 and 2

--- aoc2023/src/main/scala/Day01.scala
+++ aoc2023/src/main/scala/Day01.scala
@@ -7,12 +7,27 @@ object Day01 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+
   override def part1(strings: Seq[String]): Long = {
-    val lines = strings.map(_.split(",").map(_.trim.toLong).toSeq)
-    lines.map(l => l(0)).sum
+    val lookup = Map("0" -> 0, "1" -> 1, "2" -> 2, "3" -> 3, "4" -> 4, "5" -> 5, "6" -> 6, "7" -> 7, "8" -> 8, "9" -> 9)
+    strings.map(v => parseDigit(v, lookup)).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val lookup = Map("zero" -> 0, "one" -> 1, "two" -> 2, "three" -> 3, "four" -> 4, "five" -> 5, "six" -> 6, "seven" -> 7, "eight" -> 8, "nine" -> 9, "0" -> 0, "1" -> 1, "2" -> 2, "3" -> 3, "4" -> 4, "5" -> 5, "6" -> 6, "7" -> 7, "8" -> 8, "9" -> 9)
+    strings.map(v => parseDigit(v, lookup)).sum
   }
+
+  def parseDigit(string: String, lookup: Map[String, Int]): Long = {
+    val ll = lookup
+      .toList
+      .flatMap { case (k, vv) => (string.indices.map { i => val x = string.substring(i).indexOf(k); if (x != -1) x + i else -1 }.filter(_ != -1).map((_, vv))) }
+      .sortBy(_._1)
+    if (ll.isEmpty) {
+      0
+    } else {
+      s"${ll.head._2}${ll.last._2}".toLong
+    }
+  }
+
 }
--- aoc2023/src/test/scala/Day01Spec.scala
+++ aoc2023/src/test/scala/Day01Spec.scala
@@ -2,16 +2,26 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day01Spec extends AnyFlatSpec with Matchers {
+  "Part1 example" should "be correct" in {
+    val example =
+      """
+        |1abc2
+        |pqr3stu8vwx
+        |a1b2c3d4e5f
+        |treb7uchet
+        |""".stripMargin.split("\n")
+    Day01.part1(example) shouldEqual 142
+  }
   "Part1 Test" should "be correct" in {
-    Day01.part1TestResult shouldEqual -1
+    Day01.part1TestResult shouldEqual 209
   }
   "Part1" should "be correct" in {
-    Day01.part1Result shouldEqual -1
+    Day01.part1Result shouldEqual 54953
   }
   "Part2 Test" should "be correct" in {
-    Day01.part2TestResult shouldEqual -1
+    Day01.part2TestResult shouldEqual 281
   }
   "Part2" should "be correct" in {
-    Day01.part2Result shouldEqual -1
+    Day01.part2Result shouldEqual 53868
   }
 }
