5012cb0430fa1fe9bde8ffc47bf37ea0f44008f3
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-01 08:24:30 +0100

    :sparkles: Year 2017 Day 08 - Completed part 1

--- aoc2023/submit.sh
+++ aoc2023/submit.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 d=$( echo $1 | sed "s/^0//")
 l=$2
 r=$3
-echo "{\"level\": $l, \"answer\": \"$r\"}" | curl -f https://adventofcode.com/2023/day/$d/answer -H "cookie: session=$AOC_ID"
+y=${4:-2023}
+echo "{\"level\": $l, \"answer\": \"$r\"}" | curl -f https://adventofcode.com/$y/day/$d/answer -H "cookie: session=$AOC_ID"
 
 git add -u
-git commit -m ":sparkles: Year 2023 Day $1 - Completed part $l"
+git commit -m ":sparkles: Year $y Day $1 - Completed part $l"
 
-open https://adventofcode.com/2023/day/$d
+open https://adventofcode.com/$y/day/$d
