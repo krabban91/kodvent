a7b2fd69989a2952c5584d0c03743d1ad60acc54
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-01 08:26:34 +0100

    :sparkles: Year 2017 Day 08 - Completed part 1

--- aoc2023/submit.sh
+++ aoc2023/submit.sh
@@ -5,8 +5,12 @@ d=$( echo $1 | sed "s/^0//")
 l=$2
 r=$3
 y=${4:-2023}
-echo "{\"level\": $l, \"answer\": \"$r\"}" | curl -f https://adventofcode.com/$y/day/$d/answer -H "cookie: session=$AOC_ID"
-
+echo "{\"level\": $l, \"answer\": \"$r\"}" | curl https://adventofcode.com/$y/day/$d/answer -H "cookie: session=$AOC_ID"
+res=$?
+if test "$res" != "0"; then
+  echo "failed with $res"
+  exit $res
+fi
 git add -u
 git commit -m ":sparkles: Year $y Day $1 - Completed part $l"
 
