f523e70248d4fded0e3c3425e46a29ce278339fa
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-01 06:00:01 +0100

    :card_file_box: Year 2023 Day 01 - Added input of day

--- aoc2023/src/main/scala/Day01.scala
+++ aoc2023/src/main/scala/Day01.scala
@@ -8,7 +8,8 @@ object Day01 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val lines = strings.map(_.split(",").map(_.trim.toLong).toSeq)
+    lines.map(l => l(0)).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
