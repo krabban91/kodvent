7785928f66c89c14dc7412ef61bb8a526d3a53c2
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-02 09:17:04 +0100

    :memo: Year 2023 Day 02 - Updated tests

--- aoc2023/src/test/scala/Day02Spec.scala
+++ aoc2023/src/test/scala/Day02Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day02Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day02.part1TestResult shouldEqual -1
+    Day02.part1TestResult shouldEqual 8
   }
   "Part1" should "be correct" in {
-    Day02.part1Result shouldEqual -1
+    Day02.part1Result shouldEqual 2156
   }
   "Part2 Test" should "be correct" in {
-    Day02.part2TestResult shouldEqual -1
+    Day02.part2TestResult shouldEqual 2286
   }
   "Part2" should "be correct" in {
-    Day02.part2Result shouldEqual -1
+    Day02.part2Result shouldEqual 66909
   }
 }
