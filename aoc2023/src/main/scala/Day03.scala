f53826a1133ac4573445a0f61c2fb47df20a6058
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-03 09:52:44 +0100

    :construction: Year 2023 Day 03 - Working on part 1

--- aoc2023/src/main/scala/Day03.scala
+++ aoc2023/src/main/scala/Day03.scala
@@ -7,11 +7,44 @@ object Day03 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  case class Gear(number: Long, adjacent: String) {
+    def hasSymbol: Boolean = adjacent.filterNot(_.isDigit).exists(_ != '.')
+  }
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val res = deriveGears(strings)
+    val value = res.filter(_.hasSymbol)
+    println(value)
+    value.map(_.number).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
     -1
   }
+
+  private def deriveGears(strings: Seq[String]) = {
+    strings.indices.flatMap { y =>
+      val line = strings(y)
+      val mask = line.indices.map(x => (x, line(x).isDigit))
+      mask.foldLeft(Seq[Gear]()) { case (l, (x, isDig)) =>
+        if (isDig) {
+          if (x == 0 || !line(x - 1).isDigit) {
+            val n = mask.drop(x).takeWhile { case (x, d) => d }.map(_._1)
+            val v = n.foldLeft("") { case (s, v) => s"$s${line(v)}" }.toLong
+            val value = (math.max(0, y - 1) to math.min(strings.length - 1, y + 1))
+              .flatMap(dy => (math.max(0, x - 1) to math.min(x + n.size + 1, line.length - 1)).map((_, dy)))
+            val adj = value
+              .filterNot { case (dx, dy) => y == dy && dx >= x && dx <= x + n.size - 1 }
+              .map { case (dx, dy) => strings(dy)(dx) }
+            l ++ Seq(Gear(v, adj.foldLeft("") { case (s, v) => s"$s$v" }))
+          } else {
+            l
+          }
+        } else {
+          l
+        }
+      }
+    }
+  }
+
 }
--- aoc2023/src/test/scala/Day03Spec.scala
+++ aoc2023/src/test/scala/Day03Spec.scala
@@ -3,10 +3,10 @@ import org.scalatest.matchers.should.Matchers
 
 class Day03Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day03.part1TestResult shouldEqual -1
+    Day03.part1TestResult shouldEqual 4361
   }
   "Part1" should "be correct" in {
-    Day03.part1Result shouldEqual -1
+    Day03.part1Result shouldEqual 544597
   }
   "Part2 Test" should "be correct" in {
     Day03.part2TestResult shouldEqual -1
