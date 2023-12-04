9b83442fb30e30f405571e18a77ff680d5e53958
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-04 08:10:56 +0100

    :sparkles: Year 2023 Day 04 - Completed part 1

--- aoc2023/src/main/scala/Day04.scala
+++ aoc2023/src/main/scala/Day04.scala
@@ -7,11 +7,31 @@ object Day04 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = {
-    -1
-  }
+  override def part1(strings: Seq[String]): Long = strings.map(ScratchCard(_)).map(_.points).sum
 
   override def part2(strings: Seq[String]): Long = {
     -1
   }
+
+
+  case class ScratchCard(id: Int, winning: Seq[Int], drawn: Seq[Int]) {
+    def points: Long = {
+      val i = drawn.count(winning.contains)
+      if (i > 0) {
+        math.pow(2, i - 1).toLong
+      } else 0
+    }
+  }
+
+  object ScratchCard {
+    def apply(str: String): ScratchCard = {
+      val s = str.split(":")
+      val id = s.head.drop(4).strip().toInt
+      val s2 = s.last.split("""\|""")
+      val winning = s2.head.split(" ").filter(_.nonEmpty).map(_.strip().toInt).toSeq
+      val drawn = s2.last.split(" ").filter(_.nonEmpty).map(_.strip().toInt).toSeq
+      ScratchCard(id, winning, drawn)
+    }
+  }
+
 }
