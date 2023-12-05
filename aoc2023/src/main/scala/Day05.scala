24e9844a6883fdf133a6f7612a683e24105cde91
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-05 14:57:28 +0100

    :sparkles: Year 2023 Day 05 - Completed part 2

--- aoc2023/src/main/scala/Day05.scala
+++ aoc2023/src/main/scala/Day05.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day05 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -16,7 +18,7 @@ object Day05 extends App with AoCPart1Test with AoCPart2Test {
       val hSplit = lines.head.split(" ").head.split("-to-")
       val (f, t) = (hSplit.head, hSplit.last)
       val ls = lines.tail.map(s => s.split(" ").map(_.toLong))
-        .map(l => MapList(l.head, l.tail.head, l.last)).toSeq
+        .map(l => MapList(l(0), l(1), l(2))).toSeq
       Mapping(f,t, ls)
     }
   }
@@ -25,22 +27,46 @@ object Day05 extends App with AoCPart1Test with AoCPart2Test {
     val groups = groupsSeparatedByTwoNewlines(strings).map(_.stripPrefix("\n").stripSuffix("\n")).filter(_.nonEmpty)
     val seeds = groups.head.replace("seeds: ", "").split(" ").map(_.toLong)
     val maps = groups.tail.map(Mapping(_)).map(m => (m.from, m)).toMap
-    val locs = seeds.map{s => s
+    val locs = lookup(Seq(seeds), maps)
+
+    locs
+  }
+
+  private def lookup(seeds: Seq[Seq[Long]], maps: Map[String, Mapping]) = {
+    var out = Long.MaxValue
+    var i = 0
+    var j = 0
+    val t = mutable.HashMap[String, Long]()
+    seeds.foreach(_.foreach { s =>
+      s
       var from = "seed"
       var v = s
-      while(from != "location") {
+      t.put(from, s)
+      while (from != "location") {
         val m = maps(from)
-        val matched = m.maps.find(l => l.srcStart <= v && v <= l.srcStart + l.rangeLength).getOrElse(MapList(0,0,0))
+        val matched = m.maps.find(l => l.srcStart <= v && v < l.srcStart + l.rangeLength)
+        v = matched.map(m => v - m.srcStart + m.destStart).getOrElse(v)
+        t.put(m.to, v)
         from = m.to
-        val delta = matched.destStart - matched.srcStart
-        v = v + delta
       }
-      v
-    }
-    locs.min
+      if (v < out) {
+        out = v
+        j = j+1
+        println(t)
+      }
+      i = i+1
+      t.clear()
+    })
+    println(s"${seeds.size}, calculated $i values. decr $j times.")
+    out
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val groups = groupsSeparatedByTwoNewlines(strings).map(_.stripPrefix("\n").stripSuffix("\n")).filter(_.nonEmpty)
+    val seedRanges = groups.head.replace("seeds: ", "").split(" ").map(_.toLong)
+    val seeds = (0 until seedRanges.length/2).map(_*2).map(i => (seedRanges(i) until seedRanges(i) + seedRanges(i+1)))
+    val maps = groups.tail.map(Mapping(_)).map(m => (m.from, m)).toMap
+    val locs = lookup(seeds, maps)
+    locs
   }
 }
