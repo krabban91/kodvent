06c86e9ba082a9e70ba0469a0f5788c9df76fb53
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-06 06:50:19 +0100

    :sparkles: Year 2023 Day 06 - Completed part 1

--- aoc2023/src/main/scala/Day06.scala
+++ aoc2023/src/main/scala/Day06.scala
@@ -8,7 +8,18 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val tuples: Array[(Long, Long)] = deriveTimes(strings)
+    tuples.map{ case (t, d) =>
+      (1L until t).map { h => (t - h) * h }.count(_ > d).toLong
+    }.product
+  }
+
+  private def deriveTimes(strings: Seq[String]) = {
+    val times = strings.head.split(" ").tail.filter(_.nonEmpty).map(_.strip().toLong)
+    val distances = strings.tail.head.split(" ").tail.filter(_.nonEmpty).map(_.strip().toLong)
+
+    val tuples = times zip distances
+    tuples
   }
 
   override def part2(strings: Seq[String]): Long = {
