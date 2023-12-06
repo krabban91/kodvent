a12ebc8fdccef4d8c26b2b28885606abf9027cc2
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-06 06:54:18 +0100

    :sparkles: Year 2023 Day 06 - Completed part 2

--- aoc2023/src/main/scala/Day06.scala
+++ aoc2023/src/main/scala/Day06.scala
@@ -8,21 +8,28 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    val tuples: Array[(Long, Long)] = deriveTimes(strings)
-    tuples.map{ case (t, d) =>
+    val tuples: Seq[(Long, Long)] = deriveTimes(strings)
+    tuples.map { case (t, d) =>
       (1L until t).map { h => (t - h) * h }.count(_ > d).toLong
     }.product
   }
 
-  private def deriveTimes(strings: Seq[String]) = {
-    val times = strings.head.split(" ").tail.filter(_.nonEmpty).map(_.strip().toLong)
-    val distances = strings.tail.head.split(" ").tail.filter(_.nonEmpty).map(_.strip().toLong)
-
-    val tuples = times zip distances
-    tuples
+  override def part2(strings: Seq[String]): Long = {
+    val tuples: Seq[(Long, Long)] = deriveTimes(strings, part2 = true)
+    tuples.map { case (t, d) =>
+      (1L until t).map { h => (t - h) * h }.count(_ > d).toLong
+    }.product
   }
 
-  override def part2(strings: Seq[String]): Long = {
-    -1
+  private def deriveTimes(strings: Seq[String], part2: Boolean = false) = {
+    val times = strings.head.split(" ").tail.filter(_.nonEmpty).map(_.strip())
+    val distances = strings.tail.head.split(" ").tail.filter(_.nonEmpty).map(_.strip())
+
+    if(!part2) {
+      (times.map(_.toLong).toSeq zip distances.map(_.toLong))
+
+    } else {
+      (Seq(times.foldLeft("")(_+_).toLong) zip Seq(distances.foldLeft("")(_+_).toLong))
+    }
   }
 }
