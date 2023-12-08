45c84277abf2761c191e25ce54dd4c35531f6608
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-08 07:41:43 +0100

    :sparkles: Year 2023 Day 08 - Completed part 1

--- aoc2023/src/main/scala/Day08.scala
+++ aoc2023/src/main/scala/Day08.scala
@@ -8,7 +8,23 @@ object Day08 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val g = groupsSeparatedByTwoNewlines(strings)
+    val instr = g.head.strip()
+    val m = g.last.split("\n").map(_.strip()).filter(_.nonEmpty)
+      .map(s => (s.split(" = ").head.strip(),(s.split(" = ").last.split(", ").head.stripPrefix("("), s.split(" = ").last.split(", ").last.stripSuffix(")"))))
+      .toMap
+    val start = "AAA"
+    val end = "ZZZ"
+    var steps = 0
+    var curr = start
+    while (curr != end) {
+      curr = instr(steps % instr.length) match {
+        case 'L' => m(curr)._1
+        case 'R' => m(curr)._2
+      }
+      steps = steps + 1
+    }
+    steps.toLong
   }
 
   override def part2(strings: Seq[String]): Long = {
