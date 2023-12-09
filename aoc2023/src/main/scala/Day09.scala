fdbfafcf1afabdb2bafb5348f0742750c011b476
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-09 11:20:23 +0100

    :memo: Year 2023 Day 09 - Updated tests

--- aoc2023/src/main/scala/Day09.scala
+++ aoc2023/src/main/scala/Day09.scala
@@ -9,45 +9,24 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  def differences(input: Seq[Seq[Long]]): Seq[Seq[Seq[Long]]] = {
-    input.map { l =>
-      val out = mutable.ListBuffer[Seq[Long]]()
-      out.append(l)
-      while (out.last.exists(_ != 0)) {
-        val value = out.last.sliding(2).map(t => t.last - t.head).toSeq
-        out.append(value)
-      }
-      out.toSeq
+  override def part1(strings: Seq[String]): Long = strings.map(parseInput).map(differences).map(extrapolate(_)).sum
+
+  override def part2(strings: Seq[String]): Long = strings.map(parseInput).map(differences).map(extrapolate(_, part2 = true)).sum
+
+  private def parseInput(input: String): Seq[Long] = {
+    input.split(" ").map(_.toLong).toSeq
+  }
+
+  private def differences(input: Seq[Long]): Seq[Seq[Long]] = {
+    val out = mutable.ListBuffer[Seq[Long]]()
+    out.append(input)
+    while (out.last.exists(_ != 0)) {
+      out.append(out.last.sliding(2).map(t => t.last - t.head).toSeq)
     }
+    out.toSeq
   }
 
-  def extrapolateRight(input: Seq[Seq[Seq[Long]]]): Seq[Long] = {
-    input.map{ls =>
-      ls.foldRight(0L){ case (l, v) =>
-        l.last + v
-      }
-    }
-  }
-
-  def extrapolateLeft(input: Seq[Seq[Seq[Long]]]): Seq[Long] = {
-    input.map { ls =>
-      ls.map(_.head).foldRight(0L) { case (l, v) =>
-        l - v
-      }
-    }
-  }
-
-  override def part1(strings: Seq[String]): Long = {
-    val input = strings.map(_.split(" ").map(_.toLong).toSeq)
-    val calculated = differences(input)
-    val extrapolated = extrapolateRight(calculated)
-    extrapolated.sum
-  }
-
-  override def part2(strings: Seq[String]): Long = {
-    val input = strings.map(_.split(" ").map(_.toLong).toSeq)
-    val calculated = differences(input)
-    val extrapolated = extrapolateLeft(calculated)
-    extrapolated.sum
-  }
+  private def extrapolate(differences: Seq[Seq[Long]], part2: Boolean = false): Long = differences
+    .map(l => if (part2) l.head else l.last)
+    .reduceRight(_ + (if (part2) -1 else 1) * _)
 }
