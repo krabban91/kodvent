93efaaccb1a4483e9ba8fd5360b7903d2e9b01ea
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-09 10:54:59 +0100

    :sparkles: Year 2023 Day 09 - Completed part 1

--- aoc2023/src/main/scala/Day09.scala
+++ aoc2023/src/main/scala/Day09.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day09 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -7,8 +9,31 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  def differences(input: Seq[Seq[Long]]): Seq[Seq[Seq[Long]]] = {
+    input.map { l =>
+      val out = mutable.ListBuffer[Seq[Long]]()
+      out.append(l)
+      while (out.last.exists(_ != 0)) {
+        val value = out.last.sliding(2).map(t => t.last - t.head).toSeq
+        out.append(value)
+      }
+      out.toSeq
+    }
+  }
+
+  def extrapolate(input: Seq[Seq[Seq[Long]]]): Seq[Long] = {
+    input.map{ls =>
+      ls.foldRight(0L){ case (l, v) =>
+        l.last + v
+      }
+    }
+  }
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val input = strings.map(_.split(" ").map(_.toLong).toSeq)
+    val calculated = differences(input)
+    val extrapolated = extrapolate(calculated)
+    extrapolated.sum
   }
 
   override def part2(strings: Seq[String]): Long = {
