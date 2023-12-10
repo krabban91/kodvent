c4dd01f55891a6b9e7e5efc116d0d22390b90be9
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-10 12:54:21 +0100

    :sparkles: Year 2023 Day 10 - Completed part 2

--- aoc2023/src/main/scala/Day10.scala
+++ aoc2023/src/main/scala/Day10.scala
@@ -77,11 +77,45 @@ object Day10 extends App with AoCPart1Test with AoCPart2Test {
   def around(p: (Int, Int)) = Seq((-1, 0), (1, 0), (0, -1), (0, 1))
     .map { case p2@(dx, dy) => (p2, (p._1 + dx, p._2 + dy)) }
 
-  def exploreInterior(loop: Map[(Int, Int), Char], map: Map[(Int, Int), Char]): Seq[((Int, Int), Char)] = {
+  def exploreInterior(loop: Map[(Int, Int), Char], map: Map[(Int, Int), Char]): Set[(Int, Int)] = {
+    val toCheck = map.keySet -- loop.keySet
+    val tl = toCheck.minBy(v => (v._2, v._1))
+    val regions = mutable.HashSet[Set[(Int, Int)]]()
+    val f = mutable.PriorityQueue[(Int, Int)]()(Ordering.by(_._2))
+    f.addAll(toCheck)
+    while (f.nonEmpty) {
+      val pop = f.dequeue()
+      val ns = around(pop)
+        .filter(a => map.contains(a._2))
+        .filterNot(a => loop.contains(a._2))
+      val rs = ns.flatMap(a => regions.find(_.contains(a._2))).toSet
+      if (rs.isEmpty) {
+        //new reg
+        val newReg = Set[(Int, Int)](pop)
+        regions.add(newReg)
+      } else if (rs.size > 1) {
+        // join regs
+        rs.foreach(r => regions.remove(r))
+        val joined = rs.flatten ++ Set(pop)
+        regions.add(joined)
+      } else {
+        // one reg
+        regions.remove(rs.head)
+        regions.add(rs.head ++ Set(pop))
+      }
+    }
+
+    val skipOuter = regions.toSet.filterNot(s => s.contains((0, 0)))
+
+
+    val inside = mutable.HashSet[Set[(Int, Int)]]()
+
+    val topLeft = loop.toSeq.minBy(v => (v._1._2, v._1._1))
+    // F, -> (medsols)
+
     val frontier = mutable.PriorityQueue[((Int, Int), Char, (Int, Int))]()(Ordering.by(_._3))
     val visited = mutable.HashMap[(Int, Int), Char]()
-    val topLeft = loop.toSeq.minBy(v => (v._1._2, v._1._1))
-    frontier.addOne(((topLeft._1._1 - 1, topLeft._1._2+1), topLeft._2, (1, 0)))
+    frontier.addOne((topLeft._1, topLeft._2, (1, 0)))
     while (frontier.nonEmpty) {
       val pop@(p@(x, y), c, direction) = frontier.dequeue()
       if (!visited.contains(p)) {
@@ -90,97 +124,114 @@ object Day10 extends App with AoCPart1Test with AoCPart2Test {
           .filter(a => map.contains(a._2))
           .filterNot(a => visited.contains(a._2))
         direction match {
-          case (1, 0) => c match {
-            case '|' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'J' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case '7' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case _ => around(p)
-              .filter(a => map.contains(a._2))
-              .filterNot(a => visited.contains(a._2))
-              .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-          }
-          case (-1, 0) => c match {
-            case '|' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'L' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'F' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case _ =>
-              around(p)
-                .filter(a => map.contains(a._2))
-                .filterNot(a => visited.contains(a._2))
-                .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
+          case (1, 0) =>
+            val below = (x, y + 1)
+            if (!loop.contains(below)) {
+              regions.find(s => s.contains(below))
+                .foreach(s => inside.add(s))
+            }
+            c match {
+              case '-' =>
+                val nextDirection = direction
+                val next = (x + 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+              case 'J' =>
+                val nextDirection = (0, -1)
+                val next = (x, y - 1)
+                frontier.addOne(next, loop(next), nextDirection)
+              case '7' =>
+                val nextDirection = (0, 1)
+                val next = (x, y + 1)
+                frontier.addOne(next, loop(next), nextDirection)
+              case _ =>
+                if (pop._1 == topLeft._1) {
+                  val nextDirection = direction
+                  val next = (x + 1, y)
+                  frontier.addOne(next, loop(next), nextDirection)
+                } else {
+                  println(s"shouldn't happen: $pop")
+                }
+            }
+          case (-1, 0) =>
+            val above = (x, y - 1)
+            if (!loop.contains(above)) {
+              regions.find(s => s.contains(above))
+                .foreach(s => inside.add(s))
+            }
+            c match {
 
-          }
-          case (0, 1) => c match {
-            case '-' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'L' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'J' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case _ =>
-              around(p)
-                .filter(a => map.contains(a._2))
-                .filterNot(a => visited.contains(a._2))
-                .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
+              case '-' =>
+                val nextDirection = direction
+                val next = (x - 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+              case 'L' =>
+                val nextDirection = (0, -1)
+                val next = (x, y - 1)
+                frontier.addOne(next, loop(next), nextDirection)
 
-          }
-          case (0, -1) => c match {
-            case '-' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case 'F' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case '7' =>
-              if (!loop.contains(p)) {
-                surr
-                  .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-              }
-            case _ =>
-              surr
-                .foreach(a => frontier.addOne(a._2, map(a._2), a._1))
-          }
+              case 'F' =>
+
+                val nextDirection = (0, 1)
+                val next = (x, y + 1)
+                frontier.addOne(next, loop(next), nextDirection)
+              case _ => println(s"shouldn't happen: $pop")
+
+
+            }
+          case (0, 1) =>
+
+            val leftOf = (x - 1, y)
+            if (!loop.contains(leftOf)) {
+              regions.find(s => s.contains(leftOf))
+                .foreach(s => inside.add(s))
+            }
+            c match {
+              case '|' =>
+                val nextDirection = direction
+                val next = (x, y + 1)
+                frontier.addOne(next, loop(next), nextDirection)
+
+              case 'L' =>
+                val nextDirection = (1, 0)
+                val next = (x + 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+
+              case 'J' =>
+                val nextDirection = (-1, 0)
+                val next = (x - 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+              case _ => println(s"shouldn't happen: $pop")
+
+
+            }
+          case (0, -1) =>
+            val rightOf = (x + 1, y)
+            if (!loop.contains(rightOf)) {
+              regions.find(s => s.contains(rightOf))
+                .foreach(s => inside.add(s))
+            }
+            c match {
+
+              case '|' =>
+                val nextDirection = direction
+                val next = (x, y - 1)
+                frontier.addOne(next, loop(next), nextDirection)
+
+              case 'F' =>
+                val nextDirection = (1, 0)
+                val next = (x + 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+
+              case '7' =>
+                val nextDirection = (-1, 0)
+                val next = (x - 1, y)
+                frontier.addOne(next, loop(next), nextDirection)
+              case _ => println(s"shouldn't happen: $pop")
+            }
         }
 
       }
     }
-    (visited.toSet -- loop.toSet).toSeq
+    inside.toSet.flatten
   }
 }
