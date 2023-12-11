251be1e63c8bda30ba541277ba84f544dc4f9ed6
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-11 07:57:22 +0100

    :sparkles: Year 2023 Day 11 - Completed part 1

--- aoc2023/src/main/scala/Day11.scala
+++ aoc2023/src/main/scala/Day11.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day11 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -8,7 +10,41 @@ object Day11 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val parsed = strings.zipWithIndex.flatMap {case (s, y) => s.zipWithIndex.map{case (c, x) => ((x, y), c)}}.filter(_._2 == '#').map(_._1)
+    val galaxies = mutable.HashMap[(Int, Int), (Int, Int)]()
+    parsed.foreach(p => galaxies.put(p, (0, 0)))
+
+    val maxY = galaxies.keySet.map(_._2).max
+    val maxX = galaxies.keySet.map(_._1).max
+    //expand
+
+    (0 to maxY).foreach(y => if (!galaxies.exists(t => t._1._2 == y)) {
+      val toUpdate = galaxies.filter(t => t._1._2 > y)
+      toUpdate.foreach { case (k, (dx, dy)) => galaxies.put(k, (dx, dy + 1)) }
+    } else {
+      ()
+    })
+    (0 to maxX).foreach(x => if (!galaxies.exists(t => t._1._1 == x)) {
+      val toUpdate = galaxies.filter(t => t._1._1 > x)
+      toUpdate.foreach { case (k, (dx, dy)) => galaxies.put(k, (dx + 1, dy)) }
+    } else {
+      ()
+    })
+    val calculated: Set[(Int, Int)] = galaxies.toSeq
+      .map{ case ((x,y), (dx, dy)) => (x+dx, y+dy) }
+      .toSet
+
+
+    //distances
+    val take2 = calculated
+      .flatMap(l => calculated.filter(_ != l).map(r => Set(l, r)))
+      .map(s => s.toSeq)
+      .map(s => (s.head, s.last))
+      .toSeq
+
+    //shortest sum
+    val value = take2.map { case kv@((lx, ly), (rx, ry)) => (kv, (math.abs(lx - rx) + math.abs(ly - ry)).toLong) }
+    value.map(_._2).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
