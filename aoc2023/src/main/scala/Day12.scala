dd7742ff36762ff59cc2b110c51048efa9f80cd8
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-12 09:05:17 +0100

    wip

--- aoc2023/src/main/scala/Day12.scala
+++ aoc2023/src/main/scala/Day12.scala
@@ -10,12 +10,14 @@ object Day12 extends App with AoCPart1Test with AoCPart2Test {
   case class Row(pattern: String, combinations: Seq[Int]) {
     def countArrangements: Long = {
       if (pattern.contains('?')) {
+
+
         val value = pattern.foldLeft(Seq[String]("")) { case (s, c) =>
           val value1 = c match {
             case '?' => Seq('.', '#').flatMap(cc => s.map(v => s"$v$cc"))
             case _ => s.map(v => s"$v$c")
           }
-          value1
+          value1.filter(legal)
         }
         value.count(fits)
       } else 1L
@@ -24,6 +26,22 @@ object Day12 extends App with AoCPart1Test with AoCPart2Test {
     private def fits(s: String): Boolean = {
       val seq = s.split("""\.""").filter(_.nonEmpty).map(_.length).toSeq
       seq == combinations
+    }
+
+    private def legal(s: String): Boolean = {
+      val seq = s.split("""\.""").filter(_.nonEmpty).map(_.length).toSeq
+      if (seq.isEmpty) {
+        true
+      } else {
+        val value = combinations.take(seq.size)
+        (value.dropRight(1) zip seq.dropRight(1)).forall{case (l, r) =>l == r} && value.last >= seq.last
+      }
+    }
+
+
+    def unfold: Row = {
+
+      Row(Seq.fill(5)(pattern).mkString("?"), Seq.fill(5)(combinations).flatten)
     }
   }
 
@@ -40,6 +58,7 @@ object Day12 extends App with AoCPart1Test with AoCPart2Test {
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val rows = strings.map(Row(_)).map(_.unfold)
+    rows.map(_.countArrangements).sum
   }
 }
