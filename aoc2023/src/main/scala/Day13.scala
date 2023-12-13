510e80e5a5965fa83853aac52b38649fa16cff55
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-13 10:51:13 +0100

    :sparkles: Year 2023 Day 13 - Completed part 2

--- aoc2023/src/main/scala/Day13.scala
+++ aoc2023/src/main/scala/Day13.scala
@@ -8,44 +8,54 @@ object Day13 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   case class Pattern(map: Set[(Long, Long)]) {
-    def vertical: Option[Long] = {
-      val rows = map.groupBy(_._1)
-      (0L to rows.keySet.max - 1).find { x =>
-        val l = rows.filter(_._1 <= x).toSeq.sortBy(_._1).reverse
-        val r = rows.filter(_._1 > x).toSeq.sortBy(_._1)
-        val z = l.zip(r).map(tt => (tt._1._2.map(_._2), tt._2._2.map(_._2)))
-        val bool = z.forall { case (ls, rs) => ls == rs }
-        bool
-      }
-    }
 
-    def horisontal: Option[Long] = {
-      val columns = map.groupBy(_._2)
-      (0L to columns.keySet.max - 1).find { y =>
-        val l = columns.filter(_._1 <= y).toSeq.sortBy(_._1).reverse
-        val r = columns.filter(_._1 > y).toSeq.sortBy(_._1)
-        val z = l.zip(r).map(tt => (tt._1._2.map(_._1), tt._2._2.map(_._1)))
-        val bool = z.forall { case (ls, rs) => ls == rs }
-        bool
+    def reflectionRow(part2: Boolean = false): Option[Long] = {
+      val powers = (0L to map.map(_._1).max).map(math.pow(2, _).toLong)
+      val columns = map.groupBy(_._2).map{ case (k, s) => (k, s.map(_._1))}
+      (1L to columns.keySet.max).findLast { y =>
+        val u = columns.filter(_._1 < y).toSeq.sortBy(_._1).reverse.map(_._2)
+        val d = columns.filter(_._1 >= y).toSeq.sortBy(_._1).map(_._2)
+        val z = u.zip(d)
+          .map { case (l, r) => (l.map(math.pow(2, _).toLong).sum, r.map(math.pow(2, _).toLong).sum) }
+
+        val diff = z.map { case (ls, rs) =>
+          val xor = ls ^ rs
+          val l1 = math.abs(ls - rs)
+
+          if (powers.contains(xor)) 1 else xor
+        }.sum
+        diff == (if (part2) 1 else 0)
       }
     }
+    def transpose: Pattern = Pattern(map.map(p => (p._2, p._1)))
   }
+
 
   object Pattern {
     def apply(str: String): Pattern = {
       val lines = str.split("\n").map(_.strip()).filter(_.nonEmpty)
-      Pattern(lines.zipWithIndex.flatMap{ case (str, y) => str.zipWithIndex.filter{ case (c, _) =>c == '#'}.map{ case (_, x) =>(x.toLong,y.toLong)}}.toSet)
+      Pattern(lines.zipWithIndex.flatMap { case (str, y) => str.zipWithIndex.filter { case (c, _) => c == '#' }.map { case (_, x) => (x.toLong, y.toLong) } }.toSet)
     }
   }
 
   override def part1(strings: Seq[String]): Long = {
     val patterns = groupsSeparatedByTwoNewlines(strings).map(Pattern(_))
-    val value = patterns.map(v => v.vertical.map(_ + 1).orElse(v.horisontal.map(y => (y + 1) * 100)).get)
+    val value = patterns.map{v =>
+      val vertical = v.reflectionRow().map(y => (y) * 100).getOrElse(0L)
+      val horisontal = v.transpose.reflectionRow().getOrElse(0L)
+      math.max(vertical, horisontal)
+    }
     value.sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val patterns = groupsSeparatedByTwoNewlines(strings).map(Pattern(_))
+    val value = patterns.map{v =>
+      val vertical = v.reflectionRow(part2 = true).map(y => (y) * 100).getOrElse(0L)
+      val horisontal = v.transpose.reflectionRow(part2 = true).getOrElse(0L)
+      math.max(vertical, horisontal)
+    }
+    value.sum
   }
 }
 
