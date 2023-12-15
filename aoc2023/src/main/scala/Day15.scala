08cb6181e832b16f0205b83f7b5ca09a999bf4c4
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-15 07:13:57 +0100

    :sparkles: Year 2023 Day 15 - Completed part 1

--- aoc2023/src/main/scala/Day15.scala
+++ aoc2023/src/main/scala/Day15.scala
@@ -7,8 +7,24 @@ object Day15 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  case class HASH(str: String) {
+    def value : Long = {
+      calculate(0L)
+    }
+    def calculate(initialValue: Long): Long = {
+      val out = str.foldLeft(0L){case (o, h) =>
+        val inc = o + h.toLong
+        val mul = inc * 17
+        val byte = mul % 256
+        byte
+      }
+
+      out
+    }
+  }
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    strings.head.split(",").map(HASH).toSeq.map(_.value).map(_.toLong).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
