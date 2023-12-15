920b0e427c29f20e4144e167b4c1ee07fe609544
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-15 08:08:58 +0100

    :memo: Year 2023 Day 15 - Updated tests

--- aoc2023/src/main/scala/Day15.scala
+++ aoc2023/src/main/scala/Day15.scala
@@ -9,71 +9,37 @@ object Day15 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  case class HASH(str: String) {
-    def value: Long = {
-      calculate(str)
-    }
-
-    def calculate(v: String): Long = {
-      val out = v.foldLeft(0L) { case (o, h) =>
-        val inc = o + h.toLong
-        val mul = inc * 17
-        val byte = mul % 256
-        byte
-      }
-
-      out
-    }
-    def labelString: String = {
-      str.split("=").head.split("-").head
-    }
-
-    def operation: (Boolean, Long, Long) = {
-      if (str.contains("=")) {
-        // =
-        val s = str.split("=")
-        (true, calculate(s.head), s.last.toLong)
-      } else {
-        // -
-        val s = str.split("\\-")
-        (false, calculate(s.head), 0)
-      }
-    }
-
-    def label: Long = operation._2
-
-    def focalLength: Long = operation._3
-  }
-
   override def part1(strings: Seq[String]): Long = {
-    strings.head.split(",").map(HASH).toSeq.map(_.value).map(_.toLong).sum
+    strings.head.split(",").map(HASH).toSeq.map(_.initialize).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
-    val hashMap = mutable.HashMap[Long, Seq[HASH]]()
+    val HASHMap = mutable.HashMap[Long, Seq[HASH]]()
     strings.head.split(",").map(HASH).toSeq.foreach { h =>
-      val op = h.operation
       val loc = h.label
-      if (op._1) {
-
-        val curr = hashMap.getOrElse(loc, Seq[HASH]())
-        if (!curr.exists(_.labelString == h.labelString)) {
-          hashMap.put(loc, curr.filterNot(_.labelString == h.labelString) ++ Seq(h))
-        } else {
-          val before = curr.takeWhile(_.labelString != h.labelString)
-          val after = curr.dropWhile(_.labelString != h.labelString).drop(1)
-          hashMap.put(loc,before ++ Seq(h) ++ after)
-        }
+      if (h.operation) {
+        val curr = HASHMap.getOrElse(loc, Seq[HASH]())
+        val i = curr.indexWhere(_.labelString == h.labelString)
+        HASHMap.put(loc, if (i == -1) curr ++ Seq(h) else curr.take(i) ++ Seq(h) ++ curr.drop(i + 1))
       } else {
-        hashMap.put(loc, hashMap.getOrElse(loc, Seq[HASH]()).filterNot(_.labelString == h.labelString))
+        HASHMap.put(loc, HASHMap.getOrElse(loc, Seq[HASH]()).filterNot(_.labelString == h.labelString))
       }
     }
-    hashMap.map { case (i, lenses) => lenses.zipWithIndex
-        .map { case (h, ix) =>
-          val length = h.focalLength
-          val l = (i + 1) * (ix + 1) * length
-          l
-        }.sum
-      }.sum
+    HASHMap.map { case (i, lenses) => lenses.zipWithIndex.map { case (h, ix) => (i + 1) * (ix + 1) * h.focalLength }.sum }.sum
   }
+
+  case class HASH(str: String) {
+    def initialize: Long = calculate(str)
+
+    def label: Long = calculate(labelString)
+
+    def labelString: String = str.split("=").head.split("-").head
+
+    def operation: Boolean = str.contains("=")
+
+    def focalLength: Long = if (str.contains("=")) str.split("=").last.toLong else 0L
+
+    private def calculate(v: String): Long = v.foldLeft(0L) { case (o, h) => ((o + h.toLong) * 17) % 256 }
+  }
+
 }
