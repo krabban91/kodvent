db4e2e38a9abc205be04c960e88408a5709864fe
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-16 15:30:13 +0100

    :art: Add an implicit set of functions for long tuples

--- aoc2023/src/main/scala/Day11.scala
+++ aoc2023/src/main/scala/Day11.scala
@@ -1,36 +1,39 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import implicits.Tuples.{RichTuples2Longs, ZERO2}
 
 import scala.collection.mutable
 
 object Day11 extends App with AoCPart1Test with AoCPart2Test {
 
-  override def part1(strings: Seq[String]): Long = {
-    val parsed = strings.zipWithIndex.flatMap {case (s, y) => s.zipWithIndex.map{case (c, x) => ((x, y), c)}}.filter(_._2 == '#').map(_._1)
-    distances(parsed, 2)
-  }
+  override def part1(strings: Seq[String]): Long = distances(parse(strings), 2)
 
-  private def distances(parsed: Seq[(Int, Int)], unit: Int) = {
-    val galaxies = mutable.HashMap[(Int, Int), (Int, Int)]()
-    parsed.foreach(p => galaxies.put(p, (0, 0)))
+  override def part2(strings: Seq[String]): Long = distances(parse(strings), 1000000)
+
+  private def distances(parsed: Seq[(Long, Long)], unit: Int) = {
+    val galaxies = mutable.HashMap[(Long, Long), (Long, Long)]()
+    parsed.foreach(p => galaxies.put(p, ZERO2))
 
     val maxY = galaxies.keySet.map(_._2).max
     val maxX = galaxies.keySet.map(_._1).max
     //expand
 
-    (0 to maxY).foreach(y => if (!galaxies.exists(t => t._1._2 == y)) {
+    val added = 1L * unit - 1
+    val rowDelta = (0L, added)
+    val columnDelta = (added, 0L)
+    (0L to maxY).foreach(y => if (!galaxies.exists(t => t._1._2 == y)) {
       val toUpdate = galaxies.filter(t => t._1._2 > y)
-      toUpdate.foreach { case (k, (dx, dy)) => galaxies.put(k, (dx, dy + 1 * unit - 1)) }
+      toUpdate.foreach { case (k, p) => galaxies.put(k, p + rowDelta) }
     } else {
       ()
     })
-    (0 to maxX).foreach(x => if (!galaxies.exists(t => t._1._1 == x)) {
+    (0L to maxX).foreach(x => if (!galaxies.exists(t => t._1._1 == x)) {
       val toUpdate = galaxies.filter(t => t._1._1 > x)
-      toUpdate.foreach { case (k, (dx, dy)) => galaxies.put(k, (dx + unit-1, dy)) }
+      toUpdate.foreach { case (k, p) => galaxies.put(k, p + columnDelta) }
     } else {
       ()
     })
-    val calculated: Set[(Int, Int)] = galaxies.toSeq
-      .map { case ((x, y), (dx, dy)) => (x + dx, y + dy) }
+    val calculated: Set[(Long, Long)] = galaxies.toSeq
+      .map { case (p, dir) => p + dir }
       .toSet
 
 
@@ -42,12 +45,12 @@ object Day11 extends App with AoCPart1Test with AoCPart2Test {
       .toSeq
 
     //shortest sum
-    val value = take2.map { case kv@((lx, ly), (rx, ry)) => (kv, (math.abs(lx - rx) + math.abs(ly - ry)).toLong) }
+    val value = take2.map { case kv@(l, r) => (kv, l manhattan r) }
     value.map(_._2).sum
   }
 
-  override def part2(strings: Seq[String]): Long = {
-    val parsed = strings.zipWithIndex.flatMap { case (s, y) => s.zipWithIndex.map { case (c, x) => ((x, y), c) } }.filter(_._2 == '#').map(_._1)
-    distances(parsed, 1000000)
+
+  private def parse(strings: Seq[String]) = {
+    strings.zipWithIndex.flatMap { case (s, y) => s.zipWithIndex.map { case (c, x) => ((x.toLong, y.toLong), c) } }.filter(_._2 == '#').map(_._1)
   }
 }
