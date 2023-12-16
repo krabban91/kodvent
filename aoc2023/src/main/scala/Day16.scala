8dd2e544337dff43f236fabcb70a971068d4c788
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-16 15:30:13 +0100

    :art: Add an implicit set of functions for long tuples

--- aoc2023/src/main/scala/Day16.scala
+++ aoc2023/src/main/scala/Day16.scala
@@ -1,3 +1,4 @@
+import implicits.Tuples._
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
 import scala.collection.mutable
@@ -9,22 +10,19 @@ object Day16 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  override def part1(strings: Seq[String]): Long = followLight(((0, 0), (1, 0)), parse(strings)).size
+  override def part1(strings: Seq[String]): Long = followLight((ZERO2, EAST), parse(strings)).size
 
   override def part2(strings: Seq[String]): Long = {
     val m = parse(strings)
     startLocations(m).map(followLight(_, m)).map(_.size).max
   }
 
-  private def directions = ((0L, -1L), (0L, 1L), (1L, 0L), (-1L, 0L))
-
   private def followLight(start: ((Long, Long), (Long, Long)), m: Map[(Long, Long), Char]) = {
     val energized = mutable.HashMap[(Long, Long), Set[(Long, Long)]]()
-    val (n, s, e, w) = directions
     val frontier: mutable.PriorityQueue[((Long, Long), (Long, Long))] = mutable.PriorityQueue[((Long, Long), (Long, Long))]()(Ordering.by(_._1))
     frontier.addOne(start)
     while (frontier.nonEmpty) {
-      val (p@(x, y), dir@(dx, dy)) = frontier.dequeue()
+      val (p, dir) = frontier.dequeue()
       if (!energized.contains(p)) {
         energized.put(p, Set())
       }
@@ -32,31 +30,19 @@ object Day16 extends App with AoCPart1Test with AoCPart2Test {
       if (!ls.contains(dir)) {
         energized.put(p, Set(dir) ++ ls)
         // new state
-        val next: Set[((Long, Long), (Long, Long))] = m(p) match {
-          case '/' => dir match {
-            case `e` => Set(((x, y - 1), n))
-            case `w` => Set(((x, y + 1), s))
-            case `s` => Set(((x - 1, y), w))
-            case `n` => Set(((x + 1, y), e))
-          }
-          case '\\' => dir match {
-            case `e` => Set(((x, y + 1), s))
-            case `w` => Set(((x, y - 1), n))
-            case `s` => Set(((x + 1, y), e))
-            case `n` => Set(((x - 1, y), w))
-          }
-          case '-' => dir match {
-            case `s` | `n` => Set(((x - 1, y), w), ((x + 1, y), e))
-            case _ => Set(((x + dx, y + dy), dir))
-          }
-          case '|' => dir match {
-            case `w` | `e` => Set(((x, y - 1), n), ((x, y + 1), s))
-            case _ => Set(((x + dx, y + dy), dir))
-          }
-          case '.' => Set(((x + dx, y + dy), dir))
+        val nextDir = (m(p), dir) match {
+          case ('/', `EAST`) | ('\\', `WEST`) => Set(NORTH)
+          case ('/', `WEST`) | ('\\', `EAST`) => Set(SOUTH)
+          case ('/', `SOUTH`) | ('\\', `NORTH`) => Set(WEST)
+          case ('/', `NORTH`) | ('\\', `SOUTH`) => Set(EAST)
+          case ('-', `NORTH`) | ('-', `SOUTH`) => Set(WEST, EAST)
+          case ('|', `WEST`) | ('|', `EAST`) => Set(NORTH, SOUTH)
+          case _ => Set(dir)
         }
-        val filtered = next.filter(t => m.contains(t._1))
-        frontier.addAll(filtered)
+        val next = nextDir
+          .map(d => (p + d, d))
+          .filter(t => m.contains(t._1))
+        frontier.addAll(next)
       }
     }
     energized.toMap
@@ -65,19 +51,16 @@ object Day16 extends App with AoCPart1Test with AoCPart2Test {
   private def startLocations(m: Map[(Long, Long), Char]) = {
     val maxX = m.map(_._1._1).max
     val maxY = m.map(_._1._2).max
-    val borders = (0L to maxX)
-      .flatMap(x => (0L to maxY).map(y => (x, y)))
-      .filter { case (x, y) => x == 0L || x == maxX || y == 0L || y == maxY }
-    val (n, s, e, w) = directions
-    val starts = borders.flatMap(p => Set((p, n)).filter(_ => p._2 == maxY) ++
-      Set((p, s)).filter(_ => p._2 == 0) ++
-      Set((p, e)).filter(_ => p._1 == 0) ++
-      Set((p, w)).filter(_ => p._1 == maxX))
+    val borders = (ZERO2 to (maxX, maxY))
+      .filter { case (x, y) => Seq(0L, maxX).contains(x) || Seq(0L, maxY).contains(y) }
+    val starts = borders.flatMap(p => Set((p, NORTH)).filter(_ => p._2 == maxY) ++
+      Set((p, SOUTH)).filter(_ => p._2 == 0) ++
+      Set((p, EAST)).filter(_ => p._1 == 0) ++
+      Set((p, WEST)).filter(_ => p._1 == maxX))
     starts
   }
 
   private def parse(strings: Seq[String]) = {
     strings.zipWithIndex.flatMap { case (str, y) => str.zipWithIndex.map { case (c, x) => ((x.toLong, y.toLong), c) } }.toMap
-
   }
 }
