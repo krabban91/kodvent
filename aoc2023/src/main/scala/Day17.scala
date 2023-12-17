c90fa985063415944a45b551a86104079cf8c411
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-17 09:41:39 +0100

    :memo: Year 2023 Day 17 - Updated tests

--- aoc2023/src/main/scala/Day17.scala
+++ aoc2023/src/main/scala/Day17.scala
@@ -1,6 +1,5 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 import implicits.Tuples._
-import scala.collection.mutable
 
 object Day17 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -9,42 +8,39 @@ object Day17 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  override def part1(strings: Seq[String]): Long = {
+    val (city, starts, end) = parse(strings)
+    Graph.shortestPath[CityLocation](starts, p => p.pos == end, p => p.pos manhattan end, p => p.neighbors(false)(city))
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    val (city, starts, end) = parse(strings)
+    Graph.shortestPath[CityLocation](starts, p => p.pos == end, p => p.pos manhattan end, p => p.neighbors(true)(city))
+  }
+
   case class CityLocation(pos: (Long, Long), direction: (Long, Long), sameDirection: Long) {
-    def neighbors(city: Map[(Long, Long), Long]): Seq[(CityLocation, Long)] = {
+
+    def neighbors(part2: Boolean): Map[(Long, Long), Long] => Seq[(CityLocation, Long)] = if (part2) neighbors2 else neighbors1
+
+    private def neighbors1(city: Map[(Long, Long), Long]): Seq[(CityLocation, Long)] = {
       val directions = Seq(NORTH, SOUTH, WEST, EAST).filterNot(d => (sameDirection >= 3 && d == direction) || d == direction * (-1L, -1L))
-      val next = directions.map(d => (d + pos, d)).filter{ case (p, d) =>city.contains(p)}
-
-      next.map{ case (p, d) => (CityLocation(p, d, if (d == direction) sameDirection + 1 else 1), city(p))}
-
+      val next = directions.map(d => (d + pos, d)).filter { case (p, d) => city.contains(p) }
+      next.map { case (p, d) => (CityLocation(p, d, if (d == direction) sameDirection + 1 else 1), city(p)) }
     }
 
-    def neighbors2(city: Map[(Long, Long), Long]): Seq[(CityLocation, Long)] = {
+    private def neighbors2(city: Map[(Long, Long), Long]): Seq[(CityLocation, Long)] = {
       val directions = Seq(NORTH, SOUTH, WEST, EAST)
         .filterNot(d => (sameDirection >= 10 && d == direction) || d == direction * (-1L, -1L))
         .filter(d => sameDirection >= 4 || d == direction)
       val next = directions.map(d => (d + pos, d)).filter { case (p, d) => city.contains(p) }
       next.map { case (p, d) => (CityLocation(p, d, if (d == direction) sameDirection + 1 else 1), city(p)) }
     }
-
-  }
-
-  override def part1(strings: Seq[String]): Long = {
-    val city = parse(strings)
-    val starts = Seq(CityLocation((0L, 0L), (0L, 1L), 0L)
-      ,CityLocation((0L, 0L), (1L, 0L), 0L))
-    val end = (city.keySet.map(_._1).max, city.keySet.map(_._2).max)
-    Graph.shortestPath[CityLocation](starts, p => p.pos == end, p => p.pos manhattan end, p => p.neighbors(city))
-  }
-
-  override def part2(strings: Seq[String]): Long = {
-    val city = parse(strings)
-    val starts = Seq(CityLocation((0L, 0L), (0L, 1L), 0L)
-      , CityLocation((0L, 0L), (1L, 0L), 0L))
-    val end = (city.keySet.map(_._1).max, city.keySet.map(_._2).max)
-    Graph.shortestPath[CityLocation](starts, p => p.pos == end, p => p.pos manhattan end, p => p.neighbors2(city))
   }
 
   private def parse(strings: Seq[String]) = {
-    strings.zipWithIndex.flatMap { case (str, y) => str.zipWithIndex.map { case (c, x) => ((x.toLong, y.toLong), s"$c".toLong) } }.toMap
+    val map = strings.zipWithIndex.flatMap { case (str, y) => str.zipWithIndex.map { case (c, x) => ((x.toLong, y.toLong), s"$c".toLong) } }.toMap
+    val starts = Seq(CityLocation((0L, 0L), (0L, 1L), 0L), CityLocation((0L, 0L), (1L, 0L), 0L))
+    val end = (strings.head.length.toLong, strings.length.toLong) - (1L, 1L)
+    (map, starts, end)
   }
 }
--- aoc2023/src/test/scala/Day17Spec.scala
+++ aoc2023/src/test/scala/Day17Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day17Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day17.part1TestResult shouldEqual -1
+    Day17.part1TestResult shouldEqual 102
   }
   "Part1" should "be correct" in {
-    Day17.part1Result shouldEqual -1
+    Day17.part1Result shouldEqual 970
   }
   "Part2 Test" should "be correct" in {
-    Day17.part2TestResult shouldEqual -1
+    Day17.part2TestResult shouldEqual 94
   }
   "Part2" should "be correct" in {
-    Day17.part2Result shouldEqual -1
+    Day17.part2Result shouldEqual 1149
   }
 }
