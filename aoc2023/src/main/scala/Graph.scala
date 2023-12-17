abb80d4d5be73f8508604d01880d3d48c2b335a6
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-17 12:45:15 +0100

    :art: Added logging utility to print paths on 2d-maps

--- aoc2023/src/main/scala/Graph.scala
+++ aoc2023/src/main/scala/Graph.scala
@@ -2,21 +2,39 @@ import scala.collection.mutable
 
 object Graph {
 
-  def shortestPath[Point](starts: Seq[Point], end: Point => Boolean, heuristic: Point => Long, neighbors: Point => Seq[(Point, Long)]): Long = {
+  def shortestPath[Point](starts: Seq[Point], end: Point => Boolean, heuristic: Point => Long, neighbors: Point => Seq[(Point, Long)]): (Long, Seq[Point]) = {
     val queue = mutable.PriorityQueue[(Point, Long, Long)]()(Ordering.by(v => -(v._2 + v._3)))
     queue.addAll(starts.map(p => (p, 0L, heuristic(p))))
     val visited = mutable.HashSet[Point]()
+    val cameFrom: mutable.Map[(Point, Long), (Point, Long)] = mutable.HashMap[(Point, Long), (Point, Long)]()
     while (queue.nonEmpty) {
-      val pop = queue.dequeue()
-      if (end.apply(pop._1)) {
-        return pop._2
+      val (pos, cost, _) = queue.dequeue()
+      if (end.apply(pos)) {
+        val map = cameFrom.toMap
+        val path = derivePath(pos, cost, map)
+
+        return (cost, path)
       }
-      if (visited.add(pop._1)) {
-        neighbors.apply(pop._1)
+      if (visited.add(pos)) {
+        neighbors.apply(pos)
           .filterNot(t => visited.contains(t._1))
-          .foreach(p => queue.enqueue((p._1, pop._2 + p._2, heuristic(p._1))))
+          .foreach{p =>
+            queue.enqueue((p._1, cost + p._2, heuristic(p._1)))
+            cameFrom.put((p._1, cost + p._2), (pos, cost))
+          }
       }
     }
-    Long.MaxValue
+    (Long.MaxValue, Seq())
+  }
+
+  private def derivePath[Point](to: Point, cost: Long, paths: Map[(Point, Long), (Point, Long)]): Seq[Point] = {
+    val out = mutable.ListBuffer[Point]()
+    var loc = (to, cost)
+    out.addOne(loc._1)
+    while(paths.contains(loc)) {
+      loc = paths(loc)
+      out.addOne(loc._1)
+    }
+    out.toSeq.reverse
   }
 }
