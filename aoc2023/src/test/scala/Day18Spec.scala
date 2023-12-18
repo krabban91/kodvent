4667d7f95fc814018a6a253f84d1fd62f4eaa221
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-18 06:49:21 +0100

    :package: Year 2023 Day 18 preparations

---
+++ aoc2023/src/test/scala/Day18Spec.scala
@@ -0,0 +1,17 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day18Spec extends AnyFlatSpec with Matchers {
+  "Part1 Test" should "be correct" in {
+    Day18.part1TestResult shouldEqual -1
+  }
+  "Part1" should "be correct" in {
+    Day18.part1Result shouldEqual -1
+  }
+  "Part2 Test" should "be correct" in {
+    Day18.part2TestResult shouldEqual -1
+  }
+  "Part2" should "be correct" in {
+    Day18.part2Result shouldEqual -1
+  }
+}
