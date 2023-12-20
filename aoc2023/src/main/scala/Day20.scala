4e675658c0a3b65802c8a91466ef5450dbbe585b
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-20 20:56:45 +0100

    :sparkles: Year 2023 Day 20 - Completed part 2
    * The issue lied in faulty state-passing
    
    -        val v@(repeat, next, targets) = n.pulse(high, from)
    +        val v@(repeat, next, targets) = nodes(n.id).pulse(high, from)
    
    instead of getting the state from nodes, i passed an old state of the node
    * strangely enough, this worked when handling part 1

--- aoc2023/src/main/scala/Day20.scala
+++ aoc2023/src/main/scala/Day20.scala
@@ -1,5 +1,4 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
-import krabban91.kodvent.kodvent.utilities.MathUtils.LCM
 
 import scala.collection.mutable
 
@@ -26,20 +25,24 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
       //simulate button
       val toHandle = mutable.Queue[(Node, Boolean, String)]()
       toHandle.append((bc, false, "button"))
-      if (debug){println(s"button --low-> ${bc.name} ($time)")}
+      if (debug) {
+        println(s"button --low-> ${bc.name} ($time)")
+      }
       lowPulses += 1L
       while (toHandle.nonEmpty) {
         val pop@(n, high, from) = toHandle.dequeue()
         val (repeat, next, targets) = n.pulse(high, from)
         if (repeat.isDefined) {
           val value = targets.map(v => (v, nodes.get(v))).map((_, repeat.get, n.id))
-          if (debug) {value.foreach {case ((id, n), b, s) => println(s"$s --${if(b)"high" else "low"}--> ${id}")}}
+          if (debug) {
+            value.foreach { case ((id, n), b, s) => println(s"$s --${if (b) "high" else "low"}--> ${id}") }
+          }
           if (repeat.get) {
             highPulses += value.size
           } else {
             lowPulses += value.size
           }
-          toHandle.addAll(value.collect{ case ((_, Some(n)), bool, str) => (n, bool, str) })
+          toHandle.addAll(value.collect { case ((_, Some(n)), bool, str) => (n, bool, str) })
           nodes.put(n.id, next)
         }
         //println(nodes)
@@ -57,16 +60,22 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
     val tmpC = strings.filter(_.startsWith("&")).map(s => Conjunction(s, Seq()))
     val allNodes = (invs.map(n => (n.id, n)).toMap ++ tmpC.map(n => (n.id, n)).toMap ++ Map((broadcast.id -> broadcast)))
     val conjs = tmpC.map(c => Conjunction(c.id, allNodes.values.filter(_.targets.contains(c.id)).map(v => (v.id, false)).toMap, c.targets))
-    val trigger = conjs.find(_.targets.contains("rx")).get
+    val trigger = conjs.filter(_.targets.contains("rx")).head
     val parentsSecond = mutable.HashMap[String, Option[Long]]()
     val parents = mutable.HashMap[String, Option[Long]]()
+    val parentsThird = mutable.HashMap[String, Seq[Long]]()
     trigger.inputPulses.keySet.foreach(s => parents.put(s, None))
     trigger.inputPulses.keySet.foreach(s => parentsSecond.put(s, None))
+    trigger.inputPulses.keySet.foreach(s => parentsThird.put(s, Seq()))
     var highPulses = 0L
     var lowPulses = 0L
     val maxStates = mutable.HashMap[Map[String, Boolean], Long]()
-    (0 to 100000).foldLeft((broadcast, invs, conjs)) { case (computer@(bc, in, c), time) =>
-      if (parentsSecond.exists(_._2.isEmpty)) {
+    var computer = (broadcast, invs, conjs)
+    var it = 0L
+    while (parents.exists(_._2.isEmpty)) {
+      val (bc, in, c) = computer
+      val time = it
+
 
       var hp = 0L
       var lp = 0L
@@ -83,13 +92,18 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
       lowPulses += 1L
       while (toHandle.nonEmpty) {
         val pop@(n, high, from) = toHandle.dequeue()
-        val v@(repeat, next, targets) = n.pulse(high, from)
+        val v@(repeat, next, targets) = nodes(n.id).pulse(high, from)
 
         if (n.id == trigger.id && high) {
           if (parentsSecond(from).isEmpty) {
             parentsSecond.put(from, Some(time))
           }
+          val c = n.asInstanceOf[Conjunction]
+          if (!c.inputPulses(from)) {
+            parentsThird.put(from, parentsThird(from) :+ time)
+          }
         }
+
 
         if (repeat.isDefined) {
           if (parents.contains(n.id) && parents(n.id).isEmpty) {
@@ -97,13 +111,10 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
               parents.put(n.id, Some(time))
             }
           }
-          if (parents.contains(n.id) && parents(n.id).isDefined && parents(n.id).get != time && parentsSecond.contains(n.id) && parentsSecond(n.id).isEmpty) {
-            if (repeat.get) {
-              parentsSecond.put(n.id, Some(time - parents(n.id).get))
-            }
-          }
           val value = targets.map(v => (v, nodes.get(v))).map((_, repeat.get, n.id))
-          if (debug) {
+          if (n.id == trigger.id && high) {
+
+            println(s"$time\n## $from --${if (high) "high" else "low"}--> ${n.id}")
             value.foreach { case ((id, n), b, s) => println(s"$s --${if (b) "high" else "low"}--> ${id}") }
           }
           if (repeat.get) {
@@ -130,21 +141,52 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
         }
         //println(nodes)
       }
+      /*
+              val vc = nodes(trigger.id).asInstanceOf[Conjunction]
+              vc.inputPulses.filter(_._2).foreach{ case (s, b) =>
+                parentsThird.put(s, parentsThird(s) :+ time)
+              }
+      */
       val nextComputer = (nodes(bc.id).asInstanceOf[Broadcast], in.map(i => nodes(i.id).asInstanceOf[FlipFlop]), c.map(i => nodes(i.id).asInstanceOf[Conjunction]))
-      highPulses +=hp
-      lowPulses +=lp
+      highPulses += hp
+      lowPulses += lp
       //if (adjusted)
       //println(s"$hp\t$lp")
-      nextComputer
-      } else {computer}
+      if (it == 1000 - 1) {
+        println(s"part1-score: ${highPulses * lowPulses}")
+      }
+      computer = nextComputer
+      it += 1
     }
     val cycle = parents.values.map(_.get + 1).reduce(lcm)
+    val cycle0 = parents.values.map(_.get).reduce(lcm)
+    val cycle1 = parents.values.map(_.get - 1).reduce(lcm)
     val cycle2 = parentsSecond.values.map(_.get + 1).reduce(lcm)
-    val mul = parents.values.map(_.get + 1).product
+    /*
+    val cs = parentsThird.values.map(_.distinct).toSeq
+    val ds = cs.map(l => l.sliding(3).map{ case v => (v(1) - v(0), v(2) - v(0))}.toSeq.groupBy(v => math.max(v._1, v._2)).map(v => (v._1, v._2.size)).toSeq.sortBy(_._2).reverse)
+
+    val calced = cs.map{ l =>
+        (l(1) - l(0)) match {
+          case 1 => l(2) - l(0)
+          case 2 => l(2) - l(0)
+          case 3 => l(2) - l(0)
+          case d => d
+        }
+      }
+    val cycles3 = calced.reduce(lcm)
+    val cycles4 = ds.map(_.head._1).map(_ + 1).reduce(lcm)
+    val cycles5 = ds.map(_.filter(_._2 > 0).maxBy(_._1)._1).map(_ + 1).reduce(lcm)
+
+     */
+    val mul = parents.values.map(_.get).product
+    val mul2 = parents.values.map(_.get + 1).product
     //val start = highs.values.max
     cycle
 
   }
+
+  def isPrime(n: Int): Boolean = !((2 until n - 1) exists (n % _ == 0))
 
   def gcd(l: Long, r: Long): Long = {
     var a = l
@@ -168,6 +210,7 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
     def targets: Seq[String]
 
     def pulse(high: Boolean, from: String): (Option[Boolean], Node, Seq[String])
+
     def name: String
   }
 
@@ -206,6 +249,7 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
       (someBoolean, Conjunction(id, inputs, targets), targets)
 
     }
+
     override def name: String = s"&$id"
 
   }
@@ -214,6 +258,7 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
     override def pulse(high: Boolean, from: String): (Option[Boolean], Node, Seq[String]) = (Some(high), this, targets)
 
     override def id: String = "broadcaster"
+
     override def name: String = s"$id"
 
   }
