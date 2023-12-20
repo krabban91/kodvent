e7bd73838afa821afaf1228a03fc51d5539e2880
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-20 14:53:55 +0100

    :construction: Year 2023 Day 20 - Working on part 2

--- aoc2023/src/main/scala/Day20.scala
+++ aoc2023/src/main/scala/Day20.scala
@@ -1,11 +1,12 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import krabban91.kodvent.kodvent.utilities.MathUtils.LCM
 
 import scala.collection.mutable
 
 object Day20 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
-  printResultPart2Test
+  //printResultPart2Test
   printResultPart1
   printResultPart2
 
@@ -50,7 +51,114 @@ object Day20 extends App with AoCPart1Test with AoCPart2Test {
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val debug = false
+    val invs = strings.filter(_.startsWith("%")).map(FlipFlop(_))
+    val broadcast = Broadcast(strings.find(_.startsWith("broadcaster")).get)
+    val tmpC = strings.filter(_.startsWith("&")).map(s => Conjunction(s, Seq()))
+    val allNodes = (invs.map(n => (n.id, n)).toMap ++ tmpC.map(n => (n.id, n)).toMap ++ Map((broadcast.id -> broadcast)))
+    val conjs = tmpC.map(c => Conjunction(c.id, allNodes.values.filter(_.targets.contains(c.id)).map(v => (v.id, false)).toMap, c.targets))
+    val trigger = conjs.find(_.targets.contains("rx")).get
+    val parentsSecond = mutable.HashMap[String, Option[Long]]()
+    val parents = mutable.HashMap[String, Option[Long]]()
+    trigger.inputPulses.keySet.foreach(s => parents.put(s, None))
+    trigger.inputPulses.keySet.foreach(s => parentsSecond.put(s, None))
+    var highPulses = 0L
+    var lowPulses = 0L
+    val maxStates = mutable.HashMap[Map[String, Boolean], Long]()
+    (0 to 100000).foldLeft((broadcast, invs, conjs)) { case (computer@(bc, in, c), time) =>
+      if (parentsSecond.exists(_._2.isEmpty)) {
+
+      var hp = 0L
+      var lp = 0L
+      var adjusted = false
+      val tmp = (in.map(n => (n.id, n)).toMap ++ c.map(n => (n.id, n)).toMap ++ Map((bc.id -> bc)))
+      val nodes = mutable.HashMap[String, Node]()
+      nodes.addAll(tmp)
+      //simulate button
+      val toHandle = mutable.Queue[(Node, Boolean, String)]()
+      toHandle.append((bc, false, "button"))
+      if (debug) {
+        println(s"button --low-> ${bc.name} ($time)")
+      }
+      lowPulses += 1L
+      while (toHandle.nonEmpty) {
+        val pop@(n, high, from) = toHandle.dequeue()
+        val v@(repeat, next, targets) = n.pulse(high, from)
+
+        if (n.id == trigger.id && high) {
+          if (parentsSecond(from).isEmpty) {
+            parentsSecond.put(from, Some(time))
+          }
+        }
+
+        if (repeat.isDefined) {
+          if (parents.contains(n.id) && parents(n.id).isEmpty) {
+            if (repeat.get) {
+              parents.put(n.id, Some(time))
+            }
+          }
+          if (parents.contains(n.id) && parents(n.id).isDefined && parents(n.id).get != time && parentsSecond.contains(n.id) && parentsSecond(n.id).isEmpty) {
+            if (repeat.get) {
+              parentsSecond.put(n.id, Some(time - parents(n.id).get))
+            }
+          }
+          val value = targets.map(v => (v, nodes.get(v))).map((_, repeat.get, n.id))
+          if (debug) {
+            value.foreach { case ((id, n), b, s) => println(s"$s --${if (b) "high" else "low"}--> ${id}") }
+          }
+          if (repeat.get) {
+            hp += value.size
+            if (targets.contains("rx")) {
+              val before = n.asInstanceOf[Conjunction].inputPulses
+              val after = next.asInstanceOf[Conjunction].inputPulses
+              if (after != before) {
+                adjusted = true
+                if (!maxStates.contains(after)) {
+                  maxStates.put(after, time)
+                  println(s"$time: $before \t becomes: $after")
+                }
+              }
+            }
+          } else {
+            if (targets.contains("rx")) {
+              return time
+            }
+            lp += value.size
+          }
+          toHandle.addAll(value.collect { case ((_, Some(n)), bool, str) => (n, bool, str) })
+          nodes.put(n.id, next)
+        }
+        //println(nodes)
+      }
+      val nextComputer = (nodes(bc.id).asInstanceOf[Broadcast], in.map(i => nodes(i.id).asInstanceOf[FlipFlop]), c.map(i => nodes(i.id).asInstanceOf[Conjunction]))
+      highPulses +=hp
+      lowPulses +=lp
+      //if (adjusted)
+      //println(s"$hp\t$lp")
+      nextComputer
+      } else {computer}
+    }
+    val cycle = parents.values.map(_.get + 1).reduce(lcm)
+    val cycle2 = parentsSecond.values.map(_.get + 1).reduce(lcm)
+    val mul = parents.values.map(_.get + 1).product
+    //val start = highs.values.max
+    cycle
+
+  }
+
+  def gcd(l: Long, r: Long): Long = {
+    var a = l
+    var b = r
+    while (b != 0) {
+      val tmp = b
+      b = a % b
+      a = tmp
+    }
+    a
+  }
+
+  def lcm(l: Long, r: Long): Long = {
+    l * r / gcd(l, r)
   }
 
   trait Node {
