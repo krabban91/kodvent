9d6cda28844bc1cfd966c687d87205c2cefa6bee
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-21 07:27:15 +0100

    :sparkles: Year 2023 Day 21 - Completed part 1

--- aoc2023/src/main/scala/Day21.scala
+++ aoc2023/src/main/scala/Day21.scala
@@ -1,4 +1,5 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+import implicits.Tuples._
 
 object Day21 extends App with AoCPart1Test with AoCPart2Test {
 
@@ -8,10 +9,27 @@ object Day21 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val (map, start) = parse(strings)
+
+
+
+    val value = Graph.stepAround[(Long, Long)](Seq(start), p => neighbors (p, map), 64)
+      value.size
+
+  }
+
+  def neighbors(p: (Long, Long), map: Map[(Long, Long), String]): Seq[((Long, Long), Long)] = {
+    DIRECTIONS.map(_ + p).filter(map.get(_).exists(v => v == "." || v == "S")).map((_, 1))
   }
 
   override def part2(strings: Seq[String]): Long = {
     -1
+  }
+
+  private def parse(strings: Seq[String]) = {
+    val map = strings.zipWithIndex.flatMap { case (str, y) => str.zipWithIndex.map { case (c, x) => ((x.toLong, y.toLong), s"$c") } }.toMap
+    val start = map.find(_._2 == "S").map(_._1).get
+
+    (map, start)
   }
 }
