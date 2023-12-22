8506d31671895ca54e0e472df8b0709f0285108d
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-22 09:12:32 +0100

    :memo: Year 2023 Day 22 - Updated tests

--- aoc2023/src/main/scala/Day22.scala
+++ aoc2023/src/main/scala/Day22.scala
@@ -10,53 +10,17 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
-  case class Brick(from: (Long, Long, Long), to: (Long, Long, Long)) {
-    def volume: Long = {
-      val (dx, dy, dz) = (from delta to) + (1, 1, 1)
-      dx * dy * dz
-    }
-
-    def xyOverlaps(other: Brick): Boolean = {
-      xOverLap(other) && yOverLap(other)
-    }
-
-    def xOverLap(other: Brick): Boolean = {
-      overlap(from._1, (other.from._1, other.to._1)) || overlap(to._1, (other.from._1, other.to._1)) ||
-        overlap(other.from._1, (from._1, to._1)) || overlap(other.to._1, (from._1, to._1))
-    }
-
-    def yOverLap(other: Brick): Boolean = {
-      overlap(from._2, (other.from._2, other.to._2)) || overlap(to._2, (other.from._2, other.to._2)) ||
-        overlap(other.from._2, (from._2, to._2)) || overlap(other.to._2, (from._2, to._2))
-    }
-  }
-
-  private def overlap(p: Long, other: (Long, Long)) = {
-    p >= math.min(other._1, other._2) && p <= math.max(other._1, other._2)
-  }
-
-  object Brick {
-    def apply(str: String): Brick = {
-      val s = str.split("~")
-      val l = s.head.split(",").map(_.toLong)
-      val r = s.last.split(",").map(_.toLong)
-      Brick((l(0), l(1), l(2)), (r(0), r(1), r(2)))
-    }
-  }
-
   override def part1(strings: Seq[String]): Long = {
-    val supports: mutable.HashMap[Day22.Brick, Seq[Day22.Brick]] = parse(strings)
-
-    supports.count { case (b, bs) =>
-      bs.forall(v => supports.exists(o => o._1 != b && o._2.contains(v)))
-    }
+    val tower: Map[Day22.Brick, Seq[Day22.Brick]] = parse(strings)
+    tower.count { case (b, bs) => bs.forall(v => tower.exists(o => o._1 != b && o._2.contains(v))) }
   }
 
 
   override def part2(strings: Seq[String]): Long = {
-    val supports: mutable.HashMap[Day22.Brick, Seq[Day22.Brick]] = parse(strings)
-    val topples = supports.map { case (b, bs) =>
-      val supportCopy= supports.clone()
+    val tower: Map[Day22.Brick, Seq[Day22.Brick]] = parse(strings)
+    val topples = tower.map { case (b, _) =>
+      val supportCopy = mutable.HashMap[Day22.Brick, Seq[Day22.Brick]]()
+      supportCopy.addAll(tower)
 
       val frontier = mutable.Queue[Brick]()
       frontier.addOne(b)
@@ -96,14 +60,51 @@ object Day22 extends App with AoCPart1Test with AoCPart2Test {
     }
     val supports = mutable.HashMap[Brick, Seq[Brick]]()
     val byMyinY = landed.groupBy(b => math.min(b.from._3, b.to._3))
-    byMyinY.foreach { case (minY, bs) =>
+    byMyinY.foreach { case (_, bs) =>
       bs.map { b =>
         val maxY = math.max(b.from._3, b.to._3)
         val possible = byMyinY.getOrElse(maxY + 1, Seq())
         supports.put(b, possible.filter(_.xyOverlaps(b)))
       }
     }
-    supports
+    supports.toMap
   }
+
+
+  case class Brick(from: (Long, Long, Long), to: (Long, Long, Long)) {
+    def volume: Long = {
+      val (dx, dy, dz) = (from delta to) + (1, 1, 1)
+      dx * dy * dz
+    }
+
+    def xyOverlaps(other: Brick): Boolean = {
+      xOverLap(other) && yOverLap(other)
+    }
+
+    private def xOverLap(other: Brick): Boolean = {
+      overlap(from._1, (other.from._1, other.to._1)) || overlap(to._1, (other.from._1, other.to._1)) ||
+        overlap(other.from._1, (from._1, to._1)) || overlap(other.to._1, (from._1, to._1))
+    }
+
+    private def yOverLap(other: Brick): Boolean = {
+      overlap(from._2, (other.from._2, other.to._2)) || overlap(to._2, (other.from._2, other.to._2)) ||
+        overlap(other.from._2, (from._2, to._2)) || overlap(other.to._2, (from._2, to._2))
+    }
+
+    private def overlap(p: Long, other: (Long, Long)) = {
+      p >= math.min(other._1, other._2) && p <= math.max(other._1, other._2)
+    }
+  }
+
+
+  object Brick {
+    def apply(str: String): Brick = {
+      val s = str.split("~")
+      val l = s.head.split(",").map(_.toLong)
+      val r = s.last.split(",").map(_.toLong)
+      Brick((l(0), l(1), l(2)), (r(0), r(1), r(2)))
+    }
+  }
+
 
 }
--- aoc2023/src/test/scala/Day22Spec.scala
+++ aoc2023/src/test/scala/Day22Spec.scala
@@ -4,16 +4,16 @@ import org.scalatest.matchers.should.Matchers
 
 class Day22Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day22.part1TestResult shouldEqual -1
+    Day22.part1TestResult shouldEqual 5
   }
   "Part1" should "be correct" in {
-    Day22.part1Result shouldEqual -1
+    Day22.part1Result shouldEqual 398
   }
   "Part2 Test" should "be correct" in {
-    Day22.part2TestResult shouldEqual -1
+    Day22.part2TestResult shouldEqual 7
   }
   "Part2" should "be correct" in {
-    Day22.part2Result shouldEqual -1
+    Day22.part2Result shouldEqual 70727
   }
 
   "Brick" should "have volume" in {
