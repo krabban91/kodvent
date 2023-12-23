82d5fa1f2eddecf26554c492de01bf7f066180a1
gandersson <gabriel.andersson@recordedfuture.com>
2023-12-23 10:38:58 +0100

    :sparkles: Year 2023 Day 23 - Completed part 2

--- aoc2023/src/test/scala/Day23Spec.scala
+++ aoc2023/src/test/scala/Day23Spec.scala
@@ -14,4 +14,22 @@ class Day23Spec extends AnyFlatSpec with Matchers {
   "Part2" should "be correct" in {
     Day23.part2Result shouldEqual -1
   }
+
+  "Crossings" should "be valid for test" in {
+    val input = Day23.getInputTest
+    val (map, start, end) = Day23.parse(input)
+    val cs = Day23.getCrossings(map, start)
+    cs.contains((3L, 5L)) shouldBe true
+    cs.contains((5L, 13L)) shouldBe true
+    cs.contains((5L+ 8L, 13L)) shouldBe true
+  }
+
+  "Distances" should "be calculated for first edge" in {
+    val input = Day23.getInputTest
+    val (map, start, end) = Day23.parse(input)
+    val ds = Day23.distances(map, start, end)
+    val targets = ds(start)
+    targets.size shouldEqual 1
+    targets.head shouldEqual ((3L, 5L), 15L)
+  }
 }
