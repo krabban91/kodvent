9803e751fa0787b3925aecc74994bab6c2499862
krabban91 <anderssongabriel665@gmail.com>
2025-01-02 14:36:50 +0100

    :memo: Year 2021 Day 23 - Updated tests

--- aoc2021/src/test/scala/Day23Spec.scala
+++ aoc2021/src/test/scala/Day23Spec.scala
@@ -4,17 +4,17 @@ import org.scalatest.matchers.should.Matchers
 class Day23Spec extends AnyFlatSpec with Matchers {
   behavior of "Part 1"
   it should "Test" in {
-    Day23.part1TestResult shouldEqual -1
+    Day23.part1TestResult shouldEqual 12521
   }
   it should "Real" in {
-    Day23.part1Result shouldEqual -1
+    Day23.part1Result shouldEqual 17120
   }
 
   behavior of "Part 2"
   it should "Test" in {
-    Day23.part2TestResult shouldEqual -1
+    Day23.part2TestResult shouldEqual 44169
   }
   it should "Real" in {
-    Day23.part2Result shouldEqual -1
+    Day23.part2Result shouldEqual 47234
   }
 }
