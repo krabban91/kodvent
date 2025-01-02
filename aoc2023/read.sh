7cc74bce88feb1a0794182ea1e9c131ce29cb6a3
gandersson <gabriel.andersson@recordedfuture.com>
2025-01-02 11:30:25 +0100

    read.sh no longer tries to commit no changes

--- aoc2023/read.sh
+++ aoc2023/read.sh
@@ -5,7 +5,4 @@ d=$( echo $1 | sed "s/^0//")
 filename="src/main/resources/day$1\$.txt"
 curl https://adventofcode.com/2023/day/$d/input -H "cookie: session=$AOC_ID" > "$filename"
 
-git add -u
-git commit -m ":card_file_box: Year 2023 Day $1 - Added input of day"
-
 open https://adventofcode.com/2023/day/$d
