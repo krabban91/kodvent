fdf2dbd5f098e9b3eb6f8e3140a76dd767239a45
gandersson <gabriel.andersson@recordedfuture.com>
2025-01-02 09:35:08 +0100

    :wrench: ignore input files (copyright stuff)

--- aoc2023/read.sh
+++ aoc2023/read.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 
 d=$( echo $1 | sed "s/^0//")
-curl https://adventofcode.com/2023/day/$d/input -H "cookie: session=$AOC_ID" > src/main/resources/day$1\$.txt
+filename="src/main/resources/day$1\$.txt"
+curl https://adventofcode.com/2023/day/$d/input -H "cookie: session=$AOC_ID" > "$filename"
 
 git add -u
 git commit -m ":card_file_box: Year 2023 Day $1 - Added input of day"
