40640cd9e19c86403d801b1d263a2eb2981d338f
krabban91 <anderssongabriel665@gmail.com>
2025-12-02 07:13:21 +0100

    :package: Year 2024 Day 02 preparations

---
+++ aoc2025/src/main/scala/Day02.scala
@@ -0,0 +1,17 @@
+import aoc.numeric.{AoCPart1Test, AoCPart2Test}
+
+object Day02 extends App with AoCPart1Test with AoCPart2Test {
+
+  printResultPart1Test
+  printResultPart2Test
+  printResultPart1
+  printResultPart2
+
+  override def part1(strings: Seq[String]): Long = {
+    -1
+  }
+
+  override def part2(strings: Seq[String]): Long = {
+    -1
+  }
+}
---
+++ aoc2025/src/test/scala/Day02Spec.scala
@@ -0,0 +1,17 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day02Spec extends AnyFlatSpec with Matchers {
+  "Part1 Test" should "be correct" in {
+    Day02.part1TestResult shouldEqual -1
+  }
+  "Part1" should "be correct" in {
+    Day02.part1Result shouldEqual -1
+  }
+  "Part2 Test" should "be correct" in {
+    Day02.part2TestResult shouldEqual -1
+  }
+  "Part2" should "be correct" in {
+    Day02.part2Result shouldEqual -1
+  }
+}
