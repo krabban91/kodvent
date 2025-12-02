b70d0aab718f115cd513bb3acc31b7e21487af94
krabban91 <anderssongabriel665@gmail.com>
2025-12-02 07:23:53 +0100

    :sparkle: Complete Year 2025 Day 02 - part 1

--- aoc2025/src/main/scala/Day02.scala
+++ aoc2025/src/main/scala/Day02.scala
@@ -7,8 +7,21 @@ object Day02 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  def parseRanges(strings: Seq[String]) = {
+    strings.head.split(",").map{s => s.split("-") match {
+      case Array(start, end) => (start.toLong, end.toLong)
+    }}.toSeq
+  }
+
+  def isInvalid(num: Long): Boolean = {
+    val s = num.toString
+    s.take(s.length/2) == s.drop(s.length/2)
+  }
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val ranges = parseRanges(strings)
+    val invalid = ranges.map(r => (r._1 to r._2).filter(isInvalid))
+    invalid.map(_.sum).sum
   }
 
   override def part2(strings: Seq[String]): Long = {
