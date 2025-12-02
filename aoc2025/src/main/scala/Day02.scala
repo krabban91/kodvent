e9b6b2710a1715c5d245ee997df1a67dafd9cd9c
krabban91 <anderssongabriel665@gmail.com>
2025-12-02 07:51:06 +0100

    :sparkle: Complete Year 2025 Day 02 - part 2

--- aoc2025/src/main/scala/Day02.scala
+++ aoc2025/src/main/scala/Day02.scala
@@ -18,6 +18,26 @@ object Day02 extends App with AoCPart1Test with AoCPart2Test {
     s.take(s.length/2) == s.drop(s.length/2)
   }
 
+  def isInvalid2(num: Long): Boolean = {
+    // repeated atleast twice
+    val s = num.toString
+    (1 to s.length / 2).exists { i =>
+      val times = s.length / i
+      val key = s.take(i)
+      val tail = s.drop(i)
+      val invalid = (1 to times).foldLeft((tail, true)) { case ((t, out), _) =>
+        if (t.isEmpty || !out) {
+          ("", out)
+        } else {
+          val curr = t.take(i)
+          val next = t.drop(i)
+          (next, out && curr.length == i && curr == key)
+        }
+      }._2
+      invalid
+    }
+  }
+
   override def part1(strings: Seq[String]): Long = {
     val ranges = parseRanges(strings)
     val invalid = ranges.map(r => (r._1 to r._2).filter(isInvalid))
@@ -25,6 +45,8 @@ object Day02 extends App with AoCPart1Test with AoCPart2Test {
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val ranges = parseRanges(strings)
+    val invalid = ranges.map(r => (r._1 to r._2).filter(n => isInvalid(n) || isInvalid2(n)))
+    invalid.map(_.sum).sum
   }
 }
