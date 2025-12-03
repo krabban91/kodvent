ec0db875680c1bd95a06316a8912c96442532a27
krabban91 <anderssongabriel665@gmail.com>
2025-12-03 07:20:15 +0100

    :memo: Year 2025 Day 03 - Updated tests

--- aoc2025/src/test/scala/Day03Spec.scala
+++ aoc2025/src/test/scala/Day03Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day03Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day03.part1TestResult shouldEqual -1
+    Day03.part1TestResult shouldEqual 357L
   }
   "Part1" should "be correct" in {
-    Day03.part1Result shouldEqual -1
+    Day03.part1Result shouldEqual 3121910778619L
   }
   "Part2 Test" should "be correct" in {
-    Day03.part2TestResult shouldEqual -1
+    Day03.part2TestResult shouldEqual 16842L
   }
   "Part2" should "be correct" in {
-    Day03.part2Result shouldEqual -1
+    Day03.part2Result shouldEqual 167523425665348L
   }
 }
