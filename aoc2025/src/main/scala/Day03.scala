73b8ee239c6ea5fd50cb5613b7cbf9fd9f680b3b
krabban91 <anderssongabriel665@gmail.com>
2025-12-03 07:20:15 +0100

    :memo: Year 2025 Day 03 - Updated tests

--- aoc2025/src/main/scala/Day03.scala
+++ aoc2025/src/main/scala/Day03.scala
@@ -10,17 +10,20 @@ object Day03 extends App with AoCPart1Test with AoCPart2Test {
   override def part1(strings: Seq[String]): Long = {
     strings.map(_.strip()).map(bestBank(_, 2)).map(_.toLong).sum
   }
-  def bestBank(string: String, batteries: Int): String = {
-    if (batteries == 0) {return ""}
-    if (string == "") { print(s"bad input! $string and $batteries"); return ""}
-    var first = 0;
-    var firstVal: Char = 0;
-    (0 to string.length-batteries).foreach(i => if (string(i) > firstVal){firstVal = string(i); first = i})
-    val next = string.drop(first + 1)
-    s"$firstVal${bestBank(next,batteries-1)}"
-  }
 
   override def part2(strings: Seq[String]): Long = {
     strings.map(_.strip()).map(bestBank(_, 12)).map(_.toLong).sum
   }
+
+  private def bestBank(string: String, batteries: Int): String = {
+    if (batteries == 0) {
+      return ""
+    }
+    val test = string.dropRight(batteries - 1)
+    val value = test.max
+    val ix = test.indexOf(value)
+    val next = string.drop(ix + 1)
+    s"$value${bestBank(next, batteries - 1)}"
+  }
+
 }
