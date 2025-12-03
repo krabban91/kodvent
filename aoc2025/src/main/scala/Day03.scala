3f95f1a728a2ea9ff8e25a91f3a35b69675a8867
krabban91 <anderssongabriel665@gmail.com>
2025-12-03 07:13:19 +0100

    :sparkle: Complete Year 2025 Day 03

--- aoc2025/src/main/scala/Day03.scala
+++ aoc2025/src/main/scala/Day03.scala
@@ -8,10 +8,19 @@ object Day03 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    strings.map(_.strip()).map(bestBank(_, 2)).map(_.toLong).sum
+  }
+  def bestBank(string: String, batteries: Int): String = {
+    if (batteries == 0) {return ""}
+    if (string == "") { print(s"bad input! $string and $batteries"); return ""}
+    var first = 0;
+    var firstVal: Char = 0;
+    (0 to string.length-batteries).foreach(i => if (string(i) > firstVal){firstVal = string(i); first = i})
+    val next = string.drop(first + 1)
+    s"$firstVal${bestBank(next,batteries-1)}"
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    strings.map(_.strip()).map(bestBank(_, 12)).map(_.toLong).sum
   }
 }
