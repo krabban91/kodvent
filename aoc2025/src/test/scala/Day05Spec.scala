a5b6a21b8c3d65892f39cdedc7b2f4f9d7cce3ed
krabban91 <anderssongabriel665@gmail.com>
2025-12-05 11:48:35 +0100

    :sparkle: Complete Year 2025 Day 05

--- aoc2025/src/test/scala/Day05Spec.scala
+++ aoc2025/src/test/scala/Day05Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day05Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day05.part1TestResult shouldEqual -1
+    Day05.part1TestResult shouldEqual 3
   }
   "Part1" should "be correct" in {
-    Day05.part1Result shouldEqual -1
+    Day05.part1Result shouldEqual 701
   }
   "Part2 Test" should "be correct" in {
-    Day05.part2TestResult shouldEqual -1
+    Day05.part2TestResult shouldEqual 14
   }
   "Part2" should "be correct" in {
-    Day05.part2Result shouldEqual -1
+    Day05.part2Result shouldEqual 352340558684863L
   }
 }
