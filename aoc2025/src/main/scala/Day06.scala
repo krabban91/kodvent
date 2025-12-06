6ef430b8d8bd55f6d679a04a4bfb4e877c4d591c
krabban91 <anderssongabriel665@gmail.com>
2025-12-06 09:52:27 +0100

    :recycle: simplify string constructions

--- aoc2025/src/main/scala/Day06.scala
+++ aoc2025/src/main/scala/Day06.scala
@@ -28,33 +28,31 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
     }
   }
 
+  private def empty(size: Int) = (0 until size).map(_ => "")
+
   def parseInput(strings: Seq[String], part2: Boolean) = {
     val maxW = strings.map(_.length).max
-
-    def empty = strings.indices.map(_ => "")
-
-    val res = (0 until maxW).foldRight((empty, Seq[MathColumn]())) { case (ix, (curr, out)) =>
-      if (maxW - 1 > ix && strings.forall(_(ix) == ' ')) {
-        // clear seqs
-        val column = if (part2) {
-          MathColumn.part2(curr)
-        } else {
-          MathColumn.part1(curr)
-        }
-        (empty, column +: out)
+    val (rem, tail) = (0 until maxW).foldRight((empty(strings.size), Seq[MathColumn]())) { case (ix, (curr, out)) =>
+      if (curr.head.nonEmpty && strings.forall(_(ix) == ' ')) {
+        (empty(strings.size), MathColumn.parse(curr, part2) +: out)
       } else {
         (curr.zipWithIndex.map { case (line, j) => (if (strings(j).length > ix) strings(j)(ix) else ' ') +: line }, out)
       }
     }
-    val first = if (part2) {
-      MathColumn.part2(res._1)
-    } else {
-      MathColumn.part1(res._1)
-    }
-    first +: res._2
+    val first = MathColumn.parse(rem, part2)
+    first +: tail
   }
 
   object MathColumn {
+
+    def parse(curr: Seq[String], part2: Boolean): MathColumn = {
+      if (part2) {
+        MathColumn.part2(curr)
+      } else {
+        MathColumn.part1(curr)
+      }
+    }
+
     def part1(curr: Seq[String]): MathColumn = {
       val op = curr.last.strip() == "*"
       val values = curr.dropRight(1).map(_.strip().toLong)
@@ -63,18 +61,12 @@ object Day06 extends App with AoCPart1Test with AoCPart2Test {
 
     def part2(curr: Seq[String]): MathColumn = {
       val op = curr.last.strip() == "*"
-      val value = curr.dropRight(1)
-
-      def empty = curr.indices.map(_ => "")
-
-      val w = value.head.length
-      val values = (0 until w).foldRight(empty) { case (ix, words) =>
-        val next = value.map(_(ix)).foldLeft("") { case (w, c) => s"$w$c" }
-        words :+ next
-      }.map(_.strip()).filter(_.nonEmpty).map(_.toLong)
+      val numbers = curr.dropRight(1)
+      val words = (0 until numbers.head.length)
+        .map { ix => numbers.map(_(ix)).mkString("") }
+      val values = words
+        .map(_.strip()).filter(_.nonEmpty).map(_.toLong)
       MathColumn(values, op)
     }
   }
-
-
 }
