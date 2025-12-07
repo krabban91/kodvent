419a96f3ce58f0f26035ea8ca3a57f5167b484ad
krabban91 <anderssongabriel665@gmail.com>
2025-12-07 08:24:42 +0100

    :sparkle: Complete Year 2025 Day 07

--- aoc2025/src/main/scala/Day07.scala
+++ aoc2025/src/main/scala/Day07.scala
@@ -1,5 +1,7 @@
 import aoc.numeric.{AoCPart1Test, AoCPart2Test}
 
+import scala.collection.mutable
+
 object Day07 extends App with AoCPart1Test with AoCPart2Test {
 
   printResultPart1Test
@@ -8,10 +10,31 @@ object Day07 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart2
 
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val (_, splits) = splitBeams(strings)
+    splits
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val (out, _) = splitBeams(strings)
+    out.values.sum
+  }
+
+  private def splitBeams(strings: Seq[String]): (Map[Int, Long], Long) = {
+    val beamIndices = Map[Int, Long](strings.head.indexOf("S") -> 1L)
+    strings.tail.foldLeft((beamIndices, 0L)) { case ((indices, splits), row) =>
+      val nextBeams = mutable.HashMap[Int, Long]()
+      var dSplits = 0L
+      indices.foreach { case (ix, times) =>
+        if (row(ix) == '^') {
+          dSplits += 1L
+          val (left, right) = (ix - 1, ix + 1)
+          nextBeams.put(left, nextBeams.getOrElse(left, 0L) + times)
+          nextBeams.put(right, nextBeams.getOrElse(right, 0L) + times)
+        } else {
+          nextBeams.put(ix, nextBeams.getOrElse(ix, 0L) + times)
+        }
+      }
+      (nextBeams.toMap, splits + dSplits)
+    }
   }
 }
