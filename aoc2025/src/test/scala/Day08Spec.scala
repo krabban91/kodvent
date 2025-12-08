288b0c734b406e1b33cb5270dc7d47ad57fe45f1
krabban91 <anderssongabriel665@gmail.com>
2025-12-08 15:06:03 +0100

    :memo: Year 2025 Day 08 - Updated tests
    * Part 1: 56 minutes
    * Part 2: 25 minutes

--- aoc2025/src/test/scala/Day08Spec.scala
+++ aoc2025/src/test/scala/Day08Spec.scala
@@ -3,15 +3,15 @@ import org.scalatest.matchers.should.Matchers
 
 class Day08Spec extends AnyFlatSpec with Matchers {
   "Part1 Test" should "be correct" in {
-    Day08.part1TestResult shouldEqual -1
+    Day08.part1TestResult shouldEqual 40
   }
   "Part1" should "be correct" in {
-    Day08.part1Result shouldEqual -1
+    Day08.part1Result shouldEqual 75582
   }
   "Part2 Test" should "be correct" in {
-    Day08.part2TestResult shouldEqual -1
+    Day08.part2TestResult shouldEqual 25272
   }
   "Part2" should "be correct" in {
-    Day08.part2Result shouldEqual -1
+    Day08.part2Result shouldEqual 59039696
   }
 }
