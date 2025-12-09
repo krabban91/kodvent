ac2674fcf89ba2818aa0e2180b5a9701de97c66b
krabban91 <anderssongabriel665@gmail.com>
2025-12-09 20:30:41 +0100

    :sparkle: Complete Year 2025 Day 09 Part 1

--- aoc2025/src/main/scala/Day09.scala
+++ aoc2025/src/main/scala/Day09.scala
@@ -7,11 +7,31 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
   printResultPart1
   printResultPart2
 
+  def parseInput(strings: Seq[String]): Set[(Long, Long)] = {
+    strings.map{s => val t = s.split(","); (t.head.toLong, t.last.toLong)}.toSet
+  }
+
+  def areaOfPair(p: Set[(Long, Long)]): Long = {
+    val t = p.toSeq
+    val (a, b) = (t.head , t.last)
+    (Math.abs(a._1 - b._1) + 1) * (Math.abs(a._2 - b._2) + 1)
+  }
+
   override def part1(strings: Seq[String]): Long = {
-    -1
+    val points: Set[(Long, Long)] = parseInput(strings)
+    val allPairs = points.subsets(2).toSeq
+    val areas = allPairs.map(p => (p, areaOfPair(p)))
+    val best = areas.maxBy(_._2)
+    best._2
   }
 
   override def part2(strings: Seq[String]): Long = {
-    -1
+    val points: Set[(Long, Long)] = parseInput(strings)
+    val allPairs = points.subsets(2).toSeq
+    val areas = allPairs.map(p => (p, areaOfPair(p)))
+    val filtered = areas
+    val best = filtered.maxBy(_._2)
+    best._2
+
   }
 }
