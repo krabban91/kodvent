2eaf1d3b52abcbe50ca3e6546518d25b12dbcf88
krabban91 <anderssongabriel665@gmail.com>
2025-12-10 12:53:15 +0100

    :sparkle: Complete Year 2025 Day 09 Part 2

--- aoc2025/src/main/scala/Day09.scala
+++ aoc2025/src/main/scala/Day09.scala
@@ -27,18 +27,24 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
     best._2
   }
 
-  def isWithinBorders(t: (Set[(Long, Long)], Long), allBorders: Set[(Long, Long)], orderedRed: Seq[(Long, Long)], knownInside : mutable.HashSet[(Long, Long)]) = {
-    if (t._2 >= 2964756515L) {
+  def isWithinBorders(t: (Set[(Long, Long)], Long), allBorders: Set[(Long, Long)], orderedRed: Seq[(Long, Long)], knownOutside: mutable.HashSet[(Long, Long)], knownInside: mutable.HashSet[(Long, Long)]) = {
+    val points = t._1.toSeq
+    val (a, b) = (points.head, points.last)
+    val (minY, maxY) = (Math.min(a._2, b._2), Math.max(a._2, b._2))
+    val (minX, maxX) = (Math.min(a._1, b._1), Math.max(a._1, b._1))
+    val corners = Seq((minX, minY), (maxX, minY), (minX, maxY), (maxX, maxY))
+    val rejectFast = corners.find(p => knownOutside(p))
+      .orElse(knownOutside.find { case (ox, oy) => ox >= minX && ox <= maxX && oy >= minY && oy <= maxY })
+    if (rejectFast.isDefined) {
+      //println(s"Reject fast: area: ${t._2}, [$a,$b] due to ${rejectFast.get} being inside box")
       false
     } else {
-      println(s"Checking ${t}")
+      println(s"Checking: area=${t._2}, [$a,$b]")
       val redSet = orderedRed.toSet
-      val points = t._1.toSeq
-      val (a, b) = (points.head, points.last)
-      val xs = (Math.min(a._1, b._1) to Math.max(a._1, b._1))
-      val ys = (Math.min(a._2, b._2) to Math.max(a._2, b._2))
+      val smallYs = orderedRed.map(_._2).flatMap(y => Seq((y - 1), y, (y + 1))).distinct
+        .filter(y => y >= minY && y <= maxY).sorted
 
-      val horiCheck = ys.forall { y =>
+      val horiCheck = smallYs.forall { y =>
         val sameY = allBorders.filter(_._2 == y)
         if (sameY.isEmpty) {
           false
@@ -46,133 +52,149 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
           val leftMost = sameY.minBy(_._1)
           val rightMost = sameY.maxBy(_._1)
           if (a._1 < leftMost._1 || b._1 < leftMost._1 || a._1 > rightMost._1 || b._1 > rightMost._1) {
-            false
-          } else {
-            // TODO
-            var enteredOnRed = false
-            var inside = false
-            var isValid = true
-            var currX = leftMost._1 - 1
-            val (minX, maxX) = (Math.min(a._1, b._1), Math.max(a._1, b._1))
-            while (isValid && currX <= maxX) {
-              val currP = (currX, y)
-              if (redSet.contains(currP)) {
-                val nextP = (currX + 1, y - 1)
-                enteredOnRed = !enteredOnRed
-                if (inside && knownInside(nextP)) {
-                  // naive inside check worked
-                  print(s"Stayed inside: $currP")
-                } else {
-                  floodFromKnown(allBorders, knownInside)
-                  if (inside && knownInside(nextP)) {
-                    // inside check worked
-                    print(s"Stayed inside: $currP")
-                  } else {
-                  inside = !inside
-                  }
-                }
-              }
-              else if (!enteredOnRed && allBorders.contains(currP)) {
-                inside = !inside
-              }
-              else {
-                if (inside && !allBorders(currP)) {
-                  knownInside.add(currP)
-                }
-                if (currX >= minX && currX <= maxX && !inside) {
-                  isValid = false
-                }
-              }
-              currX += 1
+            if ((a._1 < leftMost._1 || a._1 > rightMost._1)) {
+              val outside = (a._1, y)
+              knownOutside.add(outside)
             }
-
-            isValid
-          }
-        }
-      }
-      val out = if (horiCheck) {
-
-        val vertCheck = xs.forall { x =>
-          val sameX = allBorders.filter(_._1 == x)
-          if (sameX.isEmpty) {
+            if ((b._1 < leftMost._1 || b._1 > rightMost._1)) {
+              val outside = (b._1, y)
+              knownOutside.add(outside)
+            }
             false
           } else {
-            val topMost = sameX.minBy(_._2)
-            val bottomMost = sameX.maxBy(_._2)
-            if (a._2 < topMost._2 || b._2 < topMost._2 || a._2 > bottomMost._2 || b._2 > bottomMost._2) {
-              false
+            val redSameY = redSet.filter(_._2 == y)
+            val xToCheck = (Set(minX, maxX) ++ sameY.map(_._1).flatMap(x => Seq(x - 1, x, x + 1)))
+              .filter(x => x <= maxX).toSeq.sorted
+            val needsToBeInside = xToCheck.filter(x => x >= minX).map((_, y))
+            if (needsToBeInside.forall(p => knownInside(p))) {
+              // already checked. Ok
+              true
             } else {
-              // TODO ensure that all Ys are inside polygon
+              // TODO
+              // either enter on red, or cross green border.
+              // - if enter on red, need to know if exits into same or different.
+              // - if cross green border, need to know if we are crossing green multiple times
               var enteredOnRed = false
               var inside = false
               var isValid = true
-              var currY = topMost._2 - 1
-              val (minY, maxY) = (Math.min(a._2, b._2), Math.max(a._2, b._2))
-              while (isValid && currY <= maxY) {
-                val currP = (x, currY)
-                if (redSet.contains(currP)) {
-                  val nextP = (x - 1, currY + 1)
-                  enteredOnRed = !enteredOnRed
-                  if (inside && knownInside(nextP)) {
-                    // naive inside check worked
-                    print(s"Stayed inside: $currP")
+              var upIsIn = false
+              var ix = 0
+              while (isValid && ix < xToCheck.size) {
+                val currX = xToCheck(ix)
+                val p = (currX, y)
+                if (!inside) {
+                  if (enteredOnRed) {
+                    println("Unhandled scenario: outside, enterOnRed")
+                    val z = 1 - 1
+                    val v = 1 / z
                   } else {
-                    floodFromKnown(allBorders, knownInside)
-                    if (inside && knownInside(nextP)) {
-                      // inside check worked
-                      print(s"Stayed inside: $currP")
-                    } else {
-                      inside = !inside
-                    }
-                  }
-                }
-                else if (!enteredOnRed && allBorders.contains(currP)) {
-                  inside = !inside
-                }
-                else {
-                  if (inside && !allBorders(currP)) {
-                    knownInside.add(currP)
-                  }
-                  if (currY >= minY && currY <= maxY && !inside) {
-                    isValid = false
-                  }
-                }
-                currY += 1
-              }
+                    if (redSet(p)) {
+                      inside = true
+                      // prev was empty
+                      enteredOnRed = true
+                      // instead of checking node directly above: check the ordered seq if item before or after is above
+                      val redIx = orderedRed.indexOf(p)
+                      val sameX = Seq(redIx - 1, redIx + 1).map(v => ((v + orderedRed.size) % orderedRed.size))
+                        .map(orderedRed)
+                        .filter(_._1 == p._1).head
 
+                      val turnFromAbove = sameX._2 < p._2
+                      if (turnFromAbove) {
+                        upIsIn = true
+                      } else {
+                        upIsIn = false
+                      }
+                    } else if (allBorders(p)) {
+                      inside = true
+                    } else {
+                      if (currX >= minX) {
+                        // not inside, not on border. Fail if intersect with box
+                        knownOutside.add(p)
+                        println(s"Adding ${p} to known outside due to inside check failed")
+                        isValid = false
+                      }
+                    }
+
+                  }
+                } else {
+                  if (redSet(p)) {
+                    if (!enteredOnRed) {
+                      //println("Unhandled scenario: inside: Entering on red")
+                      // now lets figure out when aboveIsIn
+                      enteredOnRed = true
+                      val redIx = orderedRed.indexOf(p)
+                      val sameX = Seq(redIx - 1, redIx + 1).map(v => ((v + orderedRed.size) % orderedRed.size))
+                        .map(orderedRed)
+                        .filter(_._1 == p._1).head
+
+                      val turnFromAbove = sameX._2 < p._2
+                      if (turnFromAbove) {
+                        upIsIn = false
+                      } else {
+                        upIsIn = true
+                      }
+
+                    } else {
+
+                      // exiting hori-line
+                      enteredOnRed = false
+                      val redIx = orderedRed.indexOf(p)
+                      val sameX = Seq(redIx - 1, redIx + 1).map(v => ((v + orderedRed.size) % orderedRed.size))
+                        .map(orderedRed)
+                        .filter(_._1 == p._1).head
+                      val turnsUp = sameX._2 < p._2
+                      inside = (turnsUp != upIsIn)
+                      if (turnsUp) {
+                        // regardless: inside = (turnsUp != upIsIn)
+                        // from outside (upIsIn == true)
+                        // ´-´ is out: false = (turnsUp != upIsIn)
+                        // `-. is in : true  = (turnsUp != upIsIn)
+                        // from inside (upIsIn == false)
+                        // ´-´ is in : true  = (turnsUp != upIsIn)
+                        // `-. is out: false = (turnsUp != upIsIn)
+                        // inside = (turnsUp != upIsIn)
+                      } else {
+                        // regardless: inside = (turnsUp != upIsIn)
+                        // from outside (upIsIn == false)
+                        // .-. is out: false = (turnsUp != upIsIn)
+                        // .-´ is in : true  = (turnsUp != upIsIn)
+                        // from inside (upIsIn == true)
+                        // .-. is in : true  = (turnsUp != upIsIn)
+                        // .-´ is out: false = (turnsUp != upIsIn)
+                        // inside = (turnsUp != upIsIn)
+                      }
+
+                    }
+                  } else if (allBorders(p)) {
+                    if (enteredOnRed) {
+                      // follow line
+                    } else {
+                      // flip state
+                      inside = false
+                    }
+                  } else {
+                    // inside, nothing to check
+                  }
+                }
+                ix += 1
+              }
+              if (isValid) {
+                knownInside.addAll(needsToBeInside)
+              }
               isValid
             }
           }
         }
-        vertCheck
-      } else {
-        false
       }
-      println(s"area: ${t._2}, [$a,$b] xs.size=${xs.size}, ys.size=${ys.size}")
-      out
-    }
-  }
 
-  private def floodFromKnown(allBorders: Set[(Long, Long)], knownInside: mutable.HashSet[(Long, Long)]): Unit = {
-    // need to flood from known.
-    val candidates = mutable.PriorityQueue[(Long, Long)]()
-    candidates.addAll(knownInside)
-    val checked = mutable.HashSet[(Long, Long)]()
-    while (candidates.nonEmpty) {
-      val pop = candidates.dequeue()
-      if (!checked(pop)) {
-        knownInside.add(pop)
-        checked.add(pop)
-        val directions = Seq((0, 1), (1, 0), (0, -1), (-1, 0)).map(p => (p._1 + pop._1, p._2 + pop._2)).toSet
-          .diff(knownInside).diff(checked).diff(allBorders)
-        candidates.addAll(directions)
-      }
+      //println(s"area: ${t._2}, [$a,$b] xs.size=${xs.size}, ys.size=${ys.size}: $horiCheck")
+      horiCheck
     }
   }
 
   override def part2(strings: Seq[String]): Long = {
     val redPoints: Seq[(Long, Long)] = parseInput(strings)
-    val greenPointBorders = redPoints.sliding(2).flatMap { window =>
+    val greenPointBorders = (redPoints :+ redPoints.head).sliding(2).flatMap { window =>
       val (from, to) = (window.head, window.last)
       if (from._1 == to._1) {
         val min = Math.min(from._2, to._2)
@@ -185,77 +207,22 @@ object Day09 extends App with AoCPart1Test with AoCPart2Test {
       }
     }.toSet
 
-    val knownInside = mutable.HashSet[(Long, Long)]()
-
     val redSet = redPoints.toSet
     val allBorders = redSet ++ greenPointBorders
 
-    // from left
-    greenPointBorders.map(_._2).foreach{ y =>
-      val xs = allBorders.map(_._1)
-      val (minX, maxX) = (xs.min, xs.max)
-      var inside = false
-      var redCornered = false
-      var upCorner = false
-      (minX - 1 to maxX).foreach{ x =>
-        val p = (x,y)
-        if (inside) {
-          if (allBorders(p)) {
-            inside = false
-            if (redSet(p)) {
-              redCornered = true
-              if (greenPointBorders((p._1, p._2 - 1))) {
-                upCorner = true
-              }
-            }
-          } else {
-            knownInside.add(p)
-          }
-        } else {
-          if (!redCornered) {
-            if (redSet(p)) {
-              redCornered = true
-              if (greenPointBorders((p._1, p._2 - 1))) {
-                upCorner = true
-              }
-            } else {
-              if (greenPointBorders(p)) {
-                inside = true
-              }
-            }
-          } else {
-            if (redSet(p)) {
-
-              redCornered = false
-              if (upCorner) {
-                if (greenPointBorders((p._1, p._2 - 1))) {
-                  inside = false
-                } else {
-                  inside = true
-                }
-              } else {
-                if (greenPointBorders((p._1, p._2 - 1))) {
-                  inside = true
-                } else {
-                  inside = false
-                }
-              }
-            } else {
-              if (greenPointBorders(p)) {
-                inside = true
-              }
-            }
-          }
-        }
-      }
-    }
-
-
+    val knownOutside = mutable.HashSet[(Long, Long)]()
+    val knownInside = mutable.HashSet[(Long, Long)]()
     val allPairs = redSet.subsets(2).toSeq
     val areas = allPairs.map(p => (p, areaOfPair(p))).sortBy(_._2).reverse
-    val filtered = areas.find { t => isWithinBorders(t, allBorders, redPoints, knownInside) }
+    val filtered = areas
+      //.find { t => checkFloodBorders(t, allBorders, redPoints, knownInside) }
+      .find { t => isWithinBorders(t, allBorders, redPoints, knownOutside, knownInside) }
     val best = filtered.maxBy(_._2)
     best._2
+    // 2445367554 is too high => had to try
+    // 1566346198
+    // 222157805 is too low => rejecting too many
+    // 113278013 is too low => rejecting too many
 
   }
 
