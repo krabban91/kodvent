cb6ebe57134974fc52e128badb20f9b59061e7ae
krabban91 <anderssongabriel665@gmail.com>
2026-01-17 19:45:06 +0100

    Create a rust project for 2024

--- aoc2024/project/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
-  lazy val combinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.0"
-  lazy val kodventCommon = "kodvent" % "common" % "0.0.1-SNAPSHOT"
-  lazy val kodventScalaCommon = "kodvent" % "scala-common_2.13" % "0.1.0-SNAPSHOT"
-}
