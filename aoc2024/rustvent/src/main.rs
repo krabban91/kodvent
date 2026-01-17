eb32717989070390a9c24d76f76b003115f88a65
krabban91 <anderssongabriel665@gmail.com>
2026-01-17 20:55:43 +0100

    :sparkle: Complete Year 2024 Day 01 'with rust'

--- aoc2024/rustvent/src/main.rs
+++ aoc2024/rustvent/src/main.rs
@@ -1,10 +1,5 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+mod day01;
 
 fn main() {
-    let stdo = stdout();
-    let msg = String::from("Hello, world!");
-    let w = msg.chars().count();
-    let writer = BufWriter::new(stdo.lock());
-    say(&msg, w, writer).unwrap()
+    day01::day();
 }
