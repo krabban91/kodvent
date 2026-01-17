7d7b8a9bec6160520a795bbb8bf5b5d87a3b242f
krabban91 <anderssongabriel665@gmail.com>
2026-01-17 19:57:21 +0100

    :package: Year 2024 Day 01 preparations

--- aoc2024/read.sh
+++ aoc2024/read.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 d=$( echo $1 | sed "s/^0//")
-filename="src/main/resources/day$1\$.txt"
-curl https://adventofcode.com/2023/day/$d/input -H "cookie: session=$AOC_ID" > "$filename"
+filename="rustvent/input/real/day$1.txt"
+curl https://adventofcode.com/2024/day/$d/input -H "cookie: session=$AOC_ID" > "$filename"
 
-open https://adventofcode.com/2023/day/$d
+open https://adventofcode.com/2024/day/$d
